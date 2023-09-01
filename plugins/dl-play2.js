e589a819415aa69b98860a18bfdab150bc23f460
Dede Kurniawan <dede.klender@gmail.com>
2023-09-01 08:55:25 +0700

    update dl-play2.js

--- plugins/dl-play2.js
+++ plugins/dl-play2.js
@@ -21,7 +21,7 @@ let handler = async (m, { conn, text, args, isPrems, isOwner, usedPrefix, comman
   let title = await yt.title
   let size = await (isVideo ? yt.video[q].fileSizeH : yt.audio[q].fileSizeH)
   let play = `
-	≡ *Akebi-chan MUSIC*
+	≡ *Mika Bot MUSIC*
 ┌──────────────
 ▢ 📌 *Judul* : ${vid.title}
 ▢ 📆 *Diunggah pada:* ${vid.ago}
@@ -32,10 +32,10 @@ let handler = async (m, { conn, text, args, isPrems, isOwner, usedPrefix, comman
 _Mengirim..._`
 conn.sendFile(m.chat, vid.thumbnail, 'play', play, m, null, rpig)
 
-if (size.split('MB')[0] >= limit) return m.reply(` ≡  *Akebi-chan YTDL*\n\n▢ *⚖️ Ukuran* : ${size}\n▢ *🎞 Deskripsi* : ${q}\n\n▢ _File melebihi batas ukuran_ *+${limit} MB*`) 
+if (size.split('MB')[0] >= limit) return m.reply(` ≡  *Mika Bot YTDL*\n\n▢ *⚖️ Ukuran* : ${size}\n▢ *🎞 Deskripsi* : ${q}\n\n▢ _File melebihi batas ukuran_ *+${limit} MB*`) 
 if (size.includes('GB')) return m.reply(` ≡  *Akebi-chan YTDL*\n\n▢ *⚖️ Ukuran* : ${size}\n▢ *🎞 Bitrate* : ${q}\n\n▢ _File melebihi batas ukuran_ *+${limit} MB*`)   
 	  conn.sendFile(m.chat, dl_url, title + '.mp' + (3 + /vid$/.test(command)), `
- ≡  *Akebi-chan YTDL*
+ ≡  *Mika Bot YTDL*
   
 ▢ *📌 Judul* : ${title}
 ▢ *🎞 Bitrate* : ${q}
