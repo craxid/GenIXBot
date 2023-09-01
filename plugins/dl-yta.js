3a7d7a192e1e8b7ba830c12448b62e6bdd758ea3
Dede Kurniawan <dede.klender@gmail.com>
2023-09-01 09:00:43 +0700

    update dl-yta.js

--- plugins/dl-yta.js
+++ plugins/dl-yta.js
@@ -13,7 +13,7 @@ let handler = async (m, { conn, text, args, isPrems, isOwner, usedPrefix, comman
 		const title = await yt.title
 		const size = await yt.audio[q].fileSizeH
 		conn.sendFile(m.chat, dl_url, title + '.mp3', `
- ≡  *Akebi-chan YTDL*
+ ≡  *Mika Bot YTDL*
   
 ▢ *📌 Judul* : ${title}
 ▢ *⚖️ Ukuran* : ${size}
