90c7a427dfd29d4a7d72d2ec37c32ce6f4a1bd96
Dede Kurniawan <dede.klender@gmail.com>
2023-09-03 07:07:15 +0700

    update remini.js

--- plugins/remini.js
+++ plugins/remini.js
@@ -1,12 +1,12 @@
 import fetch from 'node-fetch'
 let handler = async (m, { conn, args, usedPrefix, command }) => {
     
-    if (!text) throw `✳️ Under Development!`
-    m.react('')
+    if (!text) throw `✳️ This feature is under Development!`
+    m.react('🥰')
 }
 handler.help = ['enchant <image>']
 handler.tags = ['ai']
-handler.command = ['enchant','reminit','hd','tohd']
+handler.command = ['enchant','remini','hd','tohd']
 handler.premium = false
 handler.diamond = true
 
