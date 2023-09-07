9d35c6584d1ea09a0e947ccb4344565648b11b52
Dede Kurniawan <dede.klender@gmail.com>
2023-09-08 02:52:32 +0700

    update upload2.js

---
+++ plugins/upload2.js
@@ -0,0 +1,23 @@
+import axios from 'axios'
+import FormData from 'form-data'
+import { fileTypeFromBuffer } from "file-type"
+import fs from "fs"
+
+const tmpFile = (buffer) => {
+  return new Promise(async (resolve, reject) => {
+    const { ext, mime } = await fileTypeFromBuffer(buffer)
+    const form = new FormData();
+    form.append('file', buffer, {
+      filename: new Date() * 1 + '.' + ext,   
+      contentType: mime
+    });
+
+    const { data } = await axios.post("https://tmpfiles.org/api/v1/upload", form, {
+      headers: {
+        ...form.getHeaders(),
+      },
+    })
+    .catch((e) => resolve(e?.response))
+    resolve(data) 
+  })
+}
