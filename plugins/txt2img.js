0285a25e23924c164d1b3dfa98407882da72c06a
Dede Kurniawan <dede.klender@gmail.com>
2023-09-07 22:16:46 +0700

    update txt2img.js

--- plugins/txt2img.js
+++ plugins/txt2img.js
@@ -9,7 +9,7 @@ m.react('💬')
 try {
 let diff = await conn.getFile(`https://api.azz.biz.id/api/text2img?q=${text}&key=mangea`)
 	    
-conn.sendFile(m.chat, diff.data, 'img.jpg', `*TEXT TO IMAGE*\n\nPROMPT:\n${text}`, m)
+conn.sendFile(m.chat, diff.data, 'img.jpg', `*[ TEXT TO IMAGE ]*\n\nPROMPT:\n${text}`, m)
 	}
 	catch {
 		m.reply(`❎ Error: Ada sebuah kesalahan`)
