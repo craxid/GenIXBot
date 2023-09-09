6cf63551b7b63af2f8215baba1c2194f1ab15d15
Dede Kurniawan <dede.klender@gmail.com>
2023-09-10 00:01:11 +0700

    update econ-work.js

--- plugins/econ-work.js
+++ plugins/econ-work.js
@@ -110,14 +110,14 @@ switch(type) {
 	case 'ojek':
 	if (global.db.data.users[m.sender].ojek == false) throw 'ini bukan tugas kamu atau kamu pengangguran!'
 if (new Date - global.db.data.users[m.sender].lastwork < 300000)  throw `Kamu sudah bekerja\nTunggu  *${timers}* Untuk kembali bekerja ojek`
-	global.db.data.users[m.sender].atm += uangm
+	global.db.data.users[m.sender].exp += uangm
 global.db.data.users[m.sender].lastwork = new Date * 1
 	m.reply(`Kamu Sudah Mengantarkan *${penumpang}*\nDan mendapatkan uang senilai *Rp ${uangm}*`)
 break
      case 'pedagang':
      if (global.db.data.users[m.sender].pedagang == false) throw 'ini bukan tugas kamu atau kamu pengangguran!'
 if (new Date - global.db.data.users[m.sender].lastwork < 300000)  throw `Kamu sudah bekerja\nTunggu  *${timers}* Untuk kembali bekerja dagang`
-	global.db.data.users[m.sender].atm += duit
+	global.db.data.users[m.sender].exp += duit
 global.db.data.users[m.sender].lastwork = new Date * 1
 	m.reply(`Ada pembeli yg membeli *${dagangan}*\nDan mendapatkan uang senilai *Rp ${duit}*`)
 	if (pecat > 1 ) {
@@ -128,28 +128,28 @@ break
       case 'dokter':
  if (global.db.data.users[m.sender].dokter == false) throw 'ini bukan tugas kamu atau kamu pengangguran!'
 if (new Date - global.db.data.users[m.sender].lastwork < 300000)  throw `Kamu sudah bekerja\nTunggu  *${timers}* Untuk kembali bekerja kdokter`
-	global.db.data.users[m.sender].atm += duitm
+	global.db.data.users[m.sender].exp += duitm
 global.db.data.users[m.sender].lastwork = new Date * 1
 	m.reply(`Kamu menyembuhkan pasien *${pasien}*\nDan mendapatkan uang senilai *Rp ${duitm}*`)
 break
        case 'petani':
 if (global.db.data.users[m.sender].petani == false) throw 'ini bukan tugas kamu atau kamu pengangguran!'
 if (new Date - global.db.data.users[m.sender].lastwork < 300000)  throw `Kamu sudah bekerja\nTunggu  *${timers}* Untuk kembali bekerja petani`
-	global.db.data.users[m.sender].atm += uangm
+	global.db.data.users[m.sender].exp += uangm
 global.db.data.users[m.sender].lastwork = new Date * 1
 	m.reply(`${panen} Sudah Panen ! Dan menjualnya\nDan mendapatkan uang senilai Rp *${duitd}*`)
 break
      case 'montir':
  if (global.db.data.users[m.sender].montir == false) throw 'ini bukan tugas kamu atau kamu pengangguran!'
 if (new Date - global.db.data.users[m.sender].lastwork < 300000)  throw `Kamu sudah bekerja\nTunggu  *${timers}* Untuk kembali bekerja montir`
-	global.db.data.users[m.sender].atm += duitr
+	global.db.data.users[m.sender].exp += duitr
 global.db.data.users[m.sender].lastwork = new Date * 1
 	m.reply(`Kamu Baru saja mendapatkan pelanggan dan memperbaiki *${bengkel}*\nDan kamu mendapatkan uang senilai *Rp ${duitr}*`)
 break
       case 'kuli':
  if (global.db.data.users[m.sender].kuli == false) throw 'ini bukan tugas kamu atau kamu pengangguran!'
 if (new Date - global.db.data.users[m.sender].lastwork < 300000)  throw `Kamu sudah bekerja\nTunggu  *${timers}* Untuk kembali bekerja kuli`
-	global.db.data.users[m.sender].atm += duitk
+	global.db.data.users[m.sender].exp += duitk
 global.db.data.users[m.sender].lastwork = new Date * 1
 	m.reply(`Kamu baru saja selesai ${rumah}\nDan mendapatkan uang senilai *Rp ${duitk}*`)
 break
