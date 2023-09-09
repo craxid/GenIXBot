8747d4fd42ec31af2cc374a26012466e599667cf
Dede Kurniawan <dede.klender@gmail.com>
2023-09-09 17:17:44 +0700

    update cuaca.js

--- plugins/cuaca.js
+++ plugins/cuaca.js
@@ -1,27 +1,32 @@
-import cuaca from 'caliph-api'
+import axios from 'axios'
 
-let handler = async (m, { conn, args, usedPrefix, command, text }) => {
+let handler = async (m, { args }) => {
+  if (!args[0]) throw "Please provide place or location name"
+  try {
+    const response = axios.get(
+      `https://api.openweathermap.org/data/2.5/weather?q=${args}&units=metric&appid=060a6bcfa19809c2cd4d97a212b19273`
+    )
+    const res = await response
+    const name = res.data.name
+    const Country = res.data.sys.country
+    const Weather = res.data.weather[0].description
+    const Temperature = res.data.main.temp + "°C"
+    const Minimum_Temperature = res.data.main.temp_min + "°C"
+    const Maximum_Temperature = res.data.main.temp_max + "°C"
+    const Humidity = res.data.main.humidity + "%"
+    const Wind = res.data.wind.speed + "km/h"
+    const wea = `「 📍 」 Place: ${name}\n「 🗺️ 」 Country: ${Country}\n「 🌤️ 」 Weather: ${Weather}\n「 🌡️ 」Temperature: ${Temperature}\n「 💠 」 Minimum Temperature: ${Minimum_Temperature}\n「 📛 」 Maximum Temperature: ${Maximum_Temperature}\n「 💦 」 Humidity: ${Humidity}\n「 🌬️ 」 Wind: ${Wind}
+  `
 
-if (!text) throw `✳️ Masukan nama daerah`
-
-m.react(rwait)
-
-try {
-  let { status, data: resultInfo } = await cuaca.search.cuaca(text);
-    
-}
-  if (status != 200) {
-    catch {m.reply(`Daerah ${text} tidak ditemukan!`);
+    m.reply(wea)
+  } catch (e) {
+    return "Error location not found!!!"
   }
-await m.reply( parseRes ( resultInfo, { title: "Cuaca Hari Ini", })
-  );
-  
-  m.react(cuaca)
 }
-handler.help = ['cuaca <daerah>']
-handler.tags = ['tools']
-handler.command = ['cuaca','weather'] 
-handler.diamond = true
-handler.premium = true
 
-export default handler
+handler.help = ['weather']
+handler.tags = ['tools']
+
+handler.command = /^(weather)$/i
+
+ecport defau handler
