b7f6d7fdd9c84f8e5448b25c0a48f08ba2f73618
Dede Kurniawan <dede.klender@gmail.com>
2023-09-09 09:29:21 +0700

    update game-slot.js

--- plugins/game-slot.js
+++ plugins/game-slot.js
@@ -67,7 +67,7 @@ handler.help = ['slot <jumlah taruhan>']
 handler.tags = ['game']
 handler.command = ['slot']
 handler.register = true
-hanlder.group = true
+handler.group = true
 
 export default handler
 
