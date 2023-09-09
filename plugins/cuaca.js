3c243ccce0eb58613c9f04564813854510656db8
Dede Kurniawan <dede.klender@gmail.com>
2023-09-09 17:21:40 +0700

    update cuaca.js

--- plugins/cuaca.js
+++ plugins/cuaca.js
@@ -6,16 +6,16 @@ let handler = async (m, { args }) => {
     const response = axios.get(
       `https://api.openweathermap.org/data/2.5/weather?q=${args}&units=metric&appid=060a6bcfa19809c2cd4d97a212b19273`
     )
-    const res = await response
-    const name = res.data.name
-    const Country = res.data.sys.country
-    const Weather = res.data.weather[0].description
-    const Temperature = res.data.main.temp + "°C"
-    const Minimum_Temperature = res.data.main.temp_min + "°C"
-    const Maximum_Temperature = res.data.main.temp_max + "°C"
-    const Humidity = res.data.main.humidity + "%"
-    const Wind = res.data.wind.speed + "km/h"
-    const wea = `「 📍 」 Place: ${name}\n「 🗺️ 」 Country: ${Country}\n「 🌤️ 」 Weather: ${Weather}\n「 🌡️ 」Temperature: ${Temperature}\n「 💠 」 Minimum Temperature: ${Minimum_Temperature}\n「 📛 」 Maximum Temperature: ${Maximum_Temperature}\n「 💦 」 Humidity: ${Humidity}\n「 🌬️ 」 Wind: ${Wind}
+    let res = await response
+    let name = res.data.name
+    let Country = res.data.sys.country
+    let Weather = res.data.weather[0].description
+    let Temperature = res.data.main.temp + "°C"
+    let Minimum_Temperature = res.data.main.temp_min + "°C"
+    let Maximum_Temperature = res.data.main.temp_max + "°C"
+    let Humidity = res.data.main.humidity + "%"
+    let Wind = res.data.wind.speed + "km/h"
+    let wea = `「 📍 」 Place: ${name}\n「 🗺️ 」 Country: ${Country}\n「 🌤️ 」 Weather: ${Weather}\n「 🌡️ 」Temperature: ${Temperature}\n「 💠 」 Minimum Temperature: ${Minimum_Temperature}\n「 📛 」 Maximum Temperature: ${Maximum_Temperature}\n「 💦 」 Humidity: ${Humidity}\n「 🌬️ 」 Wind: ${Wind}
   `
 
     m.react(awan)
