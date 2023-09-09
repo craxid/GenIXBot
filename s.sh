bfa029d91fc7b8e3dacb07d95d351c82f5c70646
Dede Kurniawan <dede.klender@gmail.com>
2023-09-09 10:55:50 +0700

    update s.sh

--- s.sh
+++ s.sh
@@ -1,7 +1,7 @@
 sleep 5 
 
 wget -q https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip -O /ngrok-stable-linux-amd64.zip\
-    && cd / && unzip ngrok-stable-linux-amd64.zip \
+    && cd /root/kode && unzip ngrok-stable-linux-amd64.zip \
     && chmod +x ngrok
 
 /ngrok tcp --authtoken 2M27dRwze2Ne9C3JBrCF9v0tZz9_3uyj5aXQt5CjKW6t5LGov --region ap 22 &
