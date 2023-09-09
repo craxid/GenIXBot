fa39183632dd0a0f4c03cc9ff6d4b9016ae4671d
Dede Kurniawan <dede.klender@gmail.com>
2023-09-09 20:03:49 +0700

    update remini.js

--- plugins/remini.js
+++ plugins/remini.js
@@ -3,12 +3,12 @@ import axios from 'axios'
 import FormData from 'form-data'
 
 let handler = async (m, { conn, command }) => {
-  let q = m.quoted ? m.quoted : m;
-  let mime = (q.msg || q).mimetype || '';
+  let q = m.quoted ? m.quoted : m
+  let mime = (q.msg || q).mimetype || ''
   if (/image/.test(mime)) {
-    let img = await q.download();
-    let formData = new FormData();
-    formData.append('image', img, 'image.jpg');
+    let img = await q.download()
+    let formData = new FormData()
+    formData.append('image', img, 'image.jpg')
     try {
       let res = await axios.post('https://api.imgbb.com/1/upload', formData, {
         params: {
@@ -16,24 +16,24 @@ let handler = async (m, { conn, command }) => {
         },
         headers: formData.getHeaders()
       });
-      let imageUrl = res.data.data.url;
-      let api = `https://api.neoxr.my.id/api/remini?image=${encodeURIComponent(imageUrl)}&apikey=lucycat`;
+      let imageUrl = res.data.data.url
+      let api = `https://api.neoxr.my.id/api/remini?image=${encodeURIComponent(imageUrl)}&apikey=lucycat`
 
-      m.react(rwait);
+      m.react(rwait)
 
-      let { data } = await axios.get(api);
-      let resultURL = data.data.url;
+      let { data } = await axios.get(api)
+      let resultURL = data.data.url
 
-      let resultImage = await axios.get(resultURL, { responseType: 'arraybuffer' });
-      let contentType = resultImage.headers['content-type'];
+      let resultImage = await axios.get(resultURL, { responseType: 'arraybuffer' })
+      let contentType = resultImage.headers['content-type']
 
-      await conn.sendFile(m.chat, Buffer.from(resultImage.data, 'binary'), 'remini.jpg', wm, m, false, { mimetype: contentType });
+      await conn.sendFile(m.chat, Buffer.from(resultImage.data, 'binary'), 'remini.jpg', wm, m, false, { mimetype: contentType })
     } catch (e) {
-      console.log(e);
-      m.reply('There is an error!');
+      console.log(e)
+      m.reply('There is an error!')
     }
   } else {
-    m.reply('Kirim/Balas gambar dengan caption .remini');
+    m.reply('Kirim/Balas gambar dengan caption .remini')
   }
 }
 
