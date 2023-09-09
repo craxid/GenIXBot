5faa016417209616eeafc75b0df01734f6b357f2
Dede Kurniawan <dede.klender@gmail.com>
2023-09-09 20:00:07 +0700

    update remini.js

--- plugins/remini.js
+++ plugins/remini.js
@@ -1,13 +1,44 @@
-import fetch from 'node-fetch'
-let handler = async (m, { conn, args, usedPrefix, command }) => {
-    
-    if (!text) throw `✳️ This feature is under Development!`
-    m.react('🥰')
-}
-handler.help = ['enchant <image>']
-handler.tags = ['img']
-handler.command = ['enchant','remini','hd','tohd']
-handler.premium = false
-handler.diamond = true
+const axios = require('axios');
+const FormData = require('form-data');
 
-export default handler
+let handler = async (m, { conn, command }) => {
+  let q = m.quoted ? m.quoted : m;
+  let mime = (q.msg || q).mimetype || '';
+  if (/image/.test(mime)) {
+    let img = await q.download();
+    let formData = new FormData();
+    formData.append('image', img, 'image.jpg');
+    try {
+      let res = await axios.post('https://api.imgbb.com/1/upload', formData, {
+        params: {
+          key: '32dccc1da5b49fc9ed09fd6d9e691d40'
+        },
+        headers: formData.getHeaders()
+      });
+      let imageUrl = res.data.data.url;
+      let api = `https://api.neoxr.my.id/api/remini?image=${encodeURIComponent(imageUrl)}&apikey=lucycat`;
+
+      m.reply(wait);
+
+      let { data } = await axios.get(api);
+      let resultURL = data.data.url;
+
+      let resultImage = await axios.get(resultURL, { responseType: 'arraybuffer' });
+      let contentType = resultImage.headers['content-type'];
+
+      await conn.sendFile(m.chat, Buffer.from(resultImage.data, 'binary'), 'remini.jpg', wm, m, false, { mimetype: contentType });
+    } catch (e) {
+      console.log(e);
+      m.reply('There is an error!');
+    }
+  } else {
+    m.reply('Kirim/Balas gambar dengan caption .remini');
+  }
+}
+
+handler.command = /^remini$/i;
+handler.tags = ['tools'];
+handler.help = ['remini'];
+handler.premium = false;
+
+modhandler;
