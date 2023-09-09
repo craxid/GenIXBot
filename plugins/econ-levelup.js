ef108a6d234b9fce71bb06af1078d0c584e0f150
Dede Kurniawan <dede.klender@gmail.com>
2023-09-09 22:31:52 +0700

    update econ-levelup.js

--- plugins/econ-levelup.js
+++ plugins/econ-levelup.js
@@ -1,5 +1,5 @@
 
-import { canLevelUp, xpRange } from '../lib/levelling.js'
+/*import { canLevelUp, xpRange } from '../lib/levelling.js'
 let handler = async (m, { conn }) => {
 	  let name = conn.getName(m.sender)
     let pp = await conn.profilePictureUrl(m.sender, 'image').catch(_ => 'https://i.ibb.co/1ZxrXKJ/avatar-contact.jpg')
@@ -60,5 +60,48 @@ try {
 handler.help = ['levelup']
 handler.tags = ['econ']
 handler.command = ['tingkat', 'lvlup', 'levelup', 'level'] 
+
+export default handler
+*/
+
+import { canLevelUp, xpRange } from '../lib/levelling.js'
+import { levelup } from '../lib/canvas.js'
+import db from '../lib/database.js'
+
+let handler = async (m, { conn }) => {
+    let user = db.data.users[m.sender]
+    if (!canLevelUp(user.level, user.exp, global.multiplier)) {
+        let { min, xp, max } = xpRange(user.level, global.multiplier)
+        throw `
+Level *${user.level} (${user.exp - min}/${xp})*
+Kurang *${max - user.exp}* lagi!
+`.trim()
+    }
+    let before = user.level * 1
+    while (canLevelUp(user.level, user.exp, global.multiplier)) user.level++
+    if (before !== user.level) {
+        user.role = global.rpg.role(user.level).name
+        let teks = `Selamat ${conn.getName(m.sender)} naik 🧬level`
+        let str = `
+${teks} 
+• 🧬Level Sebelumnya : ${before}
+• 🧬Level Baru : ${user.level}
+• 🧬Role Kamu : ${user.role}
+• Pada Jam : ${new Date().toLocaleString('id-ID')}
+*_Semakin sering berinteraksi dengan bot Semakin Tinggi level kamu_*
+`.trim()
+        try {
+            const img = await levelup(m, user.level)
+            conn.sendFile(m.chat, img, 'levelup.jpg', str, m)
+        } catch (e) {
+            m.reply(str)
+        }
+    }
+}
+
+handler.help = ['levelup']
+handler.tags = ['xp']
+
+handler.command = /^level(|up)$/i
 
 export default handler
