07ccd9f8c97073bc47d35e8bbbc303f9c0e984c5
Dede Kurniawan <dede.klender@gmail.com>
2023-09-09 20:59:38 +0700

    update qr.js

--- plugins/qr.js
+++ plugins/qr.js
@@ -1,6 +1,7 @@
 let qrcode = require("qrcode")
 let handler  = async (m, { conn, text }) => {
   if (!text) throw 'Masukkan Text'
+  m.react('rwait')
   conn.sendFile(m.chat, await qrcode.toDataURL(text.slice(0, 2048), { scale: 8 }), 'qrcode.png', '2023 © Mika Bot', m)
 }
 
