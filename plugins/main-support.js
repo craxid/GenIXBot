337c1a7f2f5894e3b00d2ec533637e76bec6eca7
Dede Kurniawan <dede.klender@gmail.com>
2023-09-10 12:32:31 +0700

    update main-support.js

--- plugins/main-support.js
+++ plugins/main-support.js
@@ -12,12 +12,14 @@ https://chat.whatsapp.com/BDYIsBhoR2I2aXpEvZRzWP
 
 Terima Kasih kepada:
 
+- Tuhan Yang Maha Esa
 - WhatsApp Business
 - Library Baileys
 - Penyedia API
 - Plugin Base DannTeam
-- Teman-teman yang ba
+- Teman-teman yang udah mau bantu ngetes
 
+_Project 
 }
 handler.help = ['support']
 handler.tags = ['main']
