bc587677c06360d0c34c3b2c3f59a94e2b98037c
Dede Kurniawan <dede.klender@gmail.com>
2023-09-10 18:33:53 +0700

    update handler.js

--- handler.js
+++ handler.js
@@ -585,8 +585,7 @@ global.dfail = (type, m, conn) => {
         unreg: '📇 Fitur ini untuk pengguna terdaftar\n\n Ketik: *.register nama.umur*\n\n📌 Contoh : *.reg Mika.16*',
         restrict: '🔐 Fitur ini *dinonaktifkan*'
     }[type]
-    if (msg) return m.reply(msg)
-    conn.sendMessage(m.chat, {
+    if (msg) return conn.sendMessage(m.chat, {
 text: msg,
 contextInfo: {
 externalAdReply: {
