5f9b78ada5bf61411cc49ae7b0a4d3afcf5b2db2
Dede Kurniawan <dede.klender@gmail.com>
2023-09-10 15:14:12 +0700

    update asahotak.js

--- plugins/asahotak.js
+++ plugins/asahotak.js
@@ -1,28 +1,40 @@
 
 import fetch from 'node-fetch'
 
-let handler = async (m, { conn, args, usedPrefix, command }) => {
-    
-    if (asahotak.hasOwnProperty(m.sender.split('@')[0])) return m.reply("Masih Ada Sesi Yang Belum Diselesaikan!")
-        let fetch = await fetchUrl(global.api("zenz", "/entertainment/asahotak", {}, "apikey"))
-        let result = await fetch.result
-        killua.sendText(m.from, `Silahkan Jawab Pertanyaan Berikut\n\n${result.soal}\n\nWaktu : 60s detik`, m).then(() => {
-            asahotak[m.sender.split('@')[0]] = result.jawaban.toLowerCase()
-            console.log("Jawaban: " + result.jawaban)
-        })
-        await sleep(60000)
-        if (asahotak.hasOwnProperty(m.sender.split('@')[0])) {
-            killua.sendText(m.from, `Waktu Habis\n\nJawaban:  ${asahotak[m.sender.split('@')[0]]}`, m)
-            delete asahotak[m.sender.split('@')[0]]
-        }
-    
-handler.help = ['asahotak']
-handler.tags = ['fun']
-handler.command = ['asahotak'] 
+let timeout = 180000
+let poin = 500
+let tiketcoin = 1
+let handler = async (m, { conn, usedPrefix }) => {
+    conn.asahotak = conn.asahotak ? conn.asahotak : {}
+    let id = m.chat
+    if (id in conn.asahotak) {
+        conn.reply(m.chat, 'Masih ada soal belum terjawab di chat ini', conn.asahotak[id][0])
+        throw false
+    }
+let src = await (await fetch('https://raw.githubusercontent.com/BochilTeam/database/master/games/asahotak.json')).json()
+    let json = src[Math.floor(Math.random() * src.length)]
+    let caption = `
+${json.soal}
 
-export default handler
-
-    
+Waktu *${(timeout / 1000).toFixed(2)} detik*
+Ketik ${usedPrefix}ao untuk bantuan
+Bonus: ${poin} XP
+Tiketcoin: ${tiketcoin} TiketCoin
+`.trim()
+    conn.asahotak[id] = [
+        await conn.reply(m.chat, caption, m),
+        json, poin,
+        setTimeout(() => {
+            if (conn.asahotak[id]) conn.reply(m.chat, `Waktu habis!\nJawabannya adalah *${json.jawaban}*`, conn.asahotak[id][0])
+            delete conn.asahotak[id]
+        }, timeout)
+    ]
 }
+handler.help = ['asahotak']
+handler.tags = ['game']
+handler.command = /^asahotak/i
+handler.limit = true
+handler.group = true
+handler.register = true
 
-/*masih males*/
+module.exports = handler
