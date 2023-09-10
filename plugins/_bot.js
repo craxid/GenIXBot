6858818863c644e4afba9a3eaf50681af5595020
Dede Kurniawan <dede.klender@gmail.com>
2023-09-10 13:41:20 +0700

    update _bot.js

---
+++ plugins/_bot.js
@@ -0,0 +1,21 @@
+let handler = async (m, { conn }) => {
+let name = await conn.getName(m.sender)
+let thumb = 'https://telegra.ph/file/bd044275940ed1b62efcd.jpg'
+let dann = `Hai ${name} 👋\n\nAda yang bisa ${namebot} bantu?\n\nKetik !help untuk membuka Menu`
+conn.sendMessage(m.chat, {
+text: dann,
+contextInfo: {
+externalAdReply: {
+title: namebot,
+body: wm,
+thumbnailUrl: thumb,
+sourceUrl: sig,
+mediaType: 1,
+showAdAttribution: true,
+renderLargerThumbnail: false
+}}})
+}
+handler.customPrefix = /^(bot|bot?|bott|mika|oy|bang)$/i
+handler.command = new RegExp
+
+module.exports = handler
