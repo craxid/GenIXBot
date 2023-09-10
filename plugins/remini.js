10fc90f669c1902723b9402c3afcb7eb0a1f1782
Dede Kurniawan <dede.klender@gmail.com>
2023-09-10 21:03:57 +0700

    update remini.js

--- plugins/remini.js
+++ plugins/remini.js
@@ -3,43 +3,46 @@ import axios from 'axios'
 import FormData from 'form-data'
 
 let handler = async (m, { conn, command }) => {
-  let q = m.quoted ? m.quoted : m;
-  let mime = (q.msg || q).mimetype || '';
+  let q = m.quoted ? m.quoted : m
+  let mime = (q.msg || q).mimetype || ''
   if (/image/.test(mime)) {
-    let img = await q.download();
-    let formData = new FormData();
-    formData.append('image', img, 'image.jpg');
+    let img = await q.download()
+    let formData = new FormData()
+    formData.append('image', img, 'image.jpg')
     try {
       let res = await axios.post('https://api.imgbb.com/1/upload', formData, {
         params: {
           key: '32dccc1da5b49fc9ed09fd6d9e691d40'
         },
         headers: formData.getHeaders()
-      });
-      let imageUrl = res.data.data.url;
-      let api = `https://api.neoxr.my.id/api/remini?image=${encodeURIComponent(imageUrl)}&apikey=lucycat`;
+      })
+      let imageUrl = res.data.data.url
+      
+      let api = `https://api.neoxr.my.id/api/remini?image=${encodeURIComponent(imageUrl)}&apikey=lucycat`
+      
+      //let api = `https://api.neoxr.my.id/api/remini?image=${encodeURIComponent(imageUrl)}&apikey=lucycat`
 
-      m.reply(wait);
+      m.reply(wait)
 
-      let { data } = await axios.get(api);
-      let resultURL = data.data.url;
+      let { data } = await axios.get(api)
+      let resultURL = data.data.url
 
-      let resultImage = await axios.get(resultURL, { responseType: 'arraybuffer' });
-      let contentType = resultImage.headers['content-type'];
+      let resultImage = await axios.get(resultURL, { responseType: 'arraybuffer' })
+      let contentType = resultImage.headers['content-type']
 
-      await conn.sendFile(m.chat, Buffer.from(resultImage.data, 'binary'), 'remini.jpg', wm, m, false, { mimetype: contentType });
+      await conn.sendFile(m.chat, Buffer.from(resultImage.data, 'binary'), 'remini.jpg', wm, m, false, { mimetype: contentType })
     } catch (e) {
-      console.log(e);
-      m.reply('There is an error!');
+      console.log(e)
+      m.reply('There is an error!')
     }
   } else {
-    m.reply('Kirim/Balas gambar dengan caption .remini');
+    m.reply('Kirim/Balas gambar dengan caption .remini')
   }
 }
 
-handler.command = /^remini$/i;
-handler.tags = ['tools'];
-handler.help = ['remini'];
-handler.premium = false;
+handler.command = /^remini$/i
+handler.tags = ['tools']
+handler.help = ['remini']
+handler.premium = false
 
-module.exports = handler;
+module.exports = handler
