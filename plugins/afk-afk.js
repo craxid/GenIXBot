9aab0b3de4b373faeef80007470a6e6dcf935c8d
Dede Kurniawan <dede.klender@gmail.com>
2023-09-10 14:26:16 +0700

    update afk-afk.js

--- plugins/afk-afk.js
+++ plugins/afk-afk.js
@@ -10,6 +10,20 @@ Sekarang Kamu AFK sampai Kamu mengirim pesan
 ▢ *User:* ${conn.getName(m.sender)} 
 ▢ *Alasan:* ${text ? text : ''}
   `)
+  
+conn.sendMessage(m.chat, {
+text: mika,
+contextInfo: {
+externalAdReply: {
+title: ('Mika Misono Bot'),
+body: ('Jangan Spam Bot!'),
+thumbnailUrl: thumb,
+sourceUrl: global.dygp,
+mediaType: 1,
+showAdAttribution: true,
+renderLargerThumbnail: true
+}}})
+  
 }
 handler.help = ['afk <Alasan>']
 handler.tags = ['fun']
