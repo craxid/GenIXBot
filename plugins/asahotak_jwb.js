153e3263adfafc146869585d9685f50086daea30
Dede Kurniawan <dede.klender@gmail.com>
2023-09-10 21:39:18 +0700

    update asahotak_jwb.js

--- plugins/asahotak_jwb.js
+++ plugins/asahotak_jwb.js
@@ -13,7 +13,7 @@ handler.before = async function (m) {
         if (m.text.toLowerCase() == json.jawaban.toLowerCase().trim()) {
             global.db.data.users[m.sender].exp += this.asahotak[id][2]
             global.db.data.users[m.sender].tiketcoin += 1
-            m.reply(`*\nJawaban Benar 🎉*\nKamu mendapat +${this.asahotak[id][2]} XP\n`)
+            m.reply(`\n*Jawaban Benar 🎉*\nKamu mendapat +${this.asahotak[id][2]} XP\n`)
             clearTimeout(this.asahotak[id][3])
             delete this.asahotak[id]
         } else if (similarity(m.text.toLowerCase(), json.jawaban.toLowerCase().trim()) >= threshold) m.reply(`*Dikit Lagi!*`)
