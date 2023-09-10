75343a1c57d9d44eef85d691aa3a7395799af9db
Dede Kurniawan <dede.klender@gmail.com>
2023-09-10 13:56:24 +0700

    update _bot.js

--- plugins/_bot.js
+++ plugins/_bot.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn }) => {
 let name = await conn.getName(m.sender)
 let thumb = 'https://telegra.ph/file/bd044275940ed1b62efcd.jpg'
-let dann = `Hai ${name} 👋\n\nAda yang bisa ${global.namebot} bantu?\n\nKetik !help untuk membuka Menu`
+let dann = `Halo ${name} 👋\n\nAda yang bisa ${global.packname} bantu?\n\nKetik !help untuk membuka Menu`
 conn.sendMessage(m.chat, {
 text: dann,
 contextInfo: {
