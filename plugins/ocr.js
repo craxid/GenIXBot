d10f9a0d4109d9e6e82d6ff32f39b8f8ffc2bc8b
Dede Kurniawan <dede.klender@gmail.com>
2023-09-10 19:24:04 +0700

    update ocr.js

--- plugins/ocr.js
+++ plugins/ocr.js
@@ -1,9 +1,9 @@
 import uploadImage from '../lib/uploadImage'
-import ocrapi = from "ocr-space-api-wrapper"
+import ocrapi from "ocr-space-api-wrapper"
 import { MessageType } from '@adiwajshing/baileys'
 
 let handler = async (m, { conn, text }) => {
-      let q = m.quoted ? m.quoted : m
+    let q = m.quoted ? m.quoted : m
     let mime = (q.msg || q).mimetype || ''
     if (!mime) throw `balas gambar dengan perintah .ocr`
     if (!/image\/(jpe?g|png)/.test(mime)) throw `_*jenis ${mime} tidak didukung!*_`
