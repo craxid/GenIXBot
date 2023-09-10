44573163b62c2ede7ec90f7027cc62adf023f7cb
Dede Kurniawan <dede.klender@gmail.com>
2023-09-10 13:24:56 +0700

    update cuaca.js

--- plugins/cuaca.js
+++ plugins/cuaca.js
@@ -20,7 +20,7 @@ let handler = async (m, { args }) => {
     let bmkglogo = 'https://telegra.ph/file/5e539e4e1194c057c1fc7.jpg'
     
     await m.react(awan)
-    m.reply(wea, bmkglogo)
+    conn.senFile(wea, bmkglogo)
   } catch (e) {
     return "Error: Lokasi tidak ditemukan!"
   }
