3ef76b4a87103ad46c7ea1d89ebd65f581bcda49
Dede Kurniawan <dede.klender@gmail.com>
2023-09-11 08:05:58 +0700

    update txt2img.js

--- plugins/txt2img.js
+++ plugins/txt2img.js
@@ -3,7 +3,7 @@ import fetch from 'node-fetch'
 let handler = async (m, { conn, text }) => {
 	
 if (!text) throw `✳️ Membuat Gambar AI menggunakan perintah yang diberikan pengguna\n\nContoh: !txt2img 1girls`
-m.react('💬')
+m.reply(wait)
 
 //code area
 try {
