6738e6bc3ce3226355ee241e89ee829d0d3329b4
Dede Kurniawan <dede.klender@gmail.com>
2023-09-11 08:03:20 +0700

    update animedif.js

--- plugins/animedif.js
+++ plugins/animedif.js
@@ -3,7 +3,7 @@ import fetch from 'node-fetch'
 let handler = async (m, { conn, text }) => {
 	
 if (!text) throw `✳️ Membuat Gambar AI menggunakan perintah yang diberikan pengguna\n\nContoh: !animedif 1girls`
-m.react('💬')
+m.reply(wait)
 
 //code area
 try {
