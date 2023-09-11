5a8dcdb0f1b837d2b18489e720ed78ce6ee544bc
Dede Kurniawan <dede.klender@gmail.com>
2023-09-11 08:28:06 +0700

    update txt2img.js

--- plugins/txt2img.js
+++ plugins/txt2img.js
@@ -7,7 +7,9 @@ m.reply(wait)
 
 //code area
 try {
-let diff = await conn.getFile(`https://api.azz.biz.id/api/text2img?q=${text}&key=mangea`)
+    diff = await conn.getFile(`https://api.azz.biz.id/api/text2img?q=${text}&key=mangea`)
+    1girl,%20hoodie,%20smile
+//let diff = await conn.getFile(`https://api.azz.biz.id/api/text2img?q=${text}&key=mangea`)
 	    
 conn.sendFile(m.chat, diff.data, 'img.jpg', `*[ TEXT TO IMAGE ]*\n\nPROMPT:\n${text}`, m)
 	}
