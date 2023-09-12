eb908a4c9ecd74f9e25a08475f62ece13315609e
Dede Kurniawan <dede.klender@gmail.com>
2023-09-12 20:48:03 +0700

    update econ-levelup.js

--- plugins/econ-levelup.js
+++ plugins/econ-levelup.js
@@ -84,6 +84,8 @@ Kurang *${max - user.exp}XP* lagi!
     if (before !== user.level) {
         user.role = global.rpg.role(user.level).name
         
+    let img = ('${glob')
+    
         let teks = `Selamat, ${conn.getName(m.sender)} naik level 🎉`
         let str = `
 ${teks} 
