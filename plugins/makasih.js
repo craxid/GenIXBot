79618508a7f27cdc56d896432c94ae001ec726c2
Dede Kurniawan <dede.klender@gmail.com>
2023-09-12 20:44:12 +0700

    update makasih.js

--- plugins/makasih.js
+++ plugins/makasih.js
@@ -3,8 +3,9 @@ let name = await conn.getName(m.sender)
 let thumb = 'https://telegra.ph/file/bd044275940ed1b62efcd.jpg'
 let mikapp = 'https://telegra.ph/file/a9a78b769836fe10fa62d.jpg'
 
-let mika = `Sama-sama kak ${name}`
-conn.sendMessage(m.chat, {
+m.reply(`Sama-sama kak ${name}`)
+
+/*conn.sendMessage(m.chat, {
 text: mika,
 contextInfo: {
 externalAdReply: {
@@ -15,7 +16,7 @@ sourceUrl: ('https://www.facebook.com/dede2015k'),
 mediaType: 1,
 showAdAttribution: true,
 renderLargerThumbnail: true
-}}})
+}}})*/
 }
 handler.customPrefix = /^(makasih|terimakasih|thx|thanks|thank you)$/i
 handler.command = new RegExp
