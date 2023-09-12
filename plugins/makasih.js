54fd4bea516a4e7c0a888262308ff2ef34a4ac10
Dede Kurniawan <dede.klender@gmail.com>
2023-09-12 20:42:37 +0700

    update makasih.js

--- plugins/makasih.js
+++ plugins/makasih.js
@@ -17,7 +17,7 @@ showAdAttribution: true,
 renderLargerThumbnail: true
 }}})
 }
-handler.customPrefix = /^(makasih|terimakasih|thx|than)$/i
+handler.customPrefix = /^(makasih|terimakasih|thx|thanks|thank you)$/i
 handler.command = new RegExp
 
 export default handler
