7379b7dbf62c6c91bd2ba93da495b93a72c645bc
Dede Kurniawan <dede.klender@gmail.com>
2023-09-13 01:19:09 +0700

    update _bot.js

--- plugins/_bot.js
+++ plugins/_bot.js
@@ -5,16 +5,14 @@ let mikapp = 'https://telegra.ph/file/a9a78b769836fe10fa62d.jpg'
 
 let mika = `Halo kak ${name} 👋\nAda yang bisa ${global.packname} bantu?\n\nKetik !help untuk membuka Menu`
 
-let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-if (!(who in global.db.data.users)) throw `✳️ Pengguna tidak ada di database`
+
     let pp = await conn.profilePictureUrl(who, 'image').catch(_ => './src/avatar_contact.png')
-    let username = conn.getName(who)
     
 conn.sendMessage(m.chat, {
 text: mika,
 contextInfo: {
 externalAdReply: {
-title: (`${username}`),
+title: (`${name}`),
 body: ('Aku Online Kak!'),
 thumbnailUrl: mikapp,
 sourceUrl: ('https://www.facebook.com/dede2015k'),
