1a8b69bf980ef507d95b6fbe2dd71e6752bd7ce5
Dede Kurniawan <dede.klender@gmail.com>
2023-09-13 13:26:44 +0700

    update main-script.js

--- plugins/main-script.js
+++ plugins/main-script.js
@@ -11,7 +11,7 @@ let _package = JSON.parse(await promises.readFile(join(__dirname, '../package.js
 `.trim())*/
 
 let sc = (`
-Mau script? Silahkan pesan ke owner\n\nKetik .owner
+Mau script bot? Silahkan pesan ke owner\n\nKetik .owner
 `)
 
 conn.sendMessage(m.chat, {
