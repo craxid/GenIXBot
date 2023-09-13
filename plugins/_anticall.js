15e0884d1de0a2451c780c98f3fd2f36a98dfaa2
Dede Kurniawan <dede.klender@gmail.com>
2023-09-13 22:19:13 +0700

    update _anticall.js

---
+++ plugins/_anticall.js
@@ -0,0 +1,25 @@
+const { WAMessageStubType } = require('@adiwajshing/baileys')
+var { format } = require('util');
+
+let handler = m => m
+
+const isNumber = x => typeof x === 'number' && !isNaN(x)
+const delay = ms => isNumber(ms) && new Promise(resolve => setTimeout(function () {
+    clearTimeout(this)
+    resolve()
+}, ms))
+
+handler.all = async function (m) {
+	if (m.fromMe && m.isBaileys) return !0
+	let text;
+	let setting = global.db.data.settings[this.user.jid]
+	if(!setting.anticall) return 
+	
+	if (m.messageStubType === (WAMessageStubType.CALL_MISSED_VOICE || WAMessageStubType.CALL_MISSED_VIDEO)) {
+		await this.reply(m.chat, 'Kamu telah di blokir!\nKarena melanggar aturan bot', null)
+		await delay(1000)
+		await this.updateBlockStatus(m.chat, "block")
+	}
+}
+
+export default handler
