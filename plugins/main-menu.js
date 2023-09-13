e6bf33f47a0b17c4bf59ba1852c48c97083c5b40
Dede Kurniawan <dede.klender@gmail.com>
2023-09-14 06:27:02 +0700

    update main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -138,6 +138,7 @@ let handler = async (m, { conn, usedPrefix: _p, __dirname }) => {
       '%': '%',
       p: _p, uptime, muptime,
       me: conn.getName(conn.user.jid),
+      ucapan: ucapan(),
       npmname: _package.name,
       npmdesc: _package.description,
       version: _package.version,
