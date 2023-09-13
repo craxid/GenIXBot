8a925806bcf229cfd210a8abaed5dd59a6458064
Dede Kurniawan <dede.klender@gmail.com>
2023-09-13 22:23:13 +0700

    update _antispam.js

---
+++ plugins/_antispam.js
@@ -0,0 +1,26 @@
+let handler = m => m
+
+handler.all = async function (m) {
+    if (!m.message)
+        return
+    this.spam = this.spam ? this.spam : {}
+    if (m.sender in this.spam) {
+        this.spam[m.sender].count++
+        if (m.messageTimestamp.toNumber() - this.spam[m.sender].lastspam > 10) {
+            if (this.spam[m.sender].count > 10) {
+                //global.db.data.users[m.sender].banned = true
+                m.reply('Jangan spam, beri jeda 5 detik!')
+            }
+            this.spam[m.sender].count = 0
+            this.spam[m.sender].lastspam = m.messageTimestamp.toNumber()
+        }
+    }
+    else
+        this.spam[m.sender] = {
+            jid: m.sender,
+            count: 0,
+            lastspam: 0
+        }
+}
+
+export default handler
