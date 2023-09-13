fb1b3f744ac155867c51a1c2ed2607fd9ced00fa
Dede Kurniawan <dede.klender@gmail.com>
2023-09-13 13:20:48 +0700

    update main-script.js

--- plugins/main-script.js
+++ plugins/main-script.js
@@ -4,11 +4,13 @@ import { join } from 'path'
 let handler = async function (m, { conn, __dirname }) {
 let _package = JSON.parse(await promises.readFile(join(__dirname, '../package.json')).catch(_ => ({}))) || {}
   
-m.reply(`
+/*m.reply(`
 *≡ SCRIPT*
 
 ▢ Git : ${_package.homepage}
-`.trim())
+`.trim())*/
+
+let sc = 
     
 }
 
