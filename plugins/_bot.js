0eaad06118356d7d86554dbfcf694f6ecc9db096
Dede Kurniawan <dede.klender@gmail.com>
2023-09-14 06:45:25 +0700

    update _bot.js

--- plugins/_bot.js
+++ plugins/_bot.js
@@ -1,30 +1,9 @@
-
-import moment from 'moment-timezone'
 let handler = async (m, { conn }) => {
 let name = await conn.getName(m.sender)
 let thumb = 'https://telegra.ph/file/bd044275940ed1b62efcd.jpg'
 let mikapp = 'https://telegra.ph/file/a9a78b769836fe10fa62d.jpg'
 
-let replace = {
-      '%': '%',
-      p: _p, uptime, muptime,
-      me: conn.getName(conn.user.jid),
-      ucapan: ucapan(),
-      npmname: package.name,
-      npmdesc: package.description,
-      version: package.version,
-      exp: exp - min,
-      maxexp: xp,
-      totalexp: exp,
-      xp4levelup: max - exp,
-      github: package.homepage ? package.homepage.url || package.homepage : '[unknown github url]',
-      level, limit, name, weton, week, date, dateIslamic, dateCountdown, wib, wit, wita, time, totalmem, totalreg, rtotalreg, platformInfo, freemem, infomem, role,
-      readmore: readMore
-    }
-
-let mika = `
-%ucapan ${name} 👋\nAda yang bisa ${global.packname} bantu?\n\nKetik !help untuk membuka Menu
-`
+let mika = `Halo kak ${name} 👋\nAda yang bisa ${global.packname} bantu?\n\nKetik !help untuk membuka Menu`
 
 let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 
@@ -48,21 +27,3 @@ handler.customPrefix = /^(bot|bot?|bott|mika|oy|bang|banh)$/i
 handler.command = new RegExp
 
 export default handler
-
-function ucapan() {
-  const time = moment.tz('Asia/Jakarta').format('HH')
-  let res = "Dini kak"
-  if (time >= 4) {
-    res = "Pagi kak"
-  }
-  if (time >= 10) {
-    res = "Siang kak️"
-  }
-  if (time >= 15) {
-    res = "Sore kak"
-  }
-  if (time >= 18) {
-    res = "Malam kak"
-  }
-  return res
-}
