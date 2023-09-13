fefa586f575126f62ae8c859c4acd4dc8d0bdd4c
Dede Kurniawan <dede.klender@gmail.com>
2023-09-14 06:44:15 +0700

    update _bot.js

--- plugins/_bot.js
+++ plugins/_bot.js
@@ -5,7 +5,9 @@ let name = await conn.getName(m.sender)
 let thumb = 'https://telegra.ph/file/bd044275940ed1b62efcd.jpg'
 let mikapp = 'https://telegra.ph/file/a9a78b769836fe10fa62d.jpg'
 
-let mika = `%ucapan ${name} 👋\nAda yang bisa ${global.packname} bantu?\n\nKetik !help untuk membuka Menu`
+let mika = `
+%ucapan ${name} 👋\nAda yang bisa ${global.packname} bantu?\n\nKetik !help untuk membuka Menu
+`
 
 let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 
