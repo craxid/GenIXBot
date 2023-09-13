a5954df46edc7042fb7ae4de27abadfa48467d6f
Dede Kurniawan <dede.klender@gmail.com>
2023-09-14 06:20:45 +0700

    update owner-broadcast.js

--- plugins/owner-broadcast.js
+++ plugins/owner-broadcast.js
@@ -3,10 +3,14 @@ import { randomBytes } from 'crypto'
 
 let handler = async (m, { conn, text }) => {
   let chats = Object.entries(conn.chats).filter(([_, chat]) => chat.isChats).map(v => v[0])
+  
   let cc = conn.serializeM(text ? m : m.quoted ? await m.getQuotedObj() : false || m)
+  
   let teks = text ? text : cc.text
   conn.reply(m.chat, `✅ BROADCAST BERHASIL DIKIRIM *Total:* ${chats.length} chats`, m)
+  
   for (let id of chats) await conn.copyNForward(id, conn.cMod(m.chat, cc, /bc|broadcast|tx/i.test(teks) ? teks : `*BROADCAST ┃ STAFF*\n_____________________\n${teks} ` ), true).catch(_ => _)
+  
   m.reply('✅ PESAN SUDAH DITERUSKAN KE SEMUA CHAT :)')
 }
 handler.help = ['broadcast']
