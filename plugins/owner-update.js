111857562267eedcc21b41451845a2c7697a64e2
Dede Kurniawan <dede.klender@gmail.com>
2023-09-13 15:27:30 +0700

    update owner-update.js

--- plugins/owner-update.js
+++ plugins/owner-update.js
@@ -1,6 +1,6 @@
 //import db from '../lib/database.js'
 
-import { execSync } from 'child_process'
+/*import { execSync } from 'child_process'
 let handler = async (m, { conn, text }) => {
 if (conn.user.jid == conn.user.jid) {
 let stdout = execSync('git pull' + (m.fromMe && text ? ' ' + text : ''))
@@ -13,3 +13,30 @@ handler.command = ['update', 'perbarui', 'fix', 'fixed']
 handler.rowner = true
 
 export default handler
+*/
+
+import { execSync } from 'child_process'
+
+let handler = async (m, { conn, text, isROwner }) => {
+  if (global.conn.user.jid == conn.user.jid) {
+    let stdout = execSync('git remote set-url origin https://github.com/craxid/mikabot.git && git pull' + (isROwner && text ? ' ' + text : ''))
+    if (isROwner) require('fs').readdirSync('plugins').map(v => global.reload('', v))
+    m.reply(stdout.toString())
+  }
+}
+handler.help = ['update']
+handler.tags = ['owner']
+handler.command = /^update$/i
+handler.owner = true
+handler.mods = false
+handler.premium = false
+handler.group = false
+handler.private = false
+
+handler.admin = false
+handler.botAdmin = false
+
+handler.fail = null
+handler.exp = 0
+
+module.exports = handler
