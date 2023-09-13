9f8d826fb102c2d9f68157fa3292ac4d618da706
Dede Kurniawan <dede.klender@gmail.com>
2023-09-13 15:27:43 +0700

    update owner-update.js

--- plugins/owner-update.js
+++ plugins/owner-update.js
@@ -19,7 +19,7 @@ import { execSync } from 'child_process'
 
 let handler = async (m, { conn, text, isROwner }) => {
   if (global.conn.user.jid == conn.user.jid) {
-    let stdout = execSync('git remote set-url origin https://github.com/craxid/mikabot.git && git pull' + (isROwner && text ? ' ' + text : ''))
+    let stdout = execSync('git remote set-url origin https://github.com/craxix/genixbot.git && git pull' + (isROwner && text ? ' ' + text : ''))
     if (isROwner) require('fs').readdirSync('plugins').map(v => global.reload('', v))
     m.reply(stdout.toString())
   }
