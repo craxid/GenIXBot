9ac2f66eb4f7fc22dba34cd5b0d3088dcd60994b
Dede Kurniawan <dede.klender@gmail.com>
2023-09-14 19:12:28 +0700

    update dl-instagram.js

--- plugins/dl-instagram.js
+++ plugins/dl-instagram.js
@@ -22,7 +22,7 @@ import fetch from "node-fetch"
 
 var handler = async (m, { conn, text, usedPrefix, command }) => {
   if (!text) throw `Masukkan URL!\n\nContoh: ${usedPrefix + command}`
-  let dann = await fetch(`https://api.lolhuman.xyz/api/instagram?apikey=gunturganteng&url=${text}`)
+  let dann = await fetch(`https://api.arifzyn.biz.id/api/download/instagram?url=${text}`)
   let res = await dann.json()
   conn.sendFile(m.chat, res.result, 'ig.mp4', 'Nih kak', m)
 }
