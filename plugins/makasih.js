039fa9eab09baf365c54ac018c8d0e99ee72fa9d
Dede Kurniawan <dede.klender@gmail.com>
2023-09-14 20:06:16 +0700

    update makasih.js

--- plugins/makasih.js
+++ plugins/makasih.js
@@ -3,8 +3,8 @@ let name = await conn.getName(m.sender)
 let thumb = 'https://telegra.ph/file/bd044275940ed1b62efcd.jpg'
 let mikapp = 'https://telegra.ph/file/a9a78b769836fe10fa62d.jpg'
 
-m.reply(`Sama-sama kak ${name}`)
 
+let teks(`Sama-sama kak ${name}`)
 
 let iyh(m.chat, {
 text: mika,
@@ -18,6 +18,8 @@ mediaType: 1,
 showAdAttribution: true,
 renderLargerThumbnail: true
 }}})
+
+m.reply(iyh)
 }
 handler.customPrefix = /^(makasih|terima kasih|thx|thanks|tengs|thank you|arigato|arigatou|arigathanks)$/i
 handler.command = new RegExp
