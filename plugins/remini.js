b6ff51ca4c08db87c75716c57a9cf5be7c865d43
Dede Kurniawan <dede.klender@gmail.com>
2023-09-15 13:01:52 +0700

    update remini.js

--- plugins/remini.js
+++ plugins/remini.js
@@ -48,3 +48,32 @@ handler.premium = false
 export default handler
 */
 
+const { TelegraPh } = require('../lib/telegraph')
+const { fetchJson } = require("../lib/function")    
+ 
+module.exports = {
+	order: ['enhance','hd','remini'],
+	exec: async (msg, client, from, {
+		q,
+		args,
+		order,
+		prefix
+	}) => {
+	try{
+     if(!msg.fromMe) return 		
+	 if (!msg.isQuotedImage) return msg.reply('Reply image')
+	 msg.reply('_Tunggu sebentar, sedang proses..._')
+     let media = await client.downloadAndSaveMediaMessage(msg.message.extendedTextMessage.contextInfo.quotedMessage.imageMessage, 'image')
+     let tph = await TelegraPh(media)
+     let enhanced = await fetchJson("https://api.yanzbotz.my.id/api/tools/enhance?url=" + tph )
+     try{
+        client.sendMessage(from, { document : { url : enhanced.result.url }, fileName : 'Result.jpg', mimetype: 'image/jpeg' }, { quoted : msg})
+     } catch {
+       msg.reply("Sending image error!\ntrying to send in document form..... ")
+       client.sendMessage(from, { document: enhanced.url, fileName: ranJ, mimetype: 'image/jpeg', caption: 'Result' }, { quoted: msg });
+     }
+    } catch (e) {
+      msg.reply("!Type error:\n" + e)
+    }
+	}
+}
