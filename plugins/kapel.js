9e492b461d5955666cab83bfc20e4ebb486e1492
Dede Kurniawan <dede.klender@gmail.com>
2023-09-16 06:40:43 +0700

    update kapel.js

--- plugins/kapel.js
+++ plugins/kapel.js
@@ -34,6 +34,3 @@ export default handler
 function pickRandom(list) {
   return list[Math.floor(list.length * Math.random())]
 }
-function pickRandom(list) {
-  return list[Math.floor(list.length * Math.random())]
-}
