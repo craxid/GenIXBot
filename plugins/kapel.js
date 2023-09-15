5648e3d9cd469cd97cc4f0975a770ab57556d366
Dede Kurniawan <dede.klender@gmail.com>
2023-09-16 06:40:24 +0700

    update kapel.js

--- plugins/kapel.js
+++ plugins/kapel.js
@@ -2,6 +2,13 @@
 import fetch from 'node-fetch'
 let handler = async (m, { conn, args, usedPrefix, command }) => {
     
+    m.react(rwait)
+
+let type = (command).toLowerCase()
+
+switch (type) {
+
+    
 case "couple": case"ppcp":{
 if (isBan) return newReply('Lu di ban kocak awokwok') 
 if (isLimit(m.sender, isPremium, isCreator, limitCount, limit)) return newReply(`Limit kamu sudah habis silahkan kirim ${prefix}limit untuk mengecek limit`)
@@ -13,7 +20,8 @@ juna.sendMessage(m.chat,{image: {url: random.female,},caption: `Couple Female`,}
 }
 limitAdd(m.sender, limit)
 break
-
+default
+}
 }
 
 handler.help = ['ppcouple']
