70775f7e0ad00b91ece63ed5a018491df90097ad
Dede Kurniawan <dede.klender@gmail.com>
2023-09-16 04:39:03 +0700

    update _bot.js

--- plugins/_bot.js
+++ plugins/_bot.js
@@ -20,7 +20,7 @@ thumbnailUrl: mikapp,
 sourceUrl: ('https://www.facebook.com/dede2015k'),
 mediaType: 1,
 showAdAttribution: true,
-renderLargerThumbnail: true
+renderLargerThumbnail: false
 }}})
 }
 handler.customPrefix = /^(bot|bot?|bott|mika|oy|bang|banh)$/i
