0d72c4b98ebe5c3543f34d5bf1c60a1377987a1f
Dede Kurniawan <dede.klender@gmail.com>
2023-09-16 03:38:35 +0700

    update dl-instagram.js

--- plugins/dl-instagram.js
+++ plugins/dl-instagram.js
@@ -21,10 +21,10 @@ export default handler
 import fetch from "node-fetch"
 
 var handler = async (m, { conn, text, usedPrefix, command }) => {
-  if (!text) throw `Masukkan URL!\n\nContoh: ${usedPrefix + command} https://api.arifzyn.biz.id/download/instagram?url=`
+  if (!text) throw `Masukkan URL!\n\nContoh: ${usedPrefix + command} https://www.instagram.com`
   await m.reply(wait)
   
-  let idgl = await fetch(`https://api.arifzyn.biz.id/api/download/instagram?url=${text}`)
+  let idgl = await fetch(`https://api.arifzyn.biz.id/download/instagram?url=${text}`)
   let res = await idgl.json()
   
   conn.sendFile(m.chat, res.result.media, 'ig.mp4', 'Done kak!', m)
