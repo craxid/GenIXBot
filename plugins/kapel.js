ded43de6abc3f506b04ad25b4f065e8a781f4d8f
Dede Kurniawan <dede.klender@gmail.com>
2023-09-16 06:44:28 +0700

    update kapel.js

--- plugins/kapel.js
+++ plugins/kapel.js
@@ -13,8 +13,8 @@ case "ppcp":
 case "kapel": {
 if (isBan) return newReply('Lu di ban kocak awokwok') 
 
-let anu = await fetchJson("https://raw.githubusercontent.com/iamriz7/kopel_/main/kopel.json")
-newReply(mess.wait)
+let anu = await fetch("https://raw.githubusercontent.com/iamriz7/kopel_/main/kopel.json")
+
 let random = anu[Math.floor(Math.random() * anu.length)]
 conn.sendMessage(m.chat,{image: {url: random.male,},caption: `Couple Male`,},{quoted: m,})
 conn.sendMessage(m.chat,{image: {url: random.female,},caption: `Couple Female`,},{quoted: m,})
