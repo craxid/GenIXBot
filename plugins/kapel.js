70e12c2d3dc2d3795f275b1ee5a70b14b997d75f
Dede Kurniawan <dede.klender@gmail.com>
2023-09-16 06:42:57 +0700

    update kapel.js

--- plugins/kapel.js
+++ plugins/kapel.js
@@ -8,7 +8,10 @@ let type = (command).toLowerCase()
 
 switch (type) {
     
-case "couple": case"ppcp":{
+case "couple": 
+case "ppcp":
+case "kapel"
+case{
 if (isBan) return newReply('Lu di ban kocak awokwok') 
 if (isLimit(m.sender, isPremium, isCreator, limitCount, limit)) return newReply(`Limit kamu sudah habis silahkan kirim ${prefix}limit untuk mengecek limit`)
 let anu = await fetchJson("https://raw.githubusercontent.com/iamriz7/kopel_/main/kopel.json")
