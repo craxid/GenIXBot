a981c8b9190ece2e4ba22dfeb23241e873d27c54
Dede Kurniawan <dede.klender@gmail.com>
2023-09-16 04:12:47 +0700

    update animedif2.js

--- plugins/animedif2.js
+++ plugins/animedif2.js
@@ -11,8 +11,6 @@ try {
     let anidif2 = await fetch(`https://api.neoxr.eu/api/waifudiff?q=${text}`)
     
     let hasil = await anidif2.json()
-    
-// let diff = await conn.getFile(`https://api.azz.biz.id/api/animediffusion?q=${text}&key=mangea`)
 	    
 conn.sendFile(m.chat, hasil.anidif2.data.url, 'img.jpg', `*[ ANIME DIFFUSION ]*\n\nPROMPT:\n${text}`, m)
 	}
