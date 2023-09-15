e049b6008f3bb0c6ff22a75cbcb2e3462af03ac4
Dede Kurniawan <dede.klender@gmail.com>
2023-09-16 06:50:42 +0700

    update kapel.js

--- plugins/kapel.js
+++ plugins/kapel.js
@@ -12,7 +12,16 @@ case "couple":
 case "ppcouple":
 case "ppcp":
 case "kapel": {
-if
+if (isBan) return newReply('Lu di ban kocak awokwok') 
+if (isLimit(m.sender, isPremium, isCreator, limitCount, limit)) return newReply(`Limit kamu sudah habis silahkan kirim ${prefix}limit untuk mengecek limit`)
+let anu = await fetchJson("https://raw.githubusercontent.com/iamriz7/kopel_/main/kopel.json")
+newReply(mess.wait)
+let random = anu[Math.floor(Math.random() * anu.length)]
+juna.sendMessage(m.chat,{image: {url: random.male,},caption: `Couple Male`,},{quoted: m,})
+juna.sendMessage(m.chat,{image: {url: random.female,},caption: `Couple Female`,},{quoted: m,})
+}
+limitAdd(m.sender, limit)
+break
 
 default;
  }
