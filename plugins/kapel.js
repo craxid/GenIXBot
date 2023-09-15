79946e6d7b7d520086ba165f373ff23b79a4c256
Dede Kurniawan <dede.klender@gmail.com>
2023-09-16 06:42:20 +0700

    update kapel.js

--- plugins/kapel.js
+++ plugins/kapel.js
@@ -14,8 +14,8 @@ if (isLimit(m.sender, isPremium, isCreator, limitCount, limit)) return newReply(
 let anu = await fetchJson("https://raw.githubusercontent.com/iamriz7/kopel_/main/kopel.json")
 newReply(mess.wait)
 let random = anu[Math.floor(Math.random() * anu.length)]
-juna.sendMessage(m.chat,{image: {url: random.male,},caption: `Couple Male`,},{quoted: m,})
-juna.sendMessage(m.chat,{image: {url: random.female,},caption: `Couple Female`,},{quoted: m,})
+conn.sendMessage(m.chat,{image: {url: random.male,},caption: `Couple Male`,},{quoted: m,})
+conn.sendMessage(m.chat,{image: {url: random.female,},caption: `Couple Female`,},{quoted: m,})
 }
 break
 
