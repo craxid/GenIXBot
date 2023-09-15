267bcacfd3cf97aadb57ed7fe8ac99f8f1d4202e
Dede Kurniawan <dede.klender@gmail.com>
2023-09-16 05:04:56 +0700

    update handler.js

--- handler.js
+++ handler.js
@@ -445,11 +445,12 @@ export async function handler(chatUpdate) {
             }
         }
 
-        try {
+        /*try {
             if (!opts['noprint']) await (await import(`./lib/print.js`)).default(m, this)
         } catch (e) {
             console.log(m, m.quoted, e)
-        }
+        }*/
+        
         if (opts['autoread'])
             await this.chatRead(m.chat, m.isGroup ? m.sender : undefined, m.id || m.key.id).catch(() => { })
     }
