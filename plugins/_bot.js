179632c90d8f5d00089f9d45322c35188d262ee4
Dede Kurniawan <dede.klender@gmail.com>
2023-09-16 04:37:41 +0700

    update _bot.js

--- plugins/_bot.js
+++ plugins/_bot.js
@@ -10,7 +10,7 @@ let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.m
     let pp = await conn.profilePictureUrl(who, 'image').catch(_ => './src/avatar_contact.png')
     let username = conn.getName(who)
     
-conn.sendMessage(m.chat, {
+if (mika) conn.sendMessage(m.chat, {
 text: mika,
 contextInfo: {
 externalAdReply: {
