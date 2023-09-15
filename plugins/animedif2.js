53059ead6e8c0c23a0b7a75b42647362c4475ba2
Dede Kurniawan <dede.klender@gmail.com>
2023-09-16 04:05:31 +0700

    update animedif2.js

--- plugins/animedif2.js
+++ plugins/animedif2.js
@@ -8,7 +8,7 @@ m.reply(wait)
 
 //code area
 try {
-    let anidif2 = await conn.getFile(`https://api.neoxr.eu/api/waifudiff?q=${text}`)
+    let anidif2 = await conn.getFile(`https://api.neoxr.eu.org/api/waifudiff?q=${text}`)
     let hasil = await animdif2.json()
     
 // let diff = await conn.getFile(`https://api.azz.biz.id/api/animediffusion?q=${text}&key=mangea`)
