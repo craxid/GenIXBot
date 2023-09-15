a4c21d3be389de9022c334cc22ad8710d609dd75
Dede Kurniawan <dede.klender@gmail.com>
2023-09-16 06:59:02 +0700

    update kapel.js

--- plugins/kapel.js
+++ plugins/kapel.js
@@ -9,15 +9,21 @@ let handler  = async (m, { conn, command }) => {
      const time = moment.tz('Asia/Jakarta').format("HH:mm:ss")
      let username = conn.getName(m.sender)
      console.log(color('[COMMAND]', 'pink'), color(command, 'yellow'), color(time, 'white'), color('Name:', 'yellow'), color(username, 'cyan'), color('Number:', 'yellow'), color(m.sender.split('@')[0], 'cyan'))
+     
      let data = fs.readFileSync('./lib/kapelan.js');
+     
      let  jsonData = JSON.parse(data);
      let randIndex = Math.floor(Math.random() * jsonData.length);
      let json = jsonData[randIndex];
      let randCowo= await getBuffer(json.cowo)
+     
      conn.sendMessage(m.chat, { image: randCowo, caption: '*Cowo*', quoted: m })
+     
 	 // conn.sendMessage(m.chat, randCowo, MessageType.image,  { caption: '*Cowo*', quoted: m })
+	 
 	 let randCewe = await getBuffer(json.cewe)
 	conn.sendMessage(m.chat, { image: randCewe, caption: '*Cewe*', quoted: m })
+	
 	 // conn.sendMessage(m.chat, randCewe, MessageType.image, { caption: '*Cewe*', quoted: m })
 }
 handler.help = ['ppcouple']
