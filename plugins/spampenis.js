b8432dd28f9f68f3d06004a6f18dd8ff734b1dac
Dede Kurniawan <dede.klender@gmail.com>
2023-09-16 15:26:22 +0700

    update spampenis.js

--- plugins/spampenis.js
+++ plugins/spampenis.js
@@ -1,6 +1,9 @@
 let handler = async (m, { conn }) => {
 let name = await conn.getName(m.sender)
 
+if  (!text) throw `✳️ Ketik spampenis 1`
+m.reply(wait)
+
 let kntl = await ("kontol\n").repeat(100)
 
 handler.help = ['spampenis <jimlah>']
