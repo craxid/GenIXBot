7c0564ea98e38aa43d2849f3beba058b36f5320b
Dede Kurniawan <dede.klender@gmail.com>
2023-09-16 21:22:05 +0700

    update txt2img.js

--- plugins/txt2img.js
+++ plugins/txt2img.js
@@ -9,7 +9,8 @@ m.reply(wait)
 try {
   let diff = await fetch(`https://api.neoxr.eu/api/waifudiff?q=${text}`)
   
-  if (!diff.ok) throw await diff.json()
+  if (!diff.ok) throw await diff.text()
+  let jeson = await 
   
 //let diff = await conn.getFile(`https://api.azz.biz.id/api/text2img?q=${text}&key=mangea`)
 	    
