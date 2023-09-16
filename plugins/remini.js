15b7846f569645ad1ebcb4ef1ccb01f1109f2a18
Dede Kurniawan <dede.klender@gmail.com>
2023-09-16 10:40:40 +0700

    update remini.js

--- plugins/remini.js
+++ plugins/remini.js
@@ -1,50 +1,145 @@
 
-import axios from 'axios'
-import FormData from 'form-data'
+import FormData from "form-data"
+Jimp from "jimp"
 
-let handler = async (m, { conn, command }) => {
-  let q = m.quoted ? m.quoted : m
-  let mime = (q.msg || q).mimetype || ''
-  if (/image/.test(mime)) {
-    let img = await q.download()
-    let formData = new FormData()
-    formData.append('image', img, 'image.jpg')
-    
-    try {
-      let res = await axios.post('https://api.imgbb.com/1/upload', formData, {
-        params: {
-          key: '32dccc1da5b49fc9ed09fd6d9e691d40'
-        },
-        
-        headers: formData.getHeaders()
-      })
-      let imageUrl = res.data.data.url
-      
-      let api = `https://api.yanzbotz.my.id/api/tools/enhance?url=${encodeURIComponent(imageUrl)}`
-      
-      //let api = `https://api.neoxr.my.id/api/remini?image=${encodeURIComponent(imageUrl)}&apikey=lucycat`
-
-      m.react(rwait)
-
-      let { data } = await axios.get(api)
-      let resultURL = data.data.url
-
-      let resultImage = await axios.get(resultURL, { responseType: 'arraybuffer' })
-      let contentType = resultImage.headers['content-type']
-
-      await conn.sendFile(m.chat, Buffer.from(resultImage.result.url, 'binary'), 'remini.jpg', wm, m, false, { mimetype: contentType })
-    } catch (e) {
-      console.log(e)
-      m.reply('There is an error!')
-    }
-  } else {
-    m.reply('Kirim/Balas gambar dengan caption .remini')
-  }
+async function processing(urlPath, method) {
+	return new Promise(async (resolve, reject) => {
+		let Methods = ["enhance", "recolor", "dehaze"];
+		Methods.includes(method) ? (method = method) : (method = Methods[0]);
+		let buffer,
+			Form = new FormData(),
+			scheme = "https" + "://" + "inferenceengine" + ".vyro" + ".ai/" + method;
+		Form.append("model_version", 1, {
+			"Content-Transfer-Encoding": "binary",
+			contentType: "multipart/form-data; charset=uttf-8",
+		});
+		Form.append("image", Buffer.from(urlPath), {
+			filename: "enhance_image_body.jpg",
+			contentType: "image/jpeg",
+		});
+		Form.submit(
+			{
+				url: scheme,
+				host: "inferenceengine" + ".vyro" + ".ai",
+				path: "/" + method,
+				protocol: "https:",
+				headers: {
+					"User-Agent": "okhttp/4.9.3",
+					Connection: "Keep-Alive",
+					"Accept-Encoding": "gzip",
+				},
+			},
+			function (err, res) {
+				if (err) reject();
+				let data = [];
+				res
+					.on("data", function (chunk, resp) {
+						data.push(chunk);
+					})
+					.on("end", () => {
+						resolve(Buffer.concat(data));
+					});
+				res.on("error", (e) => {
+					reject();
+				});
+			}
+		);
+	});
 }
-
-handler.command = /^remini$/i
-handler.tags = ['tools']
-handler.help = ['remini','enchant','tohd']
-handler.premium = false
-
-export default handler
+let handler = async (m, { conn, usedPrefix, command }) => {
+	switch (command) {
+		case "enhancer":
+		case "unblur":
+		case "enhance":
+			{
+				conn.enhancer = conn.enhancer ? conn.enhancer : {};
+				if (m.sender in conn.enhancer)
+					throw "Masih Ada Proses Yang Belum Selesai Kak, Silahkan Tunggu Sampai Selesai Yah >//<";
+				let q = m.quoted ? m.quoted : m;
+				let mime = (q.msg || q).mimetype || q.mediaType || "";
+				if (!mime)
+					throw `Fotonya Mana Kak?`;
+				if (!/image\/(jpe?g|png)/.test(mime))
+					throw `Mime ${mime} tidak support`;
+				else conn.enhancer[m.sender] = true;
+				m.reply("Proses Kak...");
+				let img = await q.download?.();
+				let error;
+				try {
+					const This = await processing(img, "enhance");
+					conn.sendFile(m.chat, This, "", "Sudah Jadi Kak >//<", m);
+				} catch (er) {
+					error = true;
+				} finally {
+					if (error) {
+						m.reply("Proses Gagal :(");
+					}
+					delete conn.enhancer[m.sender];
+				}
+			}
+			break;
+		case "colorize":
+		case "colorizer":
+			{
+				conn.recolor = conn.recolor ? conn.recolor : {};
+				if (m.sender in conn.recolor)
+					throw "Masih Ada Proses Yang Belum Selesai Kak, Silahkan Tunggu Sampai Selesai Yah >//<";
+				let q = m.quoted ? m.quoted : m;
+				let mime = (q.msg || q).mimetype || q.mediaType || "";
+				if (!mime)
+					throw `Fotonya Mana Kak?`;
+				if (!/image\/(jpe?g|png)/.test(mime))
+					throw `Mime ${mime} tidak support`;
+				else conn.recolor[m.sender] = true;
+				m.reply("Proses Kak...");
+				let img = await q.download?.();
+				let error;
+				try {
+					const This = await processing(img, "enhance");
+					conn.sendFile(m.chat, This, "", "Sudah Jadi Kak >//<", m);
+				} catch (er) {
+					error = true;
+				} finally {
+					if (error) {
+						m.reply("Proses Gagal :(");
+					}
+					delete conn.recolor[m.chat];
+				}
+			}
+			break;
+		case "hd":
+		case "hdr":
+			{
+				conn.hdr = conn.hdr ? conn.hdr : {};
+				if (m.sender in conn.hdr)
+					throw "Masih Ada Proses Yang Belum Selesai Kak, Silahkan Tunggu Sampai Selesai Yah >//<";
+				let q = m.quoted ? m.quoted : m;
+				let mime = (q.msg || q).mimetype || q.mediaType || "";
+				if (!mime)
+					throw `Fotonya Mana Kak?`;
+				if (!/image\/(jpe?g|png)/.test(mime))
+					throw `Mime ${mime} tidak support`;
+				else conn.hdr[m.sender] = true;
+				m.reply("Proses Kak...");
+				let img = await q.download?.();
+				let error;
+				try {
+					const This = await processing(img, "enhance");
+					conn.sendFile(m.chat, This, "", "Sudah Jadi Kak >//<", m);
+				} catch (er) {
+					error = true;
+				} finally {
+					if (error) {
+						m.reply("Proses Gagal :(");
+					}
+					delete conn.hdr[m.sender];
+				}
+			}
+			break;
+	}
+};
+handler.help = ["enhancer", "hdr", "colorize"];
+handler.tags = ["tools"];
+handler.premium = false;
+handler.command = ["unblur", "enchaner", "enhance", "hdr", "colorize"];
+module.exports = handler;
