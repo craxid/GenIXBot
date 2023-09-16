b6b09c8bce501fbc9ecda2e9dd14256312be1b45
Dede Kurniawan <dede.klender@gmail.com>
2023-09-16 08:03:10 +0700

    update kapel.js

--- plugins/kapel.js
+++ plugins/kapel.js
@@ -10,7 +10,7 @@ switch (type) {
 
 case 'ppcouple':
 case 'ppcp':
-	axios.get(`https://raw.githubusercontent.com/iamriz7/kopel_/main/kopel.json`).then(({ data }) => {
+	let fetch(`https://raw.githubusercontent.com/iamriz7/kopel_/main/kopel.json`).then(({ data }) => {
 	conn.sendMessage(from, { image: { url: data.result.male }, caption: 'Male' })
 	
 	conn.sendMessage(from, { image: { url: data.result.female }, caption: 'Female' })
