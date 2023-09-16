3cde8d741a80de1998835de6e5a8137cfff4c096
Dede Kurniawan <dede.klender@gmail.com>
2023-09-16 07:59:04 +0700

    update kapel.js

--- plugins/kapel.js
+++ plugins/kapel.js
@@ -7,20 +7,12 @@ await m.reply(wait)
 let type = (command).toLowerCase()
 switch (type) {
 
-/*case "couple":
-case "ppcp": {
-
-//if (isLimit(m.sender, isPremium, isCreator, limitCount, limit)) return newReply(`Limit kamu sudah habis silahkan kirim ${prefix}limit untuk mengecek limit`)
-
-let anu = await fetch("https://raw.githubusercontent.com/iamriz7/kopel_/main/kopel.json")
-let anuin = await anu.json()
-
-//let random = anuin[Math.floor(Math.random() * anuin.length)]
-
-conn.sendMessage(m.chat,{image: {anuin.url: random.male,},caption: `Couple Male`,},{quoted: m,})
-
-conn.sendMessage(m.chat,{image: {anuin.url: random.female,},caption: `Couple Female`,},{quoted: m,})
-}
+case 'ppcouple':
+	axios.get(`https://api.lolhuman.xyz/api/random/${command}?apikey=${apikey}`).then(({ data }) => {
+	conn.sendMessage(from, { image: { url: data.result.male }, caption: 'Male' })
+	
+	sock.sendMessage(from, { image: { url: data.result.female }, caption: 'Female' })
+	})
 break
 default;*/
   }
