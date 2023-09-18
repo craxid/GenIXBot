e2a3b608ee5f14fe7623f25dd7fc6f5c19b807a5
Dede Kurniawan <dede.klender@gmail.com>
2023-09-18 23:19:28 +0700

    update ncnlstat.js

--- plugins/ncnlstat.js
+++ plugins/ncnlstat.js
@@ -34,7 +34,7 @@ text: tegs,
 contextInfo: {
 externalAdReply: {
 title: (`Server Stalker`),
-body: (`${sevre.`),
+body: (`${sevre.hostname}`),
 thumbnailUrl: thumb,
 sourceUrl: ('minecraft://?addExternalServer=GenIX Network|play.genix.eu.org:29427'),
 mediaType: 1,
