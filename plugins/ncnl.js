8fbd9c4873718697a325f5084b6197b8fa850354
Dede Kurniawan <dede.klender@gmail.com>
2023-09-19 06:39:42 +0700

    update ncnl.js

--- plugins/ncnl.js
+++ plugins/ncnl.js
@@ -43,7 +43,7 @@ renderLargerThumbnail: false
 
 	}
 	catch {
-		m.reply(`❎ Server sedang offline\n\nHOST: play.ncnl.eu.org\nPORT: 40383`)
+		m.reply(`👾 Server sedang offline\n\nHOST: play.ncnl.eu.org\nPORT: 40383`)
 	}
 //end
 
