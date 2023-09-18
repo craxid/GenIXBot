823403682a4291829890a1adb979e46daa5f83af
Dede Kurniawan <dede.klender@gmail.com>
2023-09-18 22:16:59 +0700

    update ncnlstat.js

--- plugins/ncnlstat.js
+++ plugins/ncnlstat.js
@@ -12,7 +12,7 @@ let sevre = await ncnlstat.json()
 let tegs = `
 ${global.htki} *GenIX Server* ${global.htka}
 ${global.gz} *📅 Version:* ${sevre.version.name}
-${global.gz} *MOTD:* ${sevre.motd.clean.replace(/^GenIX(S1)/, '')}
+${global.gz} *MOTD:* ${sevre.motd.clean.replace(/^GenIX S1)/, '')}
 ${global.gz} *Game Mode:* ${sevre.gamemode}
 ${global.gz} *Player:* ${sevre.players.online}
 ${global.gz} *Max Player:* ${sevre.players.max}
