160aba2c57a77c9a382bd8fbba2c8a25f06e95b9
Dede Kurniawan <dede.klender@gmail.com>
2023-09-18 11:37:36 +0700

    update dl-igstalk.js

--- plugins/dl-igstalk.js
+++ plugins/dl-igstalk.js
@@ -23,7 +23,7 @@ externalAdReply: {
 title: ('Instagram Stalker'),
 body: (`${res.username}`),
 thumbnailUrl: (`${res.profilePic}`),
-sourceUrl: ('`${res.res.username}`),
+sourceUrl: ('https://instagram.com/'`${res.res.username}`),
 mediaType: 1,
 showAdAttribution: true,
 renderLargerThumbnail: false
