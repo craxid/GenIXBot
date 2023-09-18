0983cedaa9797cee9caec06b0810d4efa89b7b7e
Dede Kurniawan <dede.klender@gmail.com>
2023-09-18 23:03:22 +0700

    update ncnlstat.js

--- plugins/ncnlstat.js
+++ plugins/ncnlstat.js
@@ -6,7 +6,7 @@ m.reply(wait)
 
 
 try {
-let ncnlstat = await fetch(`https://api.mcsrvstat.us/bedrock/3/play.genix.eu.org:2942`)
+let ncnlstat = await fetch(`https://api.mcsrvstat.us/bedrock/3/play.genix.eu.org:29427`)
 let sevre = await ncnlstat.json()
     
 let tegs = `
