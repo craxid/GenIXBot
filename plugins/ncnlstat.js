65442bc5cd2fef962dfdefc38d7d25d2304c0cf5
Dede Kurniawan <dede.klender@gmail.com>
2023-09-18 18:20:10 +0700

    update ncnlstat.js

--- plugins/ncnlstat.js
+++ plugins/ncnlstat.js
@@ -21,7 +21,7 @@ try {
     Status: ${statu.online}
     Host: ${statu.host}
     Port: ${statu.port}
-    Versoon: ${statu.version}
+    Versoon: ${versi.name}
     `
 conn.sendMessage(m.chat, {
 text: tegs,
