84f4adecbbe76aaf8776a2d59406e7e11312f564
Dede Kurniawan <dede.klender@gmail.com>
2023-09-18 16:53:22 +0700

    update _anticall.js

--- plugins/_anticall.js
+++ plugins/_anticall.js
@@ -15,6 +15,7 @@ handler.all = async function (m) {
 	if(!setting.anticall) return 
 	
 	if (m.messageStubType === (WAMessageStubType.CALL_MISSED_VOICE || WAMessageStubType.CALL_MISSED_VIDEO)) {
+	    
 		await this.reply(m.chat, 'Kamu telah di blokir!\nKarena melanggar aturan bot', null)
 		await delay(1000)
 		await this.updateBlockStatus(m.chat, "block")
