6c6ee91f1e92eedec29dae8f7f8dad208fd6e8f8
Dede Kurniawan <dede.klender@gmail.com>
2023-09-19 08:11:15 +0700

    update smeme.js

--- plugins/smeme.js
+++ plugins/smeme.js
@@ -1,38 +1,27 @@
-
-import uploadImage from '../lib/uploadImage.js'
-import { sticker } from '../lib/sticker.js'
-import fs from 'fs'
+const uploadImage = require('../lib/uploadImage') 
+const { sticker } = require('../lib/sticker')
 
 let handler = async (m, { conn, text, usedPrefix }) => {
-    
-switch (type) {
-case 'smeme': 
-case 'stickmeme':
-case 'stikmeme':
-case 'stickermeme':
-case 'stikermeme': {
-let respond = `Kirim/reply image/sticker dengan caption ${prefix + command} text1|text2`
-
-if (!/image/.test(mime)) throw respond
-if (!text) throw respond
-	  m.reply(wait)
-	  
-atas = text.split('|')[0] ? text.split('|')[0] : '-'
-bawah = text.split('|')[1] ? text.split('|')[1] : '-'
-	
-	let fatGans = await uploadImage(img)
-
-let smeme = `https://api.memegen.link/images/custom/${encodeURIComponent(atas)}/${encodeURIComponent(bawah)}.png?background=${fatGans}`
-	 
-let stiker = await sticker(null, smeme, packname, author)       
-conn.sendFile(m.chat, stiker, 'sticker.webp', '', m)
-	        await fs.unlinkSync(FaTiH)
-            }
-	       break
-	       default;
+ try {
+  let [teks, teks2] = text.split('|')
+  let q = m.quoted ? m.quoted : m
+  let mime = (q.msg || q).mimetype || ''
+  if (!mime) throw 'Tidak ada foto'
+  if (!/image\/(jpe?g|png)/.test(mime)) throw `Mime ${mime} tidak support`
+  let img = await q.download()
+  let url = await uploadImage(img)
+  let wasted = `https://api.memegen.link/images/custom/${teks}/${teks2}.png?background=${url}`
+//  let wasted = `http://docs-jojo.herokuapp.com/api/meme-gen?top=${teks}&bottom=${teks2}&img=${url}`
+  let stiker = await sticker(null, wasted, packname, author)
+  conn.sendFile(m.chat, stiker, 'sticker.webp', '', m)
+ } catch (e) {
+   m.reply(`Masukan format!!\nReply image dengan caption ${usedPrefix}smeme teks1|teks2\n*Jangan reply sticker*`)
+  }
 }
 handler.help = ['smeme']
 handler.tags = ['sticker']
 handler.command = /^smeme$/i
+handler.diamond = false
+handler.fail = null
 
 export default handler
