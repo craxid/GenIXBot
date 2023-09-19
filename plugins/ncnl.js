7d733776d3c93c7f31a0c07b815a1b99ad162e29
Dede Kurniawan <dede.klender@gmail.com>
2023-09-19 10:30:36 +0700

    update ncnl.js

--- plugins/ncnl.js
+++ plugins/ncnl.js
@@ -16,7 +16,8 @@ ${global.htki} *NCNL Server* ${global.htka}
 ${global.gz} *Version:* ${sevre.protocol.name}
 ${global.gz} *MOTD:* ${sevre.motd.clean}
 ${global.gz} *World Name:* ${sevre.map.clean}
-${global.gz} *Game Mode:* ${sevre.gamemode}/ ${sevre.players.online}
+${global.gz} *Game Mode:* ${sevre.gamemode}
+${global.gz} *Player Online:* ${sevre.players.online}
 ${global.gz} *Max Player:* ${sevre.players.max}
 ${global.gz} *Online:* ${sevre.online}
 ${global.sb}
