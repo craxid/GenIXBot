b68925a07afc33c2c675caff1e0f590ece5c8bf7
Dede Kurniawan <dede.klender@gmail.com>
2023-09-19 08:17:35 +0700

    update smeme.js

--- plugins/smeme.js
+++ plugins/smeme.js
@@ -3,7 +3,8 @@ const { sticker } from '../lib/sticker.js'
 
 let handler = async (m, { conn, text, usedPrefix }) => {
  try {
-  let [teks1 = text.split('|')[0] ? text.split('|')[0] : '-'
+  let teks1 = text.split('|')[0] ? text.split('|')[0] : '-'
+  let teks2 = text.split('|')[1] ? text.split('|')[1] : '-'
   
 //atas = text.split('|')[0] ? text.split('|')[0] : '-'
 //bawah = text.split('|')[1] ? text.split('|')[1] : '-'
