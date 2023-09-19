18615bd1266da7b9c611cfefa52b747a436f7e68
Dede Kurniawan <dede.klender@gmail.com>
2023-09-20 06:16:22 +0700

    update owner-restart.js

--- plugins/owner-restart.js
+++ plugins/owner-restart.js
@@ -2,15 +2,14 @@ import { spawn } from 'child_process'
 let handler = async (m, { conn, isROwner, text }) => {
     if (!process.send) throw 'Dont: node main.js\nDo: node index.js'
     if (conn.user.jid == conn.user.jid) {
-    await m.reply('🔄 Reiniciando Bot...\n Espere un momento')
+    await m.reply('🔄 Memulai ulang Bot...\n tunggu bentar...')
     process.send('reset')
   } else throw 'eh'
 }
 
 handler.help = ['restart']
 handler.tags = ['owner']
-handler.command = ['restart','reiniciar'] 
-
+handler.command = ['restart'] 
 handler.rowner = true
 
 export default handler
