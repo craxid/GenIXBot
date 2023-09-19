2b9de3f773592e652a20b31ab42e0be8d84a80e8
Dede Kurniawan <dede.klender@gmail.com>
2023-09-19 08:09:07 +0700

    update smeme.js

--- plugins/smeme.js
+++ plugins/smeme.js
@@ -11,15 +11,17 @@ case 'stickmeme':
 case 'stikmeme':
 case 'stickermeme':
 case 'stikermeme': {
-	let respond = `Kirim/reply image/sticker dengan caption ${prefix + command} text1|text2`
-	if (!/image/.test(mime)) throw respond
+let respond = `Kirim/reply image/sticker dengan caption ${prefix + command} text1|text2`
+
+if (!/image/.test(mime)) throw respond
 if (!text) throw respond
-	  m.reply(mess.wait)
+	  m.reply(wait)
+	  
 atas = text.split('|')[0] ? text.split('|')[0] : '-'
 bawah = text.split('|')[1] ? text.split('|')[1] : '-'
 	
 	let fatGans = await uploadImage(img)
-	
+
 let smeme = `https://api.memegen.link/images/custom/${encodeURIComponent(atas)}/${encodeURIComponent(bawah)}.png?background=${fatGans}`
 	 
 let stiker = await sticker(null, smeme, packname, author)       
