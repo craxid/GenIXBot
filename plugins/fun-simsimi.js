b03043189e0eea4612aebdef5941b65a810722f5
Dede Kurniawan <dede.klender@gmail.com>
2023-09-20 22:36:59 +0700

    update fun-simsimi.js

--- plugins/fun-simsimi.js
+++ plugins/fun-simsimi.js
@@ -3,10 +3,10 @@
 let handler = async (m, { conn, text, usedPrefix, command }) => {
 	
  let name = conn.getName(m.sender)
-  if (!text) throw `Hai kak *${name}* mau ngobrol denganku? \nTanggapi dengan *${usedPrefix + command}* (isi pesan) \n\n📌 Contoh : *${usedPrefix + command}* Halo Akebi`
+  if (!text) throw `Hai kak *${name}* mau ngobrol denganku? \nTanggapi dengan *${usedPrefix + command}* (isi pesan) \n\n📌 Contoh : *${usedPrefix + command}* Halo Mika`
   m.react('🗣️') 
   
-  let res = await fetch(global.API(`https://api.yanzbotz.my.id/api/ai/characterai?id=id3xBWK9-mcu8Y8JOQjbKzzAmOzgGgmp8HomTp-UOq8&query=${m.text}`))
+  let res = await fetch(global.API(`https://api.yanzbotz.my.id/api/ai/characterai?id=id3xBWK9-mcu8Y8JOQjbKzzAmOzgGgmp8HomTp-UOq8&query=${text}`))
   
   let json = await res.json()
   if (json.success) m.reply(json.success.replace('simsimi', 'DyLux').replace('Simsimi', 'DyLux').replace('sim simi', 'DyLux'))
