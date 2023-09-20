a22bff22ad1a0198ecbba63cc5e97f4326caed13
Dede Kurniawan <dede.klender@gmail.com>
2023-09-20 23:23:05 +0700

    update _chatbot.js

--- plugins/_chatbot.js
+++ plugins/_chatbot.js
@@ -12,7 +12,7 @@ if (!user.chatbot)
 return !0
 //let mika = await fetch(`https://api.yanzbotz.my.id/api/ai/characterai?id=id3xBWK9-mcu8Y8JOQjbKzzAmOzgGgmp8HomTp-UOq8&query=${m.text}`)
 
-let mika = await fetch(`https://api.yanzbotz.my.id/api/ai/characterai?text=What%20is%20your%20favorite%20food?&name=Misono%20Mikay`)
+let mika = await fetch(`https://api.yanzbotz.my.id/api/ai/characterai?text=${m.text}&name=Misono%20Mikay`)
 
         
 let res = await mika.json()
