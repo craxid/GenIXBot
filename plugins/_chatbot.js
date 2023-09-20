fc08add1ee27c57813c77de7ea9af44346d234b1
Dede Kurniawan <dede.klender@gmail.com>
2023-09-20 22:38:51 +0700

    update _chatbot.js

--- plugins/_chatbot.js
+++ plugins/_chatbot.js
@@ -4,14 +4,16 @@ import fetch from 'node-fetch'
 export async function before(m, { conn }) {
 if (m.isBaileys && m.fromMe)
         return !0
-    if (!m.isGroup) return !1
+
+if (!m.isGroup) return !1
     let user = global.db.data.users[m.sender]
     
-      if (!user.chatbot)
+if (!user.chatbot)
         return !0
-        let mika = await fetch(`https://api.yanzbotz.my.id/api/ai/characterai?id=UGiJxTlsUMl802QN8K5k0IJ7xzNMSDpxQfARcM3Fu8Y&query=${m.text}`)
+let mika = await fetch(`https://api.yanzbotz.my.id/api/ai/characterai?id=UGiJxTlsUMl802QN8K5k0IJ7xzNMSDpxQfARcM3Fu8Y&query=${m.text}`)
         
-        let res = await mika.json()
+let res = await mika.json()
         
-        m.reply(res.result)
+m.reply(res.result)
+
 }
