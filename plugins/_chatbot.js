4a39bc1a811df450f4e9411d1e579c2c7cc50e17
Dede Kurniawan <dede.klender@gmail.com>
2023-09-20 23:48:42 +0700

    update _chatbot.js

--- plugins/_chatbot.js
+++ plugins/_chatbot.js
@@ -11,7 +11,7 @@ if (!m.isGroup) return !1
 if (!user.chatbot)
 return !0
 
-let mika = await fetch(`https://api.yanzbotz.my.id/api/ai/characterai?text=${m.text}&name=Misono%20Mika`)
+let mika = await fetch(`https://api.yanzbotz.my.id/api/ai/characterai?text=${m.text}&name=Mika`)
 
 let res = await mika.json()
 
