9f74d5083b2fc0a70649c99a7e3e698d55b7e29e
Dede Kurniawan <dede.klender@gmail.com>
2023-09-21 10:31:41 +0700

    update fun-simsimi.js

--- plugins/fun-simsimi.js
+++ plugins/fun-simsimi.js
@@ -3,7 +3,8 @@ import fetch from 'node-fetch'
 let handler = async (m, { text }) => {
   let res = await fetch(`https://api.zahwazein.xyz/entertainment/simisimi?text=${text}&apikey=zenzkey_f59c1aacf2`)
   let json = await res.json()
-  if (json.result) m.reply(json.result.replace('simi','Mika').replace('simsimi','Mika'))
+  
+  if (json.result) m.reply(json.result.message.replace('simi','Mika').replace('simsimi','Mika'))
   else throw json
 }
 handler.help = ['simi', 'simsimi', 'simih'].map(v => v + ' <teks>')
