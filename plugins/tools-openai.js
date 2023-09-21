b6307df2b5f79578528fe4132658c58fd4bd41ed
Dede Kurniawan <dede.klender@gmail.com>
2023-09-22 06:36:50 +0700

    update tools-openai.js

--- plugins/tools-openai.js
+++ plugins/tools-openai.js
@@ -14,7 +14,7 @@ m.react('💬')
 	    /*let gpt = await fetch(`https://api.azz.biz.id/api/gpt?q=${text}&key=mangea`)*/
      ;
         let res = await gpt.json()
-        await m.reply(res.result.replace('YanzBotz','Mika Bot'))
+        await m.reply(res.result.replace('YanzBotz','Mika Bot').replace('-APIs', ''))
 	} catch {
 		m.reply(`❎ Error: Ada sebuah kesalahan`)
 	}
