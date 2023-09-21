4bafa9c4a5b77defa70e7d2be55022bd631c77ee
Dede Kurniawan <dede.klender@gmail.com>
2023-09-21 19:43:16 +0700

    update smeme.js

--- plugins/smeme.js
+++ plugins/smeme.js
@@ -1,32 +1,30 @@
+
 import uploadImage from '../lib/uploadImage.js'
+import fetch from 'node-fetch'
 
 let handler = async (m, { conn, text, usedPrefix }) => {
  try {
   let teks1 = text.split('|')[0] ? text.split('|')[0] : '-'
   let teks2 = text.split('|')[1] ? text.split('|')[1] : '-'
   
-//atas = text.split('|')[0] ? text.split('|')[0] : '-'
-//bawah = text.split('|')[1] ? text.split('|')[1] : '-'
   let q = m.quoted ? m.quoted : m
   let mime = (q.msg || q).mimetype || ''
   if (!mime) throw 'Tidak ada foto'
   if (!/image\/(jpe?g|png)/.test(mime)) throw `Mime ${mime} tidak support`
   let img = await q.download()
   let url = await uploadImage(img)
-  let wasted = `https://api.memegen.link/images/custom/${encodeURIComponent(teks1)}/${encodeURIComponent(teks2)}.png?background=${url}`
   
-//  let wasted = `http://docs-jojo.herokuapp.com/api/meme-gen?top=${teks}&bottom=${teks2}&img=${url}`
-
-  let stiker = await sticker(null, wasted, packname, author)
-  conn.sendFile(m.chat, stiker, 'sticker.webp', '', m)
+ let smeme = await conn.getFile(`https://api.zahwazein.xyz/creator/smeme?text=${encodeURIComponent(teks1)}&text2=${encodeURIComponent(teks2)}&url=${url}&apikey=zenzkey_f59c1aacf2`)
+  
+  conn.sendFile(m.chat, meme.data, 'meme_picture.jpg', '', m)
+  
  } catch (e) {
-   m.reply(`Masukan format!\nReply image dengan caption ${usedPrefix}smeme teks1|teks2\n*Jangan reply sticker*`)
+   m.reply(`Reply gambar dengan caption ${usedPrefix}mememaker teks atas|teks bawah\n*Jangan reply sticker*`)
   }
 }
-handler.help = ['smeme']
-handler.tags = ['sticker']
-handler.command = /^smeme$/i
+handler.help = ['mememaker']
+handler.tags = ['img']
+handler.command = /^(mememaker|memeq)$/i
 handler.diamond = false
-handler.fail = null
 
 export default handler
