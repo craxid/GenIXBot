6429637210e3d8e9bf3f35586992f3cf8137ce2c
Dede Kurniawan <dede.klender@gmail.com>
2023-09-21 20:25:57 +0700

    update _autolevelup.js

--- plugins/_autolevelup.js
+++ plugins/_autolevelup.js
@@ -1,35 +1,24 @@
+//import db from '../lib/database.js'
+import { canLevelUp } from '../lib/levelling.js'
 
-import { color } from '../lib/color'
-import moment from "moment-timezone"
+export async function before(m, { conn }) {
+    let user = global.db.data.users[m.sender]
+    if (!user.autolevelup)
+        return !0
+    let before = user.level * 1
+    while (canLevelUp(user.level, user.exp, global.multiplier))
+        user.level++
+    user.role = global.rpg.role(user.level).name
+    if (before !== user.level) {
+        m.reply(`
+*▢ NAIK LEVEL*
 
-let levelling = require('../lib/levelling.js')
-module.exports = {
-	before(m) {
-		let user = global.db.data.users[m.sender]
-		if (!user.autolevelup) return !0
-		let before = user.level * 1
-		while (levelling.canLevelUp(user.level, user.exp, global.multiplier)) user.level++
-
-		if (before !== user.level) {
-		    
-			let chating = `*Congratulations*, you have leveled up!
-*[ ${before} ]* ➠ *[ ${user.level} ]*
-Use *.profile* to check`.trim()
-			let thumb = 'https://telegra.ph/file/bd044275940ed1b62efcd.jpg'
-
-conn.sendMessage(m.chat, {
-text: chating,
-contextInfo: {
-externalAdReply: {
-title: namebot,
-body: global.wm2,
-thumbnailUrl: global.ppmika,
-sourceUrl: global.dygp,
-mediaType: 1,
-showAdAttribution: true,
-renderLargerThumbnail: true
-}}})
-			console.log(color(chating, 'yellow'))
-		}
-	}
+ *${before}* ‣  *${user.level}*
+ Rank : *${user.role}*
+ 
+ _untuk menonaktifkan, ketik_
+_*/off autolevelup*_
+	`.trim())
+    }
 }
+
