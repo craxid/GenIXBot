e7223bb79f3e46fea7ee297772053707e0c56b19
Dede Kurniawan <dede.klender@gmail.com>
2023-09-21 22:53:32 +0700

    update ocr.js

--- plugins/ocr.js
+++ plugins/ocr.js
@@ -7,7 +7,6 @@ let handler = async (m, { conn, text }) => {
     let q = m.quoted ? m.quoted : m
     let mime = (q.msg || q).mimetype || ''
     if (!mime) throw `balas gambar dengan perintah .ocr`
-    
     await m.reply(wait)
     
     if (!/image\/(jpe?g|png)/.test(mime)) throw `
