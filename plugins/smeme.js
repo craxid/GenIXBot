5ad35215252d874096d100053af33ac188779121
Dede Kurniawan <dede.klender@gmail.com>
2023-09-21 19:40:59 +0700

    update smeme.js

--- plugins/smeme.js
+++ plugins/smeme.js
@@ -15,10 +15,11 @@ let handler = async (m, { conn, text, usedPrefix }) => {
   let url = await uploadImage(img)
   let wasted = `https://api.memegen.link/images/custom/${encodeURIComponent(teks1)}/${encodeURIComponent(teks2)}.png?background=${url}`
 //  let wasted = `http://docs-jojo.herokuapp.com/api/meme-gen?top=${teks}&bottom=${teks2}&img=${url}`
+
   let stiker = await sticker(null, wasted, packname, author)
   conn.sendFile(m.chat, stiker, 'sticker.webp', '', m)
  } catch (e) {
-   m.reply(`Masukan format!!\nReply image dengan caption ${usedPrefix}smeme teks1|teks2\n*Jangan reply sticker*`)
+   m.reply(`Masukan format!\nReply image dengan caption ${usedPrefix}smeme teks1|teks2\n*Jangan reply sticker*`)
   }
 }
 handler.help = ['smeme']
