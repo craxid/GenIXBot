6ca43f6c1d86e5d40b6b6f6528ec21a7e99d93c1
Dede Kurniawan <dede.klender@gmail.com>
2023-09-21 20:19:07 +0700

    update _autolevelup.js

--- plugins/_autolevelup.js
+++ plugins/_autolevelup.js
@@ -1,5 +1,5 @@
 //import db from '../lib/database.js'
-/*import { canLevelUp } from '../lib/levelling.js'
+import { canLevelUp } from '../lib/levelling.js'
 
 export async function before(m, { conn }) {
     let user = global.db.data.users[m.sender]
@@ -19,38 +19,4 @@ export async function before(m, { conn }) {
 _*off autolevelup*_
 	`.trim())
     }
-}*/
-
-import { color } from '../lib/color.js'
-import moment from "moment-timezone"
-import levelling from '../lib/levelling.js'
-
-module.exports = { before(m) {
-		let user = global.db.data.users[m.sender]
-		if (!user.autolevelup) return !0
-		let before = user.level * 1
-		while (levelling.canLevelUp(user.level, user.exp, global.multiplier)) user.level++
-
-		if (before !== user.level) {
-			let chating = `*Congratulations*, you have leveled up!
-*[ ${before} ]* ➠ *[ ${user.level} ]*
-Use *.profile* to check`.trim()
-
-	let thumb = 'https://telegra.ph/file/bd044275940ed1b62efcd.jpg'
-	
-conn.sendMessage(m.chat, {
-text: chating,
-contextInfo: {
-externalAdReply: {
-title: namebot,
-body: wm,
-thumbnailUrl: thumb,
-sourceUrl: sig,
-mediaType: 1,
-showAdAttribution: true,
-renderLargerThumbnail: true
-}}})
-			console.log(color(chating, 'yellow'))
-		}
-	}
 }
