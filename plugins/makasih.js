b1785a0e1ac4cef1c8f6ba7bbd8d152bacceb3a9
Dede Kurniawan <dede.klender@gmail.com>
2023-09-21 14:40:20 +0700

    update makasih.js

--- plugins/makasih.js
+++ plugins/makasih.js
@@ -5,18 +5,6 @@ let mikapp = 'https://telegra.ph/file/a9a78b769836fe10fa62d.jpg'
 
 m.reply(`Sama-sama kak ${name}`)
 
-/*conn.sendMessage(m.chat, {
-text: mika,
-contextInfo: {
-externalAdReply: {
-title: ('Mika Misono Bot'),
-body: ('Jangan Spam Bot!'),
-thumbnailUrl: mikapp,
-sourceUrl: ('https://www.facebook.com/dede2015k'),
-mediaType: 1,
-showAdAttribution: true,
-renderLargerThumbnail: true
-}}})*/
 }
 handler.customPrefix = /^(makasih|terima kasih|thx|thanks|tengs|thank you|arigato|arigatou|arigathanks)$/i
 handler.command = new RegExp
