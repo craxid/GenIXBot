2a56279a9de374ab6f5b06019641c96c61cb433a
Dede Kurniawan <dede.klender@gmail.com>
2023-09-21 15:41:16 +0700

    update cekapi.js

--- plugins/cekapi.js
+++ plugins/cekapi.js
@@ -11,45 +11,6 @@ try {
       const count = args[1] && args[1].length > 0 ? Math.min(99999999, Math.max(parseInt(args[1]), 1)) : !args[1] || args.length < 3 ? 1 : Math.min(1, count)
         switch (type) {
             
-       case 'xteam':
-       if (type == 'xteam') {
-         let xteam = await (await fetch(`https://api.xteam.xyz/cekey?APIKEY=${args[0]}`)).json().catch(v => 'error')
-         await m.reply(cek)
-         if (xteam == 'error') {
-m.reply(`Maaf restapi ini sedang error, harap coba lagi nanti`)
-} else { 
-if (xteam.response == "Only alphanumeric!") return m.reply('Only alphanumeric!')
- if (xteam.response == "Apikey tidak ditemukan, silahkan daftar atau beli ke developer untuk mendapatkan apikey yang valid!") return m.reply('Invalid Apikey!')
-conn.reply(m.chat, `• *Type:* XTeam
-• *Apikey:* ${args[0]}
-
-• *Name:* ${xteam.response.name}
-• *IP Address:* ${xteam.response.ip}
-• *Email:* ${xteam.response.email}
-• *Total Hit:* ${xteam.response.totalhit}
-• *Premium:* ${xteam.response.premium}
-
-• *Expired:* ${(xteam.response.expired).replace('Premium left:', '')}`, m)
-}
-}
-            break
-            
-          case 'lolhuman': 
-    let lol = await (await fetch(`https://api.lolhuman.xyz/api/checkapikey?apikey=${args[0]}`)).json()
-    m.reply(cek)
-    if (lol.message == 'success') {
-    conn.reply(m.chat, `• *Type:* Lolhuman
-• *Apikey:* ${args[0]}
-
-• *Name:* ${lol.result.username}
-• *Total Hit:* ${lol.result.requests}
-• *Hit Today:* ${lol.result.today}
-• *Account:* ${lol.result.account_type}
-
-• *Expired:* ${lol.result.expired}`, m)
-} else m.reply('Invalid Apikey !')
-            break
-            
 case 'zenzapi': 
     let zenz = await (await fetch(`https://api.zahwazein.xyz/user/cekapi?apikey=${args[0]}`)).json()
     m.reply(cek)
