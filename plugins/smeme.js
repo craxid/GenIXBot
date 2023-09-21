e9159fc31d8e092ac153945125e6a24a90712a1b
Dede Kurniawan <dede.klender@gmail.com>
2023-09-21 19:54:23 +0700

    update smeme.js

--- plugins/smeme.js
+++ plugins/smeme.js
@@ -1,38 +1,7 @@
 
-import uploadImage from '../lib/uploadImage.js'
-import { sticker } from '.../lib/sticker.js'
-import fetch from 'node-fetch'
 
-let handler = async (m, { conn, text, usedPrefix }) => {
- try {
-  let teks1 = text.split('|')[0] ? text.split('|')[0] : '-'
-  let teks2 = text.split('|')[1] ? text.split('|')[1] : '-'
-  
-  let q = m.quoted ? m.quoted : m
-  let mime = (q.msg || q).mimetype || ''
-  if (!mime) throw 'Tidak ada foto'
-  if (!/image\/(jpe?g|png)/.test(mime)) throw `Mime ${mime} tidak support`
-  let img = await q.download()
-  let url = await uploadImage(img)
-  let smeme = `https://api.zahwazein.xyz/creator/smeme?text=${encodeURIComponent(teks1)}&text2=${encodeURIComponent(teks2)}&url=${url}&apikey=zenzkey_f59c1aacf2`
-  
-  let stiker = await sticker(null, wasted, global.packname, global.author)
-  conn.sendFile(m.chat, stiker, 'sticker.webp', '', m)
-  
- } catch (e) {
-   m.reply(`Reply gambar dengan caption ${usedPrefix}smeme teks atas|teks bawah\n*Jangan reply sticker*`)
-  }
-}
-handler.help = ['smeme']
-handler.tags = ['sticker']
-handler.command = /^(smeme)$/i
-handler.diamond = false
-
-export default handler
-
-
-const uploadImage = require('../lib/uploadImage') 
-const { sticker } = require('../lib/sticker')
+import uploadImage from '../lib/uploadImage'
+const { sticker } = require('../lib/sticker'
 
 let handler = async (m, { conn, text, usedPrefix }) => {
  try {
@@ -43,7 +12,8 @@ let handler = async (m, { conn, text, usedPrefix }) => {
   if (!/image\/(jpe?g|png)/.test(mime)) throw `Mime ${mime} tidak support`
   let img = await q.download()
   let url = await uploadImage(img)
-  let wasted = `https://api.memegen.link/images/custom/${teks}/${teks2}.png?background=${url}`
+  let wasted = `https://api.zahwazein.xyz/creator/smeme?text=${encodeURIComponent(teks1)}&text2=${encodeURIComponent(teks2)}&url=${url}&apikey=zenzkey_f59c1aacf2`
+  
 //  let wasted = `http://docs-jojo.herokuapp.com/api/meme-gen?top=${teks}&bottom=${teks2}&img=${url}`
   let stiker = await sticker(null, wasted, packname, author)
   conn.sendFile(m.chat, stiker, 'sticker.webp', '', m)
