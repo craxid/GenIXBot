3fec45d625501fba93cc50e1abdb26b54cd5a4e9
Dede Kurniawan <dede.klender@gmail.com>
2023-09-21 10:22:31 +0700

    update fun-preg.js

--- plugins/fun-preg.js
+++ plugins/fun-preg.js
@@ -3,7 +3,7 @@ import fetch from 'node-fetch'
 let handler = async (m, { text }) => {
   let res = await fetch(global.API(`https://saipulanuar.ga/api/f/simi?text=${text}`))
   let json = await res.json()
-  if (json.result) m.reply(json.result.replace('imi',))
+  if (json.result) m.reply(json.result.replace('simi','Mika').replace(simisimk))
   else throw json
 }
 handler.help = ['simi', 'simsimi', 'simih'].map(v => v + ' <teks>')
