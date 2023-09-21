08fc0d8168604230897f0228aa4f7effd9983107
Dede Kurniawan <dede.klender@gmail.com>
2023-09-21 14:35:01 +0700

    update img-asupan.js

--- plugins/img-asupan.js
+++ plugins/img-asupan.js
@@ -1,6 +1,6 @@
 
 let handler = async(m, { conn, usedPrefix, command }) => {
-	
+	await m.react(rwait)
     let img = await conn.getFile('https://api.zahwazein.xyz/randomasupan/asupan?apikey=zenzkey_f59c1aacf2')
     let asupan = img.data
     conn.sendFile(m.chat, asupan, 'vid.mp4', `✅ Done`, m)
