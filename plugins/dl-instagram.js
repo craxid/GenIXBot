5240a5aa550ab024677d9ce37a81bc15489e7fd6
Dede Kurniawan <dede.klender@gmail.com>
2023-09-22 05:55:29 +0700

    update dl-instagram.js

--- plugins/dl-instagram.js
+++ plugins/dl-instagram.js
@@ -1,22 +1,4 @@
-/*
-import fg from 'api-dylux'
 
-let handler = async (m, { conn, args, usedPrefix, command }) => {
-    if (!args[0]) throw `✳️ Cara penggunaan\n *${usedPrefix + command}* https://www.instagram.com/p/CYHeKxyMj-J/?igshid=YmMyMTA2M2Y=`
-    m.react(rwait)
-    let res = await fg.igdl(args[0])
-    for (let result of res.url_list) {
-    conn.sendFile(m.chat, result, 'igdl.mp4', `✅ Done`, m)
-    m.react(done)
-  }
-}
-handler.help = ['instagram <link ig>']
-handler.tags = ['dl']
-handler.command = ['ig', 'igdl', 'instagram', 'igimg', 'igvid'] 
-handler.diamond = true
-
-export default handler 
-*/
 
 import fetch from "node-fetch"
 let limit = 15
