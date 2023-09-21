02b43d4d29599dbb5c4097f67294dce3fd76729c
Dede Kurniawan <dede.klender@gmail.com>
2023-09-21 15:45:42 +0700

    update cekapi.js

--- plugins/cekapi.js
+++ plugins/cekapi.js
@@ -5,7 +5,44 @@ let handler = async (m, { conn, usedPrefix, args, command }) => {
     let _type = (args[1] || '').toLowerCase()
     if (!args[0]) return m.reply('Apikeynya mana?')
   
+try {
+let ncnlstat = await fetch(`https://api.mcsrvstat.us/bedrock/1/play.ncnl.eu.org:40383`)
+let sevre = await ncnlstat.json()
 
+let tegs = `
+${global.htki} *NCNL Server* ${global.htka}
+${global.gz} *Version:* ${sevre.version}
+${global.gz} *MOTD:* ${sevre.motd.clean}
+${global.gz} *World Name:* ${sevre.map}
+${global.gz} *Game Mode:* ${sevre.gamemode}
+${global.gz} *Player:* ${sevre.players.online}/${sevre.players.max}
+${global.sb}
+
+${global.htki} *HOST & PORT* ${global.htka}
+${global.gz} *Host:* ${sevre.hostname}
+${global.gz} *IP:* ${sevre.ip}
+${global.gz} *Port:* ${sevre.port}
+${global.sb}
+
+API version: ${sevre.debug.apiversion}`
+    
+conn.sendMessage(m.chat, {
+text: tegs,
+contextInfo: {
+externalAdReply: {
+title: (`Server Stalker`),
+body: (`${sevre.hostname}:${sevre.port}`),
+thumbnailUrl: thumb,
+sourceUrl: ('minecraft://?addExternalServer=NCNL Legacy|play.ncnl.eu.org:40383'),
+mediaType: 1,
+showAdAttribution: true,
+renderLargerThumbnail: false
+}}})
+
+	}
+	catch {
+		m.reply(`👾 ERROR`)
+	}
 }
 handler.help = ['cekapikey']
 handler.tags = ['tools']
