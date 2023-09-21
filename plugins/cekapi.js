f4489c80306aabc7595b7b63959c7995517b27b8
Dede Kurniawan <dede.klender@gmail.com>
2023-09-21 16:07:16 +0700

    update cekapi.js

--- plugins/cekapi.js
+++ plugins/cekapi.js
@@ -36,7 +36,7 @@ renderLargerThumbnail: false
 		m.reply(`👾 ERROR`)
 	}
 }
-handler.help = ['sekapi']
+handler.help = ['cekapi']
 handler.command = /^(cek(key|api))$/i
 handler.rowner = true
 export default handler
