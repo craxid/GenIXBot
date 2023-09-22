e7674b576317c9e9902f537a8f30fdb987d9dcbf
Dede Kurniawan <dede.klender@gmail.com>
2023-09-22 17:08:53 +0700

    update tebakgambar.js

--- plugins/tebakgambar.js
+++ plugins/tebakgambar.js
@@ -15,7 +15,7 @@ let handler = async (m, { conn, usedPrefix }) => {
   let json = src[Math.floor(Math.random() * src.length)]
   if (!json) throw json
   let caption = `
-Bantuan: ${json.deskripsi}
+Bantuan: *${json.deskripsi}*
 
 Timeout *${(timeout / 1000).toFixed(2)} detik*
 Ketik ${usedPrefix}tebar untuk bantuan
