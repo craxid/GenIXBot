03f23d256f853a90830f2d93714bc72fd52b604d
Dede Kurniawan <dede.klender@gmail.com>
2023-09-22 15:12:30 +0700

    update ocr.js

--- plugins/ocr.js
+++ plugins/ocr.js
@@ -1,4 +1,36 @@
+/*
+import uploadImage from '../lib/uploadImage.js'
+import ocrapi from 'ocr-space-api-wrapper'
+import { MessageType } from '@adiwajshing/baileys'
 
+let handler = async (m, { conn, text }) => {
+let q = m.quoted ? m.quoted : m
+let mime = (q.msg || q).mimetype || ''
+
+if (!mime) throw `balas gambar dengan perintah .ocr`
+
+if (!/image\/(jpe?g|png)/.test(mime)) throw `
+_*jenis ${mime} tidak didukung!*_
+`
+    
+    let img = await q.download()
+    let url = await uploadImage(img)
+    let hasil = await ocrapi.ocrSpace(url)
+    
+await m.reply(hasil.ParsedResults[0].ParsedText)    }
+
+handler.help = ['ocr', 'totext']
+handler.tags = ['tools']
+handler.command = /^(ocr|totext)$/i
+handler.diamond = true
+
+export default handler
+*/
+
+Berikut adalah kode yang telah diperbaiki:
+
+```javascript
+*/
 import uploadImage from '../lib/uploadImage.js'
 import ocrapi from 'ocr-space-api-wrapper'
 import { MessageType } from '@adiwajshing/baileys'
@@ -27,7 +59,7 @@ handler.command = /^(ocr|totext)$/i
 handler.diamond = true
 
 export default handler
-/*```
+```
 
 Beberapa perbaikan yang saya lakukan di antaranya:
 
@@ -36,4 +68,4 @@ Beberapa perbaikan yang saya lakukan di antaranya:
 3. Menambahkan `MessageType.text` saat mengirimkan hasil OCR agar pesan dikirim sebagai teks.
 4. Menambahkan opsi `quoted: m` saat mengirimkan pesan balasan agar pesan terkait kutipannya sesuai dengan kutipan pesan asli.
 
-Harap diingat untuk memasukkan library yang diperlukan dan memastikan path pada import module sudah benar.*/
+Harap diingat untuk memasukkan library yang diperlukan dan memastikan path pada import module sudah benar.
