45f2f6d333a28dd809cb36d801319de10917e324
Dede Kurniawan <dede.klender@gmail.com>
2023-09-22 15:37:37 +0700

    update hapusbg.js

--- plugins/hapusbg.js
+++ plugins/hapusbg.js
@@ -13,7 +13,7 @@ let media = await q.download()
 let url = await uploadImage(media)
 let hasil = await (await fetch(`https://api.zahwazein.xyz/convert/sticker-nobg?url=${url}&apikey=zenzkey_f59c1aacf2`)).buffer()
 
-await conn.sendFile(m.chat, hasil, 'img.jpg', '', '2023 © Mika Bot', m)
+await conn.sendFile(m.chat, hasil.data, 'img.jpg', '', '2023 © Mika Bot', m)
 	
 }
 handler.help = ['removebg']
