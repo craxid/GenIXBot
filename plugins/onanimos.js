791b200fe5c7adf4108302a0bb3e6c6b1774bb7d
Dede Kurniawan <dede.klender@gmail.com>
2023-09-22 10:21:05 +0700

    update onanimos.js

--- plugins/onanimos.js
+++ plugins/onanimos.js
@@ -11,14 +11,14 @@ async function handler(m, { command, usedPrefix }) {
          //   if (!room) this.sendButton(m.chat, 'Kamu tidak sedang berada di anonymous chat', wm, 'Search Partner..', '.next', m) // throw 'Kamu tidak sedang berada di anonymous chat\nKetik #start\nKetik /start\nKetik !start'
         //    if (!room) throw 'Kamu tidak sedang berada di anonymous chat\nKetik #start\nKetik /start\nKetik !start'
         if (!room) {
-                await this.sendButton(m.chat, 'Kamu tidak sedang berada di anonymous chat', wm, null, [['Search Partner', '/start']], m)
+                //await this.sendButton(m.chat, 'Kamu tidak sedang berada di anonymous chat', wm, null, [['Search Partner', '/start']], m)
                 throw 0
             }
            // this.send2Button(m.chat, 'Kamu telah memberhentikan chat', wm, 'Search Partner..', `${usedPrefix}start`, 'Stop..', `${usedPrefix}stop`, m)
            //m.reply('Kamu telah memberhentikan chat')
         //    let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
        //     if (!room) throw 'Kamu tidak sedang berada di anonymous chat\nKetik #start\nKetik /start\nKetik !start'
-            this.sendButton(m.chat, 'Kamu telah memberhentikan chat', wm, null, [['Search Partner', '/start']], m)
+            //this.sendButton(m.chat, 'Kamu telah memberhentikan chat', wm, null, [['Search Partner', '/start']], m)
             let other = room.other(m.sender)
             if (other) this.sendButton(other, 'Partner telah memberhentikan chat', wm, null, [['Search Partner', '/start']], m)
            // if (other) this.send2Button(other, 'Partner meninggalkan chat', wm, 'Search Partner..', '/start',  m)
