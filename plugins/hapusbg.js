13d012a00cf34b8b577bade04f8f8400da996477
Dede Kurniawan <dede.klender@gmail.com>
2023-09-22 13:34:14 +0700

    update hapusbg.js

---
+++ plugins/hapusbg.js
@@ -0,0 +1,23 @@
+
+import fetch from 'node-fetch'
+im uploadImage from '../lib/uploadImage.js'
+
+let handler = async (m, { conn, usedPrefix, command, text }) => {
+let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+let name = await conn.getName(who)
+let q = m.quoted ? m.quoted : m
+let mime = (q.msg || q).mimetype || ''
+if (!mime) throw 'Kirim/Reply Gambar dengan caption .removebg'
+m.reply('Tunggu Sebentar...')
+let media = await q.download()
+let url = await uploadImage(media)
+let hasil = await (await fetch(`https://api.ibeng.tech/api/maker/rmbg?url=${url}&apikey=tamvan`)).buffer()
+await conn.sendFile(m.chat, hasil, '', '2023 © Mika Bot', m)
+	
+}
+handler.help = ['removebg']
+handler.tags = ['maker']
+handler.command = /^(removebg)$/i
+handler.limit = true
+
+module.exports = handler
