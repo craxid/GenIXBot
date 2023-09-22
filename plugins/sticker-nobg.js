98fa5c6fca2563e89f6370b5d6c6a4649d4694d8
Dede Kurniawan <dede.klender@gmail.com>
2023-09-22 18:00:20 +0700

    update sticker-nobg.js

--- plugins/sticker-nobg.js
+++ plugins/sticker-nobg.js
@@ -1,24 +1,34 @@
-
-import fetch from 'node-fetch'
 import uploadImage from '../lib/uploadImage.js'
+import { sticker } from '../lib/sticker.js'
+import MessageType from '@whiskeysockets/baileys'
+const effects = ['jail', 'gay', 'glass', 'wasted' ,'triggered', 'lolice', 'simpcard', 'horny']
 
-let handler = async (m, { conn, usedPrefix, command, text }) => {
-let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-let name = await conn.getName(who)
+let handler = async (m, { conn, usedPrefix, text, command }) => {
+let effect = text.trim().toLowerCase()
+if (!effects.includes(effect)) throw `
+
+📌 *Contoh:* 
+${usedPrefix + command}snobg 
+`.trim()
 let q = m.quoted ? m.quoted : m
 let mime = (q.msg || q).mimetype || ''
-if (!mime) throw 'Kirim/Reply Gambar dengan caption .removebg'
-m.reply(wait)
-let media = await q.download()
-let url = await uploadImage(media)
-let hasil = await (await conn.getFile(`https://api.zahwazein.xyz/convert/sticker-nobg?url=${url}&apikey=zenzkey_f59c1aacf2`))
-
-conn.sendFile(m.chat, hasil.data, 'sticker.webp', m, null, rpl)
-	
-}
-handler.help = ['stickernobg']
+if (!mime) throw '✳️ Balas sebuah gambar'
+if (!/image\/(jpe?g|png)/.test(mime)) throw `✳️ Format tidak didukung`
+let img = await q.download()
+let url = await uploadImage(img)
+let apiUrl = global.API('https://some-random-api.com/canvas/', encodeURIComponent(effect), {
+avatar: url
+})
+try {
+let stiker = await sticker(null, apiUrl, global.packname, global.author)
+conn.sendFile(m.chat, stiker, null, { asSticker: true }, m)
+} catch (e) {
+m.reply('Error saat mengkonversi stiker, file terlalu besar')
+await conn.sendFile(m.chat, apiUrl, 'smaker.png', null, m)
+}}
+handler.help = ['smaker']
 handler.tags = ['sticker']
-handler.command = /^(stickernobg|stikernobg|sbg|snobg)$/i
-handler.diamond = 1
+handler.command = ['stickmaker', 'stickermaker', 'smaker'] 
+handler.diamond = true
 
 export default handler
