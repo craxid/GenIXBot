30ca1e417c55b4690bd6aff21ab55364c8ce60c8
Dede Kurniawan <dede.klender@gmail.com>
2023-09-22 09:48:02 +0700

    update game-math_answer.js

--- plugins/game-math_answer.js
+++ plugins/game-math_answer.js
@@ -13,7 +13,7 @@ handler.before = async function (m) {
             global.db.data.users[m.sender].exp += math.bonus
             clearTimeout(this.math[id][3])
             delete this.math[id]
-            m.reply(`✅ *Jawaban benar*\n\n‣ Kamu mendapatkan : *+${math.bonus} XP*`)
+            m.reply(`✅ *Jawaban benar 🎉*\n\n‣ Kamu mendapatkan : *+${math.bonus} XP*`)
         } else {
             if (--this.math[id][2] == 0) {
                 clearTimeout(this.math[id][3])
