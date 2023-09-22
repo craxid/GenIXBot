3b26678d367e01d072e03d9316027816bde9fede
Dede Kurniawan <dede.klender@gmail.com>
2023-09-22 17:46:46 +0700

    update sticker-nobg.js

---
+++ plugins/sticker-nobg.js
@@ -0,0 +1,24 @@
+
+import fetch from 'node-fetch'
+import uploadImage from '../lib/uploadImage.js'
+
+let handler = async (m, { conn, usedPrefix, command, text }) => {
+let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+let name = await conn.getName(who)
+let q = m.quoted ? m.quoted : m
+let mime = (q.msg || q).mimetype || ''
+if (!mime) throw 'Kirim/Reply Gambar dengan caption .removebg'
+m.reply(wait)
+let media = await q.download()
+let url = await uploadImage(media)
+let hasil = await (await conn.getFile(`https://api.zahwazein.xyz/convert/sticker-nobg?url=${url}&apikey=zenzkey_f59c1aacf2`))
+
+await conn.sendFile(m.chat, hasil.data, 'img.webp', 'Background Remover\n© Mika Bot', m)
+	
+}
+handler.help = ['removebg']
+handler.tags = ['img']
+handler.command = /^(removebg|nobg)$/i
+handler.diamond = 3
+
+export default handler
