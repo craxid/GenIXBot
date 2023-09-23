19db66ed19c27f5718b9a4fe997bb68bae7ec76c
Dede Kurniawan <dede.klender@gmail.com>
2023-09-23 22:22:19 +0700

    update handler.js

--- handler.js
+++ handler.js
@@ -495,7 +495,7 @@ export async function participantsUpdate({ id, participants, action }) {
                                 groupicon: ppgp,
                                 membercount: groupMetadata.participants.length,
                                 profile: pp,
-                                background: 'https://i.imgur.com/bbWbASn.jpg'
+                                background: 'https://minimalistic-wallpaper.demolab.com/?random'
                             }, 'apikey')
 
                             let lea = API('azz', '/api/goodbye', {
