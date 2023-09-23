d74278411ea4d72926204df4f3f0b72234b2b23a
Dede Kurniawan <dede.klender@gmail.com>
2023-09-23 11:18:07 +0700

    update _autolevelup.js

--- plugins/_autolevelup.js
+++ plugins/_autolevelup.js
@@ -1,37 +1,32 @@
-//import db from '../lib/database.js'
-import { canLevelUp } from '../lib/levelling.js'
-import { levelup } from '../lib/canvas.js'
-import db from '../lib/database.js'
 
-export async function before(m, { conn }) {
-    let user = global.db.data.users[m.sender]
-    if (!user.autolevelup)
-        return !0
-    let before = user.level * 1
-    while (canLevelUp(user.level, user.exp, global.multiplier))
-        user.level++
-    user.role = global.rpg.role(user.level).name
-    if (before !== user.level) {
-    
-let chating = `
-*Selamat*, *${name}* naik level!
+import { color } from '../lib/color.js'
+const moment ="moment-timezone"
+let levelling from '../lib/levelling.js'
+module.exports = {
+	before(m) {
+		let user = global.db.data.users[m.sender]
+		if (!user.autolevelup) return !0
+		let before = user.level * 1
+		while (levelling.canLevelUp(user.level, user.exp, global.multiplier)) user.level++
+
+		if (before !== user.level) {
+			let chating = `*Congratulations*, you have leveled up!
 *[ ${before} ]* ➠ *[ ${user.level} ]*
-gunakan *.profile* untuk memeriksa
-`.trim()
-
-let thumb = 'https://telegra.ph/file/bd044275940ed1b62efcd.jpg'
-
-conn.sendMessage(m.chat, {
+Use *.profile* to check`.trim()
+			let thumb = 'https://telegra.ph/file/bd044275940ed1b62efcd.jpg'
+    conn.sendMessage(m.chat, {
 text: chating,
 contextInfo: {
 externalAdReply: {
-title: global.namebot,
-body: global.wm2,
+title: namebot,
+body: wm,
 thumbnailUrl: thumb,
-sourceUrl: global.dygp,
+sourceUrl: sig,
 mediaType: 1,
 showAdAttribution: true,
 renderLargerThumbnail: true
 }}})
-    }
+			console.log(color(chating, 'yellow'))
+		}
+	}
 }
