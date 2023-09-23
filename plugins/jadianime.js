569798ecc33a3fcd12fe8c78d16c78acb6bfc7ca
Dede Kurniawan <dede.klender@gmail.com>
2023-09-24 06:24:04 +0700

    update jadianime.js

--- plugins/jadianime.js
+++ plugins/jadianime.js
@@ -7,7 +7,9 @@ let handler = async (m, { conn, usedPrefix, command, text }) => {
 let q = m.quoted ? m.quoted : m
 let mime = (q.msg || q).mimetype || ''
 if (!mime) throw 'Kirim/Reply Gambar dengan caption .jadianime'
-m.reply('Tunggu Sebentar...')
+
+m.reply(wait)
+
 let media = await q.download()
 let url = await uploadImage(media)
 let hasil = await (await fetch(`https://phototoanime.p.rapidapi.com/draw?rapidapi-key=553e4784d3msh29ae65eb1df940bp158d54jsnc55b268d0b6b&url=${url}`)).buffer()
