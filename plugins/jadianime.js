c5e51e580a0e235ed2ef5687434de9a7f0418deb
Dede Kurniawan <dede.klender@gmail.com>
2023-09-24 05:49:10 +0700

    update jadianime.js

--- plugins/jadianime.js
+++ plugins/jadianime.js
@@ -1,7 +1,7 @@
 
 import fetch from 'node-fetch'
 import uploadImage from '../lib/uploadImage.js'
-import jadiAnime from '@saipulanuar/'
+import jadiAnime from '@saipulanuar/jadianime-ts'
 let handler = async (m, { conn, usedPrefix, command, text }) => {
 let q = m.quoted ? m.quoted : m
 let mime = (q.msg || q).mimetype || ''
