0c9a21fcd3b7ecf3a5bc05c63774e7b4f8f1a890
Dede Kurniawan <dede.klender@gmail.com>
2023-09-23 08:27:50 +0700

    update jadizombie.js

---
+++ plugins/jadizombie.js
@@ -0,0 +1,29 @@
+
+import uploadImage from '../lib/uploadImage'
+import makemeazombie = require('makemeazombie'
+import zombie = new makemeazombie();
+
+async function handler(m, { conn }) {
+var q = m.quoted ? m.quoted : m;
+var mime = (q.msg || q).mimetype || '';
+if (!mime) {
+ return m.reply('Kirim/Balas gambar dengan caption *.jadizombie*')
+ }
+var media = await q.download();
+var gambar = await uploadImage(media)
+zombie.transform({
+    photo: `${gambar}`,
+    destinyFolder: './tmp'
+})
+.then(data => {
+    conn.sendFile(m.chat, data, 'zombie.jpg', wm, m);
+})
+ .catch(err => {
+    console.log('Terjadi kesalahan:', err);
+ })
+};
+
+handler.command = handler.help = ['jadizombie']
+handler.tags = ['maker']
+
+module.exports = handler
