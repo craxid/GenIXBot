6df137a6fa555008f86711fb57b68a3f31bed913
Dede Kurniawan <dede.klender@gmail.com>
2023-09-23 22:58:28 +0700

    update handler.js

--- handler.js
+++ handler.js
@@ -492,8 +492,7 @@ export async function participantsUpdate({ id, participants, action }) {
 //https://api.azz.biz.id/api/welcome?name=Miftah&gcname=Miftah%20Botz&member=167&pp=https://i.ibb.co/w7nHy5k/mark.jpg&bg=https://minimalistic-wallpaper.demolab.com/?random&key=
 
 
-let wel = conn.getFile(`https://api.azz.biz.id/api/welcome?name=${namewelyh}&gcname=${gcnamewelyh}&pp=${profilewelyh}&bg=https://minimalistic-wallpaper.demolab.com/?random&key=global
-`)
+let wel = conn.getFile(`https://api.azz.biz.id/api/welcome?name=${namewelyh}&gcname=${gcnamewelyh}&pp=${profilewelyh}&bg=https://minimalistic-wallpaper.demolab.com/?random&key=global`)
 
 let namewelyh = await this.getName(user)
 let gcnamewelyh = await this.getName(id)
@@ -504,7 +503,7 @@ let bg = 'https://i.imgur.com/bbWbASn.jpg'
 
 
 
-let lea = API('azz', '/api/goodbye'
+let wel = conn.getFile(`https://api.azz.biz.id/api/welcome?name=${namewelyh}&gcname=${gcnamewelyh}&pp=${profilewelyh}&bg=https://minimalistic-wallpaper.demolab.com/?random&key=global`)
 
 name: await this.getName(user)
 gcname: await this.getName(id)
