81afffb040348f6794c5163f6f6f9434304dc1e0
Dede Kurniawan <dede.klender@gmail.com>
2023-09-23 21:56:15 +0700

    update handler.js

--- handler.js
+++ handler.js
@@ -100,7 +100,7 @@ export async function handler(chatUpdate) {
                 if (!('isBanned' in chat))
                     chat.isBanned = false
                 if (!('welcome' in chat))
-                    chat.welcome = false
+                    chat.welcome = true
                 if (!('detect' in chat))
                     chat.detect = false
                 if (!('sWelcome' in chat))
@@ -128,7 +128,7 @@ export async function handler(chatUpdate) {
             } else
                 global.db.data.chats[m.chat] = {
                     isBanned: false,
-                    welcome: false,
+                    welcome: true,
                     detect: false,
                     autoread: true,
                     sWelcome: '',
