9b07bdefe9407230e81eb29c55d8cc65c4737bd2
Dede Kurniawan <dede.klender@gmail.com>
2023-09-23 08:28:23 +0700

    update jadizombie.js

--- plugins/jadizombie.js
+++ plugins/jadizombie.js
@@ -1,27 +1,28 @@
 
 import uploadImage from '../lib/uploadImage'
-import makemeazombie = require('makemeazombie'
-import zombie = new makemeazombie();
+import makemeazombie from 'makemeazombie'
+
+let zombie = new makemeazombie()
 
 async function handler(m, { conn }) {
-var q = m.quoted ? m.quoted : m;
-var mime = (q.msg || q).mimetype || '';
+var q = m.quoted ? m.quoted : m
+var mime = (q.msg || q).mimetype || ''
 if (!mime) {
  return m.reply('Kirim/Balas gambar dengan caption *.jadizombie*')
  }
-var media = await q.download();
+var media = await q.download()
 var gambar = await uploadImage(media)
 zombie.transform({
     photo: `${gambar}`,
     destinyFolder: './tmp'
 })
 .then(data => {
-    conn.sendFile(m.chat, data, 'zombie.jpg', wm, m);
+    conn.sendFile(m.chat, data, 'zombie.jpg', wm, m)
 })
  .catch(err => {
-    console.log('Terjadi kesalahan:', err);
+    console.log('Terjadi kesalahan:', err)
  })
-};
+}
 
 handler.command = handler.help = ['jadizombie']
 handler.tags = ['maker']
