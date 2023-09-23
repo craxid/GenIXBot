00a9f35e786f7053ef6664cc59c3037ee44c6566
Dede Kurniawan <dede.klender@gmail.com>
2023-09-24 06:17:49 +0700

    update jadianime.js

--- plugins/jadianime.js
+++ plugins/jadianime.js
@@ -1,21 +1,20 @@
-
-import fetch from 'node-fetch'
-import uploadImage from '../lib/uploadImage.js'
-import { jadianime } from '..lib/jadianime.js'
+let fetch = require('node-fetch')
+let uploadImage = require('../lib/uploadImage.js')
+import { jadianime } from '../li'
 let handler = async (m, { conn, usedPrefix, command, text }) => {
 let q = m.quoted ? m.quoted : m
 let mime = (q.msg || q).mimetype || ''
 if (!mime) throw 'Kirim/Reply Gambar dengan caption .jadianime'
-m.reply(wait)
+m.reply('Tunggu Sebentar...')
 let media = await q.download()
 let url = await uploadImage(media)
-let hasil = await jadianime(url)
-await conn.sendFile(m.chat, hasil, 'jadianime.jpg', global.wm2, m)
+let hasil = await (await fetch(`https://phototoanime.p.rapidapi.com/draw?rapidapi-key=553e4784d3msh29ae65eb1df940bp158d54jsnc55b268d0b6b&url=${url}`)).buffer()
+await conn.sendFile(m.chat, hasil, '', wm, m)
 	
 }
 handler.help = ['jadianime']
-handler.tags = ['ai']
+handler.tags = ['maker']
 handler.command = /^(jadianime)$/i
-handler.limit = 5
+handler.limit = true
 
-export default handler
+module.exports = handler
