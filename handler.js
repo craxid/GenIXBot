8ba92c4c9b46e6f3ca24a2f2a6085fca54943403
Dede Kurniawan <dede.klender@gmail.com>
2023-09-23 16:49:24 +0700

    update handler.js

--- handler.js
+++ handler.js
@@ -579,8 +579,8 @@ Untuk menyalakan/mematikan fitur, ketik
 
 global.dfail = (type, m, conn) => {
     let mikapp = 'https://telegra.ph/file/bd044275940ed1b62efcd.jpg'
-    let rndm ='https://api.zahwazein.xyz/randomanime/anime?apikey=zenzkey_f59c1aacf2'
-    let animacak =rndm.json()
+    let rndm = 'https://api.zahwazein.xyz/randomanime/anime?apikey=zenzkey_f59c1aacf2'
+
     let msg = {
         rowner: '👑 Hanya untuk *Pemilik bot*',
         owner: '🔱 Hanya untuk *Admin Bot*',
