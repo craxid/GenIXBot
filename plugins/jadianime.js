11022036fdfbf4c2eaa86e1be1088a76e8e3fe8b
Dede Kurniawan <dede.klender@gmail.com>
2023-09-24 08:47:34 +0700

    update jadianime.js

--- plugins/jadianime.js
+++ plugins/jadianime.js
@@ -6,7 +6,7 @@ let handler = async (m, { conn, usedPrefix, command, text }) => {
 let q = m.quoted ? m.quoted : m
 let mime = (q.msg || q).mimetype || ''
 if (!mime) throw 'Kirim/Reply Gambar dengan caption .jadianime'
-m.reply('Tunggu Sebentar...')
+m.reply(wait)
 let media = await q.download()
 let url = await uploadImage(media)
 let hasil = await fetch(`https://vihangayt.me/tools/toanime?url=${url}`).buffer()
