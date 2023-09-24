8b69cfe9647703a744b0e1ec781e814b7350027a
Dede Kurniawan <dede.klender@gmail.com>
2023-09-24 21:54:28 +0700

    update chord.js

--- plugins/chord.js
+++ plugins/chord.js
@@ -10,7 +10,7 @@ m.reply(wait)
 try {
     let chord = await fetch(`https://api.zahwazein.xyz/searching/chordlagu?query=${text}&apikey=zenzkey_f59c1aacf2`)
     
-    chordlagu = chord.json()
+    chordlagu = chord()
 	    
 conn.sendMessage(m.chat, chordlagu.chord, `*[ CHORD LAGU ]*\n`, m)
 	}
