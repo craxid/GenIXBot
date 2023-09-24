4111eb8df812b42b359fc3fe06b931ce66e1ab63
Dede Kurniawan <dede.klender@gmail.com>
2023-09-24 08:39:27 +0700

    update jadianime.js

--- plugins/jadianime.js
+++ plugins/jadianime.js
@@ -1,24 +1,20 @@
-
-import fetch from 'node-fetch'
-import uploadImage from '../lib/uploadImage.js'
-import { jadianime } from '../lib/jadianime.js'
+let fetch = require('node-fetch')
+let uploadImage = require('../lib/uploadImage.js')
 
 let handler = async (m, { conn, usedPrefix, command, text }) => {
 let q = m.quoted ? m.quoted : m
 let mime = (q.msg || q).mimetype || ''
 if (!mime) throw 'Kirim/Reply Gambar dengan caption .jadianime'
-
-m.reply(wait)
-
+m.reply('Tunggu Sebentar...')
 let media = await q.download()
 let url = await uploadImage(media)
-let hasil = await jadianime(url)
-await conn.sendFile(m.chat, hasil, '', global.wm2, m)
+let hasil = await (await fetch(`https://vihangayt.me/tools/toanime?url=https://upload.wikimedia.org/wikipedia/en/d/d7/Harry_Potter_character_poster.jpg${url}`)).buffer()
+await conn.sendFile(m.chat, hasil, '', wm, m)
 	
 }
 handler.help = ['jadianime']
-handler.tags = ['ai']
+handler.tags = ['maker']
 handler.command = /^(jadianime)$/i
-handler.diamond = 4
+handler.limit = true
 
-export default handler
+module.exports = handler
