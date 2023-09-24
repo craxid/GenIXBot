3b2ebf0ea0fb312b223a6e3c711fb10158fb5e12
Dede Kurniawan <dede.klender@gmail.com>
2023-09-24 09:15:15 +0700

    update jadianime.js

--- plugins/jadianime.js
+++ plugins/jadianime.js
@@ -1,21 +1,36 @@
 
-import fetch from 'node-fetch'
+import uploadImage from '../lib/uploadImage.js'
 import uploadImage from '../lib/uploadImage.js'
 
-let handler = async (m, { conn, usedPrefix, command }) => {
+let zombie = new makemeazombie()
+
+async function handler(m, { conn }) {
+    
 let q = m.quoted ? m.quoted : m
 let mime = (q.msg || q).mimetype || ''
-if (!mime) throw 'Kirim/Reply Gambar dengan caption .jadianime'
-m.reply(wait)
+await m.reply(wait)
+
+if (!mime) {
+ return m.reply('Kirim/Balas gambar dengan caption *.jadizombie*')
+
+ }
 let media = await q.download()
-let url = await uploadImage(media)
-let hasil = await fetch(`https://vihangayt.me/tools/toanime?url=${url}`)
-await conn.sendFile(m.chat, hasil, 'jadianime.jpg', (`${global.wm2}`), m)
-	
+let gambar = await uploadImage(media)
+zombie.transform({
+    photo: `${gambar}`,
+    destinyFolder: './tmp'
+})
+.then(data => {
+    conn.sendFile(m.chat, data, 'zombie.jpg', `© Mika Bot`, m)
+})
+ .catch(err => {
+    console.log('Terjadi kesalahan:', err)
+ })
 }
-handler.help = ['jadianime']
+
+handler.command = ['jadizombie']
+handler.help = ['jadizombie']
 handler.tags = ['ai']
-handler.command = /^(jadianime)$/i
 handler.diamond = 3
 
 export default handler
