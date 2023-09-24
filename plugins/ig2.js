b777867a7bda55f658dd08dbe32cfe91922d29dd
Dede Kurniawan <dede.klender@gmail.com>
2023-09-24 16:12:41 +0700

    update ig2.js

---
+++ plugins/ig2.js
@@ -0,0 +1,24 @@
+
+
+import fetch from "node-fetch"
+let limit = 15
+
+var handler = async (m, { conn, text, usedPrefix, command }) => {
+  if (!text) throw `Masukkan URL!\n\nContoh: ${usedPrefix + command} https://www.instagram.com`
+  await m.reply(wait)
+  
+  let idgl = await fetch(`https://vihangayt.me/download/instagram?url=${text}`)
+  let res = await idgl.json()
+  
+  conn.sendMessage(m.chat, res.data.url, 'ig.mp4', 'Done kak!', m)
+}
+
+handler.help = ['ig']
+handler.tags = ['dl']
+handler.command = /^(ig(dl)?)|(instagram(dl)?)$/i
+handler.register = true
+handler.diamond = true
+
+export default handler
+
+//https://vihangayt.me/download/instagram?url=
