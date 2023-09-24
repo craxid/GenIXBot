4e2471a054209d5cf04dec7602dd9ea2bed18dee
Dede Kurniawan <dede.klender@gmail.com>
2023-09-24 15:22:20 +0700

    update warnai.js

---
+++ plugins/warnai.js
@@ -0,0 +1,25 @@
+
+import fetch from 'node-fetch'
+import uploadImage from '../lib/uploadImage.js'
+
+let handler = async (m, { conn, usedPrefix, command, text }) => {
+let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+let name = await conn.getName(who)
+let q = m.quoted ? m.quoted : m
+let mime = (q.msg || q).mimetype || ''
+if (!mime) throw 'Kirim/Reply Gambar dengan caption .warnai'
+m.reply(wait)
+let media = await q.download()
+let url = await uploadImage(media)
+let hasil = await (await conn.getFile(`https://vihangayt.me/tools/toanime?url=${url}`))
+
+await conn.sendFile(m.chat, hasil.data, 'img.jpg', 'Foto ke Anime\n© Mika Bot', m)
+	
+}
+
+handler.help = ['warnai', 'col']
+handler.tags = ['ai']
+handler.command = /^(jadianime)$/i
+handler.diamond = 3
+
+export default handler
