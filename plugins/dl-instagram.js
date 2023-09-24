b4d655016897e21c2dcdc4a47a828e0be0e3ee92
Dede Kurniawan <dede.klender@gmail.com>
2023-09-24 19:14:37 +0700

    update dl-instagram.js

--- plugins/dl-instagram.js
+++ plugins/dl-instagram.js
@@ -7,10 +7,10 @@ var handler = async (m, { conn, text, usedPrefix, command }) => {
   if (!text) throw `Masukkan URL!\n\nContoh: ${usedPrefix + command} https://www.instagram.com`
   await m.reply(wait)
   
-  let idgl = await fetch(`https://api.yanzbotz.my.id/api/downloader/instagram?url=${text}`)
+  let idgl = await fetch(`https://api.arifzyn.xyz/download/instagram?url=${text}`)
   let res = await idgl.json()
   
-  conn.sendMessage(m.chat, res.result.url, 'ig.mp4', 'Done kak!', m)
+  conn.sendMessage(m.chat, res.result.media, 'ig.mp4', 'Done kak!', m)
 }
 
 handler.help = ['igdl','igvid']
