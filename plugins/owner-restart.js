21e7bf73a70ad0c147c84ec2c18e31d04c12828f
Dede Kurniawan <dede.klender@gmail.com>
2023-09-25 07:57:40 +0700

    update owner-restart.js

--- plugins/owner-restart.js
+++ plugins/owner-restart.js
@@ -1,6 +1,6 @@
 import { spawn } from 'child_process'
 let handler = async (m, { conn, isROwner, text }) => {
-    if (!process.send) throw 'Dont: node main.js\nDo: pm2 restart index.js --cron "0 * * * *"'
+    if (!process.send) throw 'Dont: node main.js\nDo: pm2 start index.js --cron "0 * * * *"'
     if (conn.user.jid == conn.user.jid) {
     await m.reply('🔄 Memulai ulang Bot...\n tunggu bentar...')
     process.send('reset')
