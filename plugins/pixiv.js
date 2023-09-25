3de73c3feec701140567456ebb022888392836af
Dede Kurniawan <dede.klender@gmail.com>
2023-09-25 11:02:57 +0700

    update pixiv.js

--- plugins/pixiv.js
+++ plugins/pixiv.js
@@ -19,7 +19,7 @@ contextInfo: {
 externalAdReply: {
 title: (`${username}`),
 body: ('Pixiv Downloader'),
-thumbnailUrl: mikapp,
+thumbnailUrl: (`${liksif.result.}`),
 sourceUrl: ('https://www.facebook.com/dede2015k'),
 mediaType: 1,
 showAdAttribution: true,
