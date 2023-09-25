29f18690c21c78cb0ca36be50cd0cf8198914d79
Dede Kurniawan <dede.klender@gmail.com>
2023-09-25 10:05:02 +0700

    update simivoice.js

---
+++ plugins/simivoice.js
@@ -0,0 +1,25 @@
+import fetch from 'node-fetch'
+import axios from 'axios'
+let handler = m => m
+
+handler.before = async (m) => {
+    let chat = global.db.data.chats[m.chat]
+    if (chat.simivoice && !chat.isBanned ) {
+        if (/^.*false|disnable|(turn)?off|0/i.test(m.text)) return
+        if (!m.text) return
+        let api = await fetch(`https://api.simsimi.net/v2/?text=${m.text}&lc=id`)
+  let res = await api.json()
+  
+  let id = 'id_001'
+  const { data } = await axios.post("https://tiktok-tts.weilnet.workers.dev/api/generation", {
+    "text": res.success,
+    "voice": id
+})
+conn.sendMessage(m.chat, { audio: Buffer.from(data.data, "base64"), mimetype: "audio/mp4" }, {quoted: m})
+        return !0
+    }
+    return true
+}
+
+handler.help = [sim]
+export default handler
