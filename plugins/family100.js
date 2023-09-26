de5952d659daad40abc919bea5f527cb86b018e8
Dede Kurniawan <dede.klender@gmail.com>
2023-09-26 12:50:07 +0700

    update family100.js

---
+++ plugins/family100.js
@@ -0,0 +1,37 @@
+
+import fetch from 'node-fetch'
+
+let timeout = 180000
+let exp = 200
+let handler = async (m, { conn, usedPrefix }) => {
+    conn.tebakkata = conn.tebakkata ? conn.tebakkata : {}
+    let id = m.chat
+    if (id in conn.tebakkata) {
+        conn.reply(m.chat, 'Masih ada soal belum terjawab di chat ini', conn.tebakkata[id][0])
+        throw false
+    }
+   let src = await (await fetch('https://raw.githubusercontent.com/BochilTeam/database/master/games/tebakkata.json')).json()
+    let json = src[Math.floor(Math.random() * src.length)]
+    let caption = `${json.soal}
+
+Timeout *${(timeout / 1000).toFixed(2)} detik*
+Ketik ${usedPrefix}teka untuk bantuan
+Bonus: ${exp} XP
+
+`.trim()
+    conn.tebakkata[id] = [
+        await conn.reply(m.chat, caption, m),
+        json, exp,
+        setTimeout(() => {
+            if (conn.tebakkata[id]) conn.reply(m.chat, `Waktu habis!\nJawabannya adalah *${json.jawaban}*`, conn.tebakkata[id][0])
+            delete conn.tebakkata[id]
+        }, timeout)
+    ]
+}
+handler.help = ['tebakkata']
+handler.tags = ['game']
+handler.command = /^tebakkata/i
+handler.diamond = false
+handler.group = true
+
+export default handler
