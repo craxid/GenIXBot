884f2405990cf36b44f3a6e05197703d4e5b73ff
Dede Kurniawan <dede.klender@gmail.com>
2023-09-26 08:22:08 +0700

    update ncnlstat.js

--- plugins/ncnlstat.js
+++ plugins/ncnlstat.js
@@ -42,7 +42,7 @@ API version: ${sevre.debug.apiversion}
   }}}}}}, {})*/
 
 
-conn.sendMessage(m, {
+conn.sendMessage(m.chat, {
 text: (`${tegs}`).replace('true','Online').replace('undefined','tidak diketahui'),
 contextInfo: {
 mentionJid: m,
