c11b352ef12a1dafc041fa64f56fb42863f111b9
Dede Kurniawan <dede.klender@gmail.com>
2023-09-27 13:50:55 +0700

    update _chatbot.js

--- plugins/_chatbot.js
+++ plugins/_chatbot.js
@@ -14,22 +14,5 @@ return !0
 
 let mika = await fetch(`https://api.azz.biz.id/api/alicia?q=${m.text}&user=Mika%20Bot&key=mangea`)
 
-let res = await mika.json()
-
-const iyhkh = {
-text: res.respon.replace('Alicia','Mika Bot').replace('6283808217881','6285892734104').replace('Ricky','ZuroX Project'),
-contextInfo: {
-externalAdReply: {
-title: ('Chat Bot'),
-body: ('Mika Bot'),
-thumbnailUrl: global.ppmika,
-sourceUrl: ('https://www.facebook.com/dede2015k'),
-mediaType: 1,
-showAdAttribution: true,
-renderLargerThumbnail: true
-}}}
-
-m.reply(m.chat, iyhkh, m)
-
-//m.reply(res.respon.replace('Alicia','Mika Bot').replace('6283808217881','6285892734104').replace('Ricky','ZuroX Project'))
+m.reply(res.respon.replace('Alicia','Mika Bot').replace('6283808217881','6285892734104').replace('Ricky','ZuroX Project'))
 }
