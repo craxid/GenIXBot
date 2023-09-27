5d54af25324469937e5e7b9724b79f67d068cd17
Dede Kurniawan <dede.klender@gmail.com>
2023-09-27 15:52:28 +0700

    update main-donate.js

--- plugins/main-donate.js
+++ plugins/main-donate.js
@@ -2,6 +2,8 @@
 let handler = async(m, { conn, usedPrefix, command }) => {
 
     let don = `
+Scan menggunakan aplikasi bank / e-wallet untuk mendukung Bot
+
 ${global.htki} *${global.packname}* ${global.htka}
 ${global.gy} Dana:  085892734104
 ${global.gy} Pulsa: 083155936234 ( AXIS )
