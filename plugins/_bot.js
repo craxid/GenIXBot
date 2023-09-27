c1a6849ace35a3eeba2273a6c7bc0c64806520d4
Dede Kurniawan <dede.klender@gmail.com>
2023-09-27 17:25:32 +0700

    update _bot.js

--- plugins/_bot.js
+++ plugins/_bot.js
@@ -4,6 +4,8 @@ let name = await conn.getName(m.sender)
 let thumb = 'https://telegra.ph/file/bd044275940ed1b62efcd.jpg'
 let mikapp = 'https://telegra.ph/file/a9a78b769836fe10fa62d.jpg'
 
+let akacn = 'https://nekos.life/api/v2/img/waifu'
+
 let mika = `Halo kak ${name} 👋\nAda yang bisa ${global.packname} bantu?\n\nKetik !help untuk membuka Menu`
 
 let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
@@ -17,7 +19,7 @@ contextInfo: {
 externalAdReply: {
 title: (`${username}`),
 body: ('Aku Online Kak!'),
-thumbnailUrl: (`${global.hwaifu.length}`),
+thumbnailUrl: acakn.length,
 sourceUrl: ('https://www.facebook.com/dede2015k'),
 mediaType: 1,
 showAdAttribution: true,
