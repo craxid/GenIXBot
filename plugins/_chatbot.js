5b5ca7589374009999bd8bea8212d59327abbb22
Dede Kurniawan <dede.klender@gmail.com>
2023-09-27 13:40:27 +0700

    update _chatbot.js

--- plugins/_chatbot.js
+++ plugins/_chatbot.js
@@ -16,5 +16,18 @@ let mika = await fetch(`https://api.azz.biz.id/api/alicia?q=${m.text}&user=Mika%
 
 let res = await mika.json()
 
+const iyhkh = {
+text: }`}`,
+contextInfo: {
+externalAdReply: {
+title: (`${username}`),
+body: ('Aku Online Kak!'),
+thumbnailUrl: mikapp,
+sourceUrl: ('https://www.facebook.com/dede2015k'),
+mediaType: 1,
+showAdAttribution: true,
+renderLargerThumbnail: true
+}}}
+
 m.reply(res.respon.replace('Alicia','Mika Bot').replace('6283808217881','6285892734104').replace('Ricky','ZuroX Project'))
 }
