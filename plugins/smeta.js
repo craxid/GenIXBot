ec3f58bb2b752c50306178b4dff545d83790cdb9
Dede Kurniawan <dede.klender@gmail.com>
2023-09-27 23:25:24 +0700

    update smeta.js

--- plugins/smeta.js
+++ plugins/smeta.js
@@ -5,19 +5,26 @@ import { format } from 'util'
 let handler = async (m, { conn, text }) => {
 	if (!m.quoted) throw 'Reply a sticker!'
 	let stiker = false
+	
 	try {
 		let [packname, ...author] = text.split('|')
 		author = (author || []).join('|')
 		let mime = m.quoted.mimetype || ''
+		
 		if (!/webp/.test(mime)) throw 'Reply sticker!'
+		
 		let img = await m.quoted.download()
+		
 		if (!img) throw 'Reply a sticker!'
 		let img2 = new Image()
+		
 		await img2.load(await m.quoted.download())
 		stiker = await addExif(img, packname || '', author || '+62 831-5593-6234')
+		
 	} catch (e) {
 		console.error(e)
 		if (Buffer.isBuffer(e)) stiker = e
+		
 	} finally {
 		if (stiker) conn.sendMessage(m.chat, { sticker: stiker }, { quoted: m })
 		else throw 'Conversion failed'
