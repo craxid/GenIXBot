65326262f429a1a4ba10b3883d9f726fccf5cd75
Dede Kurniawan <dede.klender@gmail.com>
2023-09-27 14:24:16 +0700

    update _chatbot.js

--- plugins/_chatbot.js
+++ plugins/_chatbot.js
@@ -2,25 +2,6 @@
 import fetch from 'node-fetch'
 
 export async function before(m, { conn }) {
-
-if (m.isBaileys && m.fromMe)
-        return !0
-
-//if (!m.isGroup) return !0
-    let user = global.db.data.users[m.sender]
-    
-if (!user.chatbot)
-return !0
-
-let mika = await fetch(`https://api.azz.biz.id/api/alicia?q=${m.text}&user=Mika%20Bot&key=mangea`)
-
-m.reply(res.respon.replace('Alicia','Mika Bot').replace('6283808217881','6285892734104').replace('Ricky','ZuroX Project'))
-}
-
-
-import fetch from 'node-fetch'
-
-export async function before(m, { conn }) {
 if (m.isBaileys && m.fromMe)
         return !0
     if (!m.isGroup) return !1
@@ -30,6 +11,6 @@ if (m.isBaileys && m.fromMe)
         return !0
         let api = await fetch(`https://api.azz.biz.id/api/alicia?q=${m.text}&user=Mika%20Bot&key=mangea`)
         let res = await api.json()
-        m.reply(res.success.replace('simsimi', 'DyLux').replace('Simsimi', 'DyLux').replace('sim simi', 'DyLux'))
+        m.reply(res.success.replace('Alicia','Mika Bot').replace('6283808217881','6285892734104').replace('Ricky','ZuroX Project'))
     
 }
