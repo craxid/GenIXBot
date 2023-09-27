d2ac65a5e80d845f61a347bbf39751449459bc15
Dede Kurniawan <dede.klender@gmail.com>
2023-09-27 17:18:40 +0700

    update _bot.js

--- plugins/_bot.js
+++ plugins/_bot.js
@@ -17,7 +17,7 @@ contextInfo: {
 externalAdReply: {
 title: (`${username}`),
 body: ('Aku Online Kak!'),
-thumbnailUrl: hwaifu.length,
+thumbnailUrl: global.hwaifu.length,
 sourceUrl: ('https://www.facebook.com/dede2015k'),
 mediaType: 1,
 showAdAttribution: true,
