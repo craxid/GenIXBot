df8f05315257fcba6a72fd3ef05a3a1b3d300b45
Dede Kurniawan <dede.klender@gmail.com>
2023-09-27 07:59:32 +0700

    update _chatbot.js

--- plugins/_chatbot.js
+++ plugins/_chatbot.js
@@ -1,20 +1,3 @@
-/*
-import fetch from 'node-fetch'
-
-export async function before(m, { conn }) {
-if (m.isBaileys && m.fromMe)
-        return !0
-    if (!m.isGroup) return !1
-    let user = global.db.data.users[m.sender]
-    
-      if (!user.chatbot)
-        return !0
-        let api = await fetch(`https://api.azz.biz.id/api/alicia?q=${text}&user=Mika%20Bot&key=mangea`)
-        let res = await api.json()
-        m.reply(res.respon.replace('alicia','Mika Bot'))
-    
-}
-*/
 
 import fetch from 'node-fetch'
 
