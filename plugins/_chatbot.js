4babbf5f84a5233622e500a0fe5dbaaf8c90c47b
Dede Kurniawan <dede.klender@gmail.com>
2023-09-27 07:49:07 +0700

    update _chatbot.js

--- plugins/_chatbot.js
+++ plugins/_chatbot.js
@@ -29,9 +29,9 @@ if (!m.isGroup) return !0
 if (!user.chatbot)
 return !0
 
-let mika = await fetch(`https://api.zahwazein.xyz/entertainment/simisimi?text=${m.text}&apikey=zenzkey_f59c1aacf2`)
+let mika = await fetch(`https://api.azz.biz.id/api/alicia?q=${text}&user=Mika%20Bot&key=mangea`)
 
 let res = await mika.json()
 
-m.reply(res.result.message.replace('darling','Sensei').replace('dear','Sensei').replace('dearest','Sensei').replace('sayang','Sensei').replace('Anda','Sensei').replace('Hey there','Hey Sensei').replace('Hi there','Hi Sensei').replace('Hello there', 'Hello Sensei').replace('Anda','Kamu').replace('simisimi','Mika').replace('enter text params','aku gak bisa bales setiker kak'))
+m.reply(res.result.message.replace('Alicia','Mika Bot'))
 }
