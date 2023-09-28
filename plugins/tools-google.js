4033794e0719b4690ef48b77f1991a6162c0b8aa
Dede Kurniawan <dede.klender@gmail.com>
2023-09-28 11:03:19 +0700

    update tools-google.js

--- plugins/tools-google.js
+++ plugins/tools-google.js
@@ -52,7 +52,7 @@ let handler = async (m, { conn, command, args }) => {
 }
 handler.help = ['google'].map(v => v + ' <search>')
 handler.tags = ['internet']
-handler.command = /^google?$/i
+handler.command = /^googlef?$/i
 handler.owner = false
 handler.mods = false
 handler.premium = false
