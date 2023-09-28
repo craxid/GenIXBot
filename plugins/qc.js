ad45ba230f09b68ec4245297e82eb3d2d6f7888b
Dede Kurniawan <dede.klender@gmail.com>
2023-09-28 17:33:00 +0700

    update qc.js

--- plugins/qc.js
+++ plugins/qc.js
@@ -1,4 +1,5 @@
-import { sticker } from '../lib/sticker.js'
+
+/*import { sticker } from '../lib/sticker.js'
 import axios from 'axios'
 
 let handler = async (m, { conn, text }) => {
@@ -42,5 +43,49 @@ handler.tags = ['sticker']
 handler.command = /^(qc|quoted|quotly)$/i
 
 handler.limit = true
+
+export default handler
+*/
+
+import axios from 'axios'
+import fs from 'fs'
+import { sticker } from '../lib/sticker.js'
+
+let handler = async (m, { conn, text }) => {
+if (!text) throw 'Masukan Text'
+let name = await conn.getName(m.sender)
+let avatar = await conn.profilePictureUrl(m.sender, 'image').catch(_=> "https://telegra.ph/file/a4ec75f6ce8b2b565a3e3.png")
+const json = {
+  "type": "quote",
+  "format": "png",
+  "backgroundColor": "#FFFFFF",
+  "width": 512,
+  "height": 768,
+  "scale": 2,
+  "messages": [
+    {
+      "entities": [],
+      "avatar": true,
+      "from": {
+        "id": 1,
+        "name": name,
+        "photo": {
+          "url": avatar
+        }
+      },
+      "text": text,
+      "replyMessage": {}
+    }
+  ]
+};
+const res = await axios.post('https://bot.lyo.su/quote/generate', 
+  json, 
+{
+  headers: {'Content-Type': 'application/json'}
+})
+const buffer = await sticker(Buffer.from(res.data.result.image, 'base64'), false, "By ArifzynXD", "Bot WhatsApp")
+conn.sendFile(m.chat, buffer, 'sticker.webp', '', m)
+}
+handler.command = ['qc']
 
 export default handler
