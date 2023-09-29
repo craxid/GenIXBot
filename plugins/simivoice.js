bdb2e49f55d0c2d148f9fb49409bfdbea893869c
Dede Kurniawan <dede.klender@gmail.com>
2023-09-29 16:52:46 +0700

    update simivoice.js

--- plugins/simivoice.js
+++ plugins/simivoice.js
@@ -8,7 +8,7 @@ m.react(rwait)
 try {
 let simiv = await conn.getFile(`https://api.yanzbotz.my.id/api/ai/simivoice?query=${text}`)
 
-await m.reply(simiv.data)
+await m.reply(simiv.data, 'audio.mp3', '', m, true, { mimetype: 'audio/mp4' })
 
 m.react(done)
 }
