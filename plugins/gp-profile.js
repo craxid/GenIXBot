20ff77710f74eb4d0703bff2f72ce682fdd3d22c
Dede Kurniawan <dede.klender@gmail.com>
2023-09-30 06:41:54 +0700

    update gp-profile.js

--- plugins/gp-profile.js
+++ plugins/gp-profile.js
@@ -7,7 +7,7 @@ let handler = async (m, { conn, usedPrefix, command}) => {
 
 let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 if (!(who in global.db.data.users)) throw `✳️ Pengguna tidak ada di database`
-let pp = await conn.profilePictureUrl(who, 'image').catch(_ => '../src/avatar_contact.png')
+let pp = await conn.profilePictureUrl(who, 'image').catch(_ => './src/avatar_contact.png')
 let user = global.db.data.users[who]
 let { name, exp, diamond, lastclaim, registered, regTime, age, level, role, warn } = global.db.data.users[who]
 let { min, xp, max } = xpRange(user.level, global.multiplier)
