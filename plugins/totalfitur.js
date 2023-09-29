e6e5ae563f0df44f49f7dd5a78b119af6e2032d8
Dede Kurniawan <dede.klender@gmail.com>
2023-09-29 22:17:05 +0700

    update totalfitur.js

--- plugins/totalfitur.js
+++ plugins/totalfitur.js
@@ -8,7 +8,7 @@ let totalf = Object.values(global.plugins).filter(
    let thumb = 'https://telegra.ph/file/f23a8239b80c0896b407c.jpg'
  //await conn.sendFile(m.chat, thumb, 'fitur.jpg', `Total Fitur saat ini: ${totalf}`, m)
 
-/*
+
 const iyhkh = {
 text: (`Total Fitur: ${totalf}`),
 contextInfo: {
@@ -20,9 +20,9 @@ sourceUrl: ('https://www.facebook.com/dede2015k'),
 mediaType: 1,
 showAdAttribution: true,
 renderLargerThumbnail: true
-}}}*/
+}}}
 
-await conn.sendMessage(m.chat, `Total fitur saat ini:` totalf, m)
+await conn.sendMessage(m.chat, iyhkh, m)
 
 }
 
