87ef2dea7ca3131dd63a3c6b809bed0a78d5fbc7
Dede Kurniawan <dede.klender@gmail.com>
2023-09-29 09:44:20 +0700

    update simivoice.js

--- plugins/simivoice.js
+++ plugins/simivoice.js
@@ -6,7 +6,7 @@ if (!text) throw `✳️ Masukkan Teks\n\nContoh: !simivoice Hai`
 await m.reply(wait)
 
 try {
-let simiv = await fetch(`https://api.yanzbotz.my.id/api/ai/simivoice?query=${text}`)
+let simiv = await getFile(`https://api.yanzbotz.my.id/api/ai/simivoice?query=${text}`)
 
 await conn.sendFile(m.chat, simiv.data, 'simi.opus')
 }
