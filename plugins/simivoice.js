673b689c09fd91c1195a8bb212f63314b08b9473
Dede Kurniawan <dede.klender@gmail.com>
2023-09-29 09:31:31 +0700

    update simivoice.js

--- plugins/simivoice.js
+++ plugins/simivoice.js
@@ -2,9 +2,8 @@
 import fetch from 'node-fetch'
 let handler = async (m, { text }) => {
   let res = await getFile(`https://api.yanzbotz.my.id/api/ai/simivoice?query=${text}`)
-  let json = await res.json()
-  
-conn.sendFile(m.chat, res.data, 'simi.opus', m)
+
+await conn.sendFile(m.chat, res.data, 'simi.opus', m)
 
   else throw json
 }
