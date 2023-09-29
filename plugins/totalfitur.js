1a5884fb509e5b92e330ee670dce97908d474522
Dede Kurniawan <dede.klender@gmail.com>
2023-09-29 21:59:09 +0700

    update totalfitur.js

--- plugins/totalfitur.js
+++ plugins/totalfitur.js
@@ -1,15 +1,30 @@
 
-import fs from 'fs'
-
+im fs from 'fs'
 let handler = async (m, { conn, args, command }) => {
 let totalf = Object.values(global.plugins).filter(
     (v) => v.help && v.tags
   ).length;
-   let thumb = `${global.ppmika}`
- await conn.sendFile(m.chat, thumb, 'fitur.jpg', `Total Fitur saat ini: ${totalf}`, m)
+   let thumb = 'https://telegra.ph/file/f23a8239b80c0896b407c.jpg'
+ //await conn.sendFile(m.chat, thumb, 'fitur.jpg', `Total Fitur saat ini: ${totalf}`, m)
+ 
+ const iyhkh = {
+text: Total Fitur: (`${totalf}`),
+contextInfo: {
+externalAdReply: {
+title: (`Total Fitur`),
+body: totalf,
+thumbnailUrl: thumb,
+sourceUrl: ('https://www.facebook.com/dede2015k'),
+mediaType: 1,
+showAdAttribution: true,
+renderLargerThumbnail: true
+}}}
+
+await conn.sendMessage(m.chat, iyhkh, m)
+
 }
 
 handler.help = ['totalfitur']
-handler.tags = ['main']
+handler.tags = ['info']
 handler.command = ['totalfitur']
 export default handler
