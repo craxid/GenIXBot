c7d2eda33dfc6fb603e350c0305cadb93afe919a
Dede Kurniawan <dede.klender@gmail.com>
2023-09-29 21:53:09 +0700

    update totalfitur.js

--- plugins/totalfitur.js
+++ plugins/totalfitur.js
@@ -5,7 +5,7 @@ let handler = async (m, { conn, args, command }) => {
 let totalf = Object.values(global.plugins).filter(
     (v) => v.help && v.tags
   ).length;
-   let thumb = 'https://telegra.ph/file/f23a8239b80c0896b407c.jpg'
+   let thumb = `${global`
  await conn.sendFile(m.chat, thumb, 'fitur.jpg', `Total Fitur saat ini: ${totalf}`, m)
 }
 
