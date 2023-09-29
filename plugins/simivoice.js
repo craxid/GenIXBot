ac5ace516987f773ba06c876d5f7382356b65d92
Dede Kurniawan <dede.klender@gmail.com>
2023-09-29 09:54:12 +0700

    update simivoice.js

--- plugins/simivoice.js
+++ plugins/simivoice.js
@@ -8,11 +8,11 @@ handler.before = async (m) => {
         if (/^.*false|disnable|(turn)?off|0/i.test(m.text)) return
         if (!m.text) return
         let api = await fetch(`https://api.simsimi.net/v2/?text=${m.text}&lc=id`)
-  let res = await api.json()
+  let simiv = await api.json()
   
   let id = 'id_001'
   const { data } = await axios.post("https://tiktok-tts.weilnet.workers.dev/api/generation", {
-    "text": res.success,
+    "text": simiv.success,
     "voice": id
 })
 conn.sendMessage(m.chat, { audio: Buffer.from(data.data, "base64"), mimetype: "audio/mp4" }, {quoted: m})
