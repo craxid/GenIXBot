80a2eba95d0f8895d708f977f479cc1a5cdb70de
Dede Kurniawan <dede.klender@gmail.com>
2023-09-29 20:57:00 +0700

    update _bot.js

--- plugins/_bot.js
+++ plugins/_bot.js
@@ -24,7 +24,7 @@ showAdAttribution: true,
 renderLargerThumbnail: true
 }}}
 
-conn.sendMessage(m.chat, {quoted: iyhkh})
+conn.sendMessage(m.chat, m, {quoted: iyhkh})
 
 }
 handler.customPrefix = /^(bot|bot?|bott|mika|oy|bang|banh|p)$/i
