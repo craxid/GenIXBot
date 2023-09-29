258095484ffb68785a7e6cfc0febeb2154b10136
Dede Kurniawan <dede.klender@gmail.com>
2023-09-29 09:25:17 +0700

    update simivoice.js

--- plugins/simivoice.js
+++ plugins/simivoice.js
@@ -4,7 +4,7 @@ let handler = async (m, { text }) => {
   let res = await fetch(`https://api.zahwazein.xyz/entertainment/simisimi?text=${text}&apikey=zenzkey_f59c1aacf2`)
   let json = await res.json()
   
-  if (json.result) m.reply(json.data)
+  if (json.result) m.reply(json.data, '')
   else throw json
 }
 handler.help = ['simi', 'simsimi', 'simih'].map(v => v + ' <teks>')
