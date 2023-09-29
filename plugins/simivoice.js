524e6c14d6446b68521f296606a2b1e0ef202806
Dede Kurniawan <dede.klender@gmail.com>
2023-09-29 09:33:16 +0700

    update simivoice.js

--- plugins/simivoice.js
+++ plugins/simivoice.js
@@ -4,8 +4,9 @@ let handler = async (m, { text }) => {
   let res = await getFile(`https://api.yanzbotz.my.id/api/ai/simivoice?query=${text}`)
 
 await conn.sendFile(m.chat, res.data, 'simi.opus', m)
-
-  else throw json
+catch {
+		m.reply(`❎ Error: Ada sebuah kesalahan`)
+	}
 }
 handler.help = ['simivoice', 'simiv'].map(v => v + ' <teks>')
 handler.tags = ['fun']
