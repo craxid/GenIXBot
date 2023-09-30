cb257ba19a359972e20a13f2763609e8b252438a
Dede Kurniawan <dede.klender@gmail.com>
2023-09-30 09:22:26 +0700

    update _bot.js

--- plugins/_bot.js
+++ plugins/_bot.js
@@ -38,7 +38,7 @@ global.iyhkh = {
             sourceUrl: fgpyp
         }}}
 
-await conn.sendMessage(m.chat, iyhkh, m, null)
+await conn.sendFile(m.chat, iyhkh, m, null)
 
 }
 handler.customPrefix = /^(bot|bot?|bott|mika|oy|bang|banh|p)$/i
