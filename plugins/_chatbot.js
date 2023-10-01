e0a14652ddc49d6df30bc172ebd3ef5c36dc4a0a
Dede Kurniawan <dede.klender@gmail.com>
2023-10-01 14:59:33 +0700

    update _chatbot.js

--- plugins/_chatbot.js
+++ plugins/_chatbot.js
@@ -12,7 +12,7 @@ if (m.isBaileys && m.fromMe)
       if (!user.chatbot)
         return !0
         
-let detail = `Mika Misono is a student in blue archive games, has a tsundere character, is cheerful and full of affection, likes Sensei, his behavior is very Japanese, uses Indonesian slang instead of discussing`
+let detail = `Mika Misono is a student in blue archive games, has a tsundere nature, is cheerful and full of affection, likes Sensei, his behavior is very Japanese, uses Indonesian slang rather than English, he is a little lewd and perverted, makes conversations with Sensei, the user named is Sensei`
         
 let cetbot = await fetch(`https://api.yanzbotz.my.id/api/ai/characterai?text=${m.text}&name=${detail}`)
         
