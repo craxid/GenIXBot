4c82eb8c8448e7a4bd7705bc92dda8521b6488e4
Dede Kurniawan <dede.klender@gmail.com>
2023-10-01 12:35:15 +0700

    update _chatbot.js

--- plugins/_chatbot.js
+++ plugins/_chatbot.js
@@ -12,7 +12,7 @@ if (m.isBaileys && m.fromMe)
       if (!user.chatbot)
         return !0
         
-        let cetbot = await fetch(`https://api.yanzbotz.my.id/api/ai/characterai?text=${m.text}&name=Akebi%20Komichi%20from%20anime%20(Akebi-chan%20no%20Sailor-fuku)`)
+        let cetbot = await fetch(`https://api.yanzbotz.my.id/api/ai/characterai?text=${m.text}&name=`)
         
         let res = await cetbot.json()
         
