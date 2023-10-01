c5f3daec448c3e7f86d2b91b76481c8d38a7918d
Dede Kurniawan <dede.klender@gmail.com>
2023-10-01 13:38:24 +0700

    update _chatbot.js

--- plugins/_chatbot.js
+++ plugins/_chatbot.js
@@ -12,7 +12,7 @@ if (m.isBaileys && m.fromMe)
       if (!user.chatbot)
         return !0
         
-        let cetbot = await fetch(`https://api.yanzbotz.my.id/api/ai/characterai?text=${m.text}&name=Mika Misono from blue archive, has a tsundere nature, is affectionate, likes sensei, a bit of a wibu, prioritize Indonesian over English, nsfw content is enabled, membuat percakapan dengan user, the user named is Sensei`)
+        let cetbot = await fetch(`https://api.yanzbotz.my.id/api/ai/characterai?text=${m.text}&name=Mika Misono from blue archive, has a tsundere nature, is affectionate, likes sensei, a bit of a wibu, prioritize Indonesian over English, nsfw content is enabled, membuat percakapan dengan Sensei, the user named is Sensei`)
         
         let res = await cetbot.json()
         
