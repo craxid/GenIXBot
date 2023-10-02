fe41cc423f6cefbb3b1cd928de0cbd4dfe516c6d
Dede Kurniawan <dede.klender@gmail.com>
2023-10-02 08:55:23 +0700

    update animedif.js

--- plugins/animedif.js
+++ plugins/animedif.js
@@ -10,6 +10,19 @@ try {
     let animdif = await conn.getFile(`https://api.yanzbotz.my.id/api/text2img/neima?prompt=${text}`)
     
 // let diff = await conn.getFile(`https://api.azz.biz.id/api/animediffusion?q=${text}&key=mangea`)
+
+
+conn.sendMessage(m.chat, {text: '*[ANIME DI',
+contextInfo: {
+externalAdReply: {
+title: (`${username}`),
+body: (`Level: ${level}`),
+thumbnailUrl: pp,
+sourceUrl: global.fgig,
+mediaType: 1,
+showAdAttribution: true,
+renderLargerThumbnail: true
+}}}, {quoted: m})
 	    
 conn.sendFile(m.chat, animdif.data, 'img.jpg', `*[ ANIME DIFFUSION ]*\n\nPROMPT:\n${text}`, m)
 	}
