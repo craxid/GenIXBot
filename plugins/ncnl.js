e736f31d2a6aa292ccd003cc8fea9d7e33ec03a2
Dede Kurniawan <dede.klender@gmail.com>
2023-10-02 22:48:03 +0700

    update ncnl.js

--- plugins/ncnl.js
+++ plugins/ncnl.js
@@ -10,16 +10,17 @@ let sevre = await ncnlstat.json()
 
 let tegs = `
 ${global.htki} *NCNL Server* ${global.htka}
-${global.gz} *Versi:* ${sevre.version}
+${global.gz} *Version:* ${sevre.version}
 ${global.gz} *MOTD:* ${sevre.motd.clean}
-${global.gz} *Nama Dunia:* ${sevre.map.clean}
-${global.gz} *Mode:* ${sevre.gamemode}
+${global.gz} *World Name:* ${sevre.map.clean}
+${global.gz} *Game Mode:* ${sevre.gamemode}
 ${global.gz} *Player:* ${sevre.players.online}/${sevre.players.max}
 ${global.gz} *Status:* ${sevre.online}
 ${global.sb}
 
 ${global.htki} *HOST & PORT* ${global.htka}
 ${global.gz} *Host:* ${sevre.hostname}
+${global.gz} *IP:* ${sevre.ip}
 ${global.gz} *Port:* ${sevre.port}
 ${global.sb}
 
@@ -36,7 +37,7 @@ sourceUrl: ('minecraft://?addExternalServer=NCNL Legacy|play.ncnl.eu.org:40383')
 mediaType: 1,
 showAdAttribution: true,
 renderLargerThumbnail: false
-}}}, {quoted: m})
+}}})
 
 	}
 	catch {
