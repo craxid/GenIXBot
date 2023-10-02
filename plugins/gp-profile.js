9d1e57fc7cf0bf3842dd0054e58b4048b63bbe44
Dede Kurniawan <dede.klender@gmail.com>
2023-10-02 08:35:13 +0700

    update gp-profile.js

--- plugins/gp-profile.js
+++ plugins/gp-profile.js
@@ -21,8 +21,7 @@ let teg = `@${who.replace(/@.+/, '')}`
 let str = `
 ┌───「 *Profil* 」
 │ *🔖 Nama:* 
-   • ${${registered ?}
-   • @${who.replace(/@.+/, '')}
+   • ${username${registered ? '\n   • ' + name + ' ': ''}
 │ *📱 Nomor:* ${PhoneNumber('+' + who.replace('@s.whatsapp.net', '')).getNumber('international')}
 │ *🔗 Link:* wa.me/${who.split`@`[0]}${registered ? '\n▢ *🎈 Umur*: ' + age + ' Tahun' : ''}
 │ *⚠️ Peringatan:* ${warn}/${maxwarn}
@@ -31,7 +30,7 @@ let str = `
 │ *⬆️ XP*: Total ${exp} (${user.exp - min} / ${xp})\n${math <= 0 ? `bisa untuk meningkatkan level *${usedPrefix}levelup*` : `Kurang _*${math}xp*_ untuk naik ke tingkat berikutnya`}
 │ *🏆 Rank:* ${role}
 │ *📇 Terdaftar:* ${registered ? 'Ya': 'Tidak'}
-│ *⭐ Premium*: ${prem ? 'Ya' : 'Tidak'}
+▢ *⭐ Premium*: ${prem ? 'Ya' : 'Tidak'}
 └──────────────`
     //conn.sendFile(m.chat, pp, 'profil.jpg', str, m, false, { mentions: [who] })
    let lepellu = ('Level:')
