1593f100e4e7c003f3a8ac9ed61ad950cde9fced
Dede Kurniawan <dede.klender@gmail.com>
2023-10-02 23:17:11 +0700

    update ncnlstat.js

--- plugins/ncnlstat.js
+++ plugins/ncnlstat.js
@@ -5,49 +5,49 @@ let thumb = 'https://telegra.ph/file/016c521fb63069af28cec.jpg'
 await m.reply(wait)
 
 try {
-let ncnlstat = await fetch(`https://api.mcsrvstat.us/bedrock/3/craft.pe`)
+let ncnlstat = await fetch(`https://api.mcsrvstat.us/bedrock/3/`)
 let sevre = await ncnlstat.json()
 
 let tegs = `
 ${global.htki} *NCNL Server* ${global.htka}
-${global.gz} *Version:* ${sevre.version}
+${global.gz} *Versi:* ${sevre.version}
 ${global.gz} *MOTD:* ${sevre.motd.clean}
-${global.gz} *World Name:* ${sevre.map.clean}
-${global.gz} *Game Mode:* ${sevre.gamemode}
-${global.gz} *Player:* ${sevre.players.online}/${sevre.players.max}
+${global.gz} *Nama Dunia:* ${sevre.map.clean}
+${global.gz} *Mode:* ${sevre.gamemode}
+${global.gz} *Pemain:* ${sevre.players.online}/${sevre.players.max}
 ${global.gz} *Status:* ${sevre.online}
 ${global.sb}
 
 ${global.htki} *HOST & PORT* ${global.htka}
 ${global.gz} *Host:* ${sevre.hostname}
-${global.gz} *IP:* ${sevre.ip}
 ${global.gz} *Port:* ${sevre.port}
 ${global.sb}
 
 `
     
-await conn.sendMessage(m.chat, {text: tegs,
+conn.sendMessage(m.chat, {
+text: (`${tegs}`).replace('true','Online','false','Offline'),
 contextInfo: {
 externalAdReply: {
 title: (`Server Stalker`),
 body: (`${sevre.hostname}:${sevre.port}`),
 thumbnailUrl: thumb,
-sourceUrl: ('minecraft://?addExternalServer=NCNL Legacy|craft.pe:19132'),
+sourceUrl: ('minecraft://?addExternalServer=NCNL Legacy|play.ncnl.eu.org:40383'),
 mediaType: 1,
 showAdAttribution: true,
 renderLargerThumbnail: false
 }}}, {quoted: m})
 
 	}
-/*catch {
+	catch {
 		m.reply(`👾 Server sedang offline\n\nHost: play.ncnl.eu.org\nPort: 40383`)
-	}*/
+	}
 //end
 
 }
 handler.help = ['mcstalk']
 handler.customPrefix = /^mcstalk$/i
 handler.command = new RegExp
-handler.group = false
+handler.group = true
 
 export default handler
