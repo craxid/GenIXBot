84a1c2ed9be36a52911225a92542f0e52362501e
Dede Kurniawan <dede.klender@gmail.com>
2023-10-05 09:26:41 +0700

    update sticker-wm.js

--- plugins/sticker-wm.js
+++ plugins/sticker-wm.js
@@ -22,6 +22,6 @@ let handler = async (m, { conn, text, args }) => {
 }
 handler.help = ['take <Nama Paket>|<Author>']
 handler.tags = ['sticker']
-handler.command = ['take', 'wm'] 
+handler.command = ['take', 'wm', 'colong'] 
 
 export default handler
