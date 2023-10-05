49a78db2b5a6e73b5754ff67290fc3b3660fc9b1
Dede Kurniawan <dede.klender@gmail.com>
2023-10-05 09:27:58 +0700

    update sticker-emojimix.js

--- plugins/sticker-emojimix.js
+++ plugins/sticker-emojimix.js
@@ -6,7 +6,7 @@ let handler = async (m, { conn, text, args, usedPrefix, command }) => {
 if (!args[0]) throw `📌 Contoh : ${usedPrefix + command} 😎+🤑`
 if (!text.includes('+')) throw  `✳️ Pisahkan dengan *+* \n\n📌 Contoh : \n*${usedPrefix + command}* 😎+🤑`
 let [emoji, emoji2] = text.split`+`
-let anu = await (await fetch(`https://tenor.googleapis.com/v2/featured?key=AIzaSyAyimkuYQYF_FXVALexPuGQctUWRURdCYQ&contentfilter=high&media_filter=png_transparent&component=proactive&collection=emoji_kitchen_v5&q=${encodeURIComponent(emoji)}_${encodeURIComponent(emoji2)}`) ).json()  
+let anu = await (await fetch(`https://tenor.googleapis.com/v2/featured?key=AIzaSyAyimkuYQYF_FXVALexPuGQctUWRURdCYQ&contentfilter=high&media_filter=png_transparent&component=proactive&collection=emoji_kitchen_v6&q=${encodeURIComponent(emoji)}_${encodeURIComponent(emoji2)}`) ).json()  
 for (let res of anu.results) {
 let stiker = await sticker(false, res.url, global.packname, global.author)
 conn.sendFile(m.chat, stiker, null, { asSticker: true }, m)
