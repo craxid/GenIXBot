ec01511e7ed0296eff20e3bc5988a00efa00055d
Dede Kurniawan <dede.klender@gmail.com>
2023-10-05 16:18:45 +0700

    update apkdl.js

--- plugins/apkdl.js
+++ plugins/apkdl.js
@@ -18,8 +18,8 @@ return m.reply(`
 ${global.htki} *Mika Bot APK-DL* ${global.htka}
 ${global.gx} *⚖️ Nama Aplikasi* : ${apkdl.data.name}
 ${global.gx} *🎞️ Size* : ${apkdl.data.size}
-
-${global. _File melebihi batas unduhan_ *+${limit} MB*`)
+${global.
+${global.gy} _File melebihi batas unduhan_ *+${limit} MB*`)
 
 let detil = `
 *Nama Aplikasi:* ${apkdl.data.name}
