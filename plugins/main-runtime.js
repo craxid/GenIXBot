25ac2b3f587acac88420f81b4b3e67822253fa5b
Dede Kurniawan <dede.klender@gmail.com>
2023-10-05 07:01:36 +0700

    update main-runtime.js

--- plugins/main-runtime.js
+++ plugins/main-runtime.js
@@ -22,5 +22,5 @@ function clockString(ms) {
   let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000) % 24
   let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
   let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
-  return [d, 'd ', h, 'h ', m, 'm ', s, 's '].map(v => v.toString().padStart(2, 0)).join('')
+  return [d, 'Hari ', h, 'Jam ', m, 'Menit ', s, 'Detik '].map(v => v.toString().padStart(2, 0)).join('')
 }
