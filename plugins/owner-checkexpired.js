76fe894e73065032b135e52a59af03a926d565dc
Dede Kurniawan <dede.klender@gmail.com>
2023-10-05 15:33:45 +0700

    update owner-checkexpired.js

--- plugins/owner-checkexpired.js
+++ plugins/owner-checkexpired.js
@@ -1,7 +1,7 @@
 
 let handler = async (m, { conn, args, usedPrefix, command }) => {
 	
-	if (global.db.data.chats[m.chat].expired < 1) throw `✳️`
+	if (global.db.data.chats[m.chat].expired < 1) throw `✳️ Gruo ini tidak diatur masa kedaluarsanya`
     let who
     if (m.isGroup) who = args[1] ? args[1] : m.chat
     else who = args[1]
@@ -9,7 +9,7 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
     var nDays = 86400000 * args[0]
     var now = new Date() * 1
     
-    m.reply(`✳️ Su alquiler expira en 
+    m.reply(`✳️ 
     
     ${msToDate(global.db.data.chats[who].expired - now)}
 
