38d36b6824857346d751ab099b7be7856276660b
Dede Kurniawan <dede.klender@gmail.com>
2023-10-05 09:56:37 +0700

    update dl-ytv.js

--- plugins/dl-ytv.js
+++ plugins/dl-ytv.js
@@ -1,7 +1,9 @@
 
 import fg from 'api-dylux'
 import { youtubedl, youtubedlv2 } from '@bochilteam/scraper'
+
 let limit = 14
+
 let handler = async (m, { conn, args, isPrems, isOwner, usedPrefix, command }) => {
 	if (!args || !args[0]) throw `✳️ Contoh :\n${usedPrefix + command} https://youtu.be/YzkTFFwxtXI`
     if (!args[0].match(/youtu/gi)) throw `❎ link YouTube tidak valid`
