11f254a84445f8b99ef341b2a1038a036815dca2
Dede Kurniawan <dede.klender@gmail.com>
2023-10-05 16:08:48 +0700

    update apkdl.js

--- plugins/apkdl.js
+++ plugins/apkdl.js
@@ -11,7 +11,7 @@ try {
 let ling = await fetch(`https://vihangayt.me/download/apk?id=${text}`)
 let apkdl = await ling.json()
 let apkname = `Nama Aplikasi : ${apkdl.data.name}.apk`
-let ukur = `apkdl.s`
+let ukur = `${apkdl.size}`
 
 if (size.split('MB')[0] >= limit)
 return m.reply(`
