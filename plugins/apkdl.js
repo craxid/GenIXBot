b5082e833b5d4f6193b62ba28cf1ad7c72325817
Dede Kurniawan <dede.klender@gmail.com>
2023-10-05 12:39:13 +0700

    update apkdl.js

--- plugins/apkdl.js
+++ plugins/apkdl.js
@@ -34,7 +34,7 @@ renderLargerThumbnail: false
 }}},
 {quoted: m})
 
-await conn.sendFile(m.chat, apkdl.data.dllink, (`${apkname}.apk`), apkname, m)
+await conn.sendMessage(m.chat, apkdl.data.dllink, (`${apkdl.data.name}.apk`), m)
 
  }
  catch {
