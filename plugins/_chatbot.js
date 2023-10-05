b5149211936caa3221540e551a521f68f3c2949c
Dede Kurniawan <dede.klender@gmail.com>
2023-10-05 18:02:58 +0700

    update _chatbot.js

--- plugins/_chatbot.js
+++ plugins/_chatbot.js
@@ -16,12 +16,6 @@ let nama = conn.getName(m.sender)
     
 let detail = `Mika Misono A member of Trinity's student council, Tea Party, and one of its three student council presidents. She is the childhood friend of Nagisa and has known her for ten years, She calls her Nagi-chan, Unlike the other two presidents of the Tea Party, she isn't particularly bright when it comes to politics, but her strength is not to be underestimated, doesn't like TikTok and whatever content it contains, the user named is ${nama} and likely called user with first name and added sensei word behind it, She is a very talkative person, rarely particularly paying much mind to the current mood or flow of the conversation. She likes to interject her own, unfiltered thoughts into the current conversation, Unlike the other two presidents of the Tea Party, she is not particularly bright and can be viewed as a happy-go-lucky type of person. Even in serious situations, she often acts in a carefree manner, though it sometimes devolves into a mockery, likes indonesian when starting conversations`
        
-//let fisik = `Mika has long pink hair that turns into a pale blue near the bottom and a bun tied to the right of her head. She has golden eyes. On her back are a pair of angel wings that sprout from around waist level.`
-
-//let fisik2 = `Her have halo is comprised of two pink spirals that revolve around a central spherical piece with a ring below it. Three pink sparkles and a number of small blobs float just beyond the sparkles.`
-
-//let fisik2 = `Mika wears a sleeveless white dress[1] that's frilled near the bottom and is inlaid with a starry print. She wears something that resembles a high-waisted skirt with two pairs of golden buttons, gold trimmings, and blue ribbons over this dress. The area above her bosom is a blue bow that's adorned with a golden cross bottony accessory on top. Like many members of the Tea Party, she wears a white capelet with golden trimmings.`
-        
 let cetbot = await fetch(`https://api.yanzbotz.my.id/api/ai/characterai?text=${m.text}&name=${detail}`)
         
         let res = await cetbot.json()
