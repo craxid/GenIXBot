8e1ea98966c314e0f01c2dd058528b088c9c356c
Dede Kurniawan <dede.klender@gmail.com>
2023-10-05 11:26:51 +0700

    update apkdl.js

--- plugins/apkdl.js
+++ plugins/apkdl.js
@@ -1,1 +1,8 @@
 //https://vihangayt.me/download/apk?id=
+
+import fetch from 'node-fetch'
+let handler = async (m, { conn, text }) => {
+    
+try {
+let ling = await fetch(`https://vihangayt.me/download/apk?id=${`)
+let zenzapi = await zenz.json()
