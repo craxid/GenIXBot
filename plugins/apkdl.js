261dd1cadcd77133c73e1db9129f1d2f649201a1
Dede Kurniawan <dede.klender@gmail.com>
2023-10-05 11:28:00 +0700

    update apkdl.js

--- plugins/apkdl.js
+++ plugins/apkdl.js
@@ -2,6 +2,10 @@
 
 import fetch from 'node-fetch'
 let handler = async (m, { conn, text }) => {
+  
+	
+if (!text) throw `✳️ Download aplikasi menggunakan ID\n\nContoh: !chord iwan fals manusia setengah dewa`
+m.reply(wait)
     
 try {
 let ling = await fetch(`https://vihangayt.me/download/apk?id=${text}`)
