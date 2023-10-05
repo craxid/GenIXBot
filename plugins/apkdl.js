816282b212a859fc1db47ef01cf96c723af4398e
Dede Kurniawan <dede.klender@gmail.com>
2023-10-05 11:27:25 +0700

    update apkdl.js

--- plugins/apkdl.js
+++ plugins/apkdl.js
@@ -4,5 +4,5 @@ import fetch from 'node-fetch'
 let handler = async (m, { conn, text }) => {
     
 try {
-let ling = await fetch(`https://vihangayt.me/download/apk?id=${`)
-let zenzapi = await zenz.json()
+let ling = await fetch(`https://vihangayt.me/download/apk?id=${text}`)
+let apkdl = await ling.json()
