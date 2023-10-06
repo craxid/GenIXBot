d6657c58afe430a160cbb454db7b478839ad9969
Dede Kurniawan <dede.klender@gmail.com>
2023-10-06 10:56:20 +0700

    update dl-tiktok.js

--- plugins/dl-tiktok.js
+++ plugins/dl-tiktok.js
@@ -5,7 +5,7 @@ import { tiktokdl } from '@bochilteam/scraper'
 let handler = async (m, { conn, text, args, usedPrefix, command}) => {
 
     
-if (!args[0]) throw `✳️ Masukan link Tiktok\n\n 📌 Contoh : ${usedPrefix + command} https://vm.tiktok.com/L3N2t1Kt02/`
+if (!text) throw `✳️ Masukan link Tiktok\n\n 📌 Contoh : ${usedPrefix + command} https://vm.tiktok.com/L3N2t1Kt02/`
 if (!args[0].match(/tiktok/gi)) throw `❎  Mohon periksa apakah link berasal dari TikTok`
 
 try {
