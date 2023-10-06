42eff2736b64f6ccaa09c1621604ede2a878e29f
Dede Kurniawan <dede.klender@gmail.com>
2023-10-06 10:08:08 +0700

    update jadianime2.js

--- plugins/jadianime2.js
+++ plugins/jadianime2.js
@@ -12,7 +12,7 @@ if (!mime) throw 'Kirim/Reply Gambar dengan caption .jadianime'
 m.reply(wait)
 let media = await q.download()
 let url = await uploadImage(media)
-let hasil = await toanime(media)
+let hasil = await toanime(url)
 
 await conn.sendFile(m.chat, hasil.image_data, 'img.jpg', 'Foto ke Anime\n© Mika Bot', m)
 	
