99780c18486d5758d891dec13d55730e9e0c8489
Dede Kurniawan <dede.klender@gmail.com>
2023-10-10 21:58:15 +0700

    update sfiledl.js

--- plugins/sfiledl.js
+++ plugins/sfiledl.js
@@ -34,7 +34,7 @@ renderLargerThumbnail: true
 }}},
 {quoted: m})
 
-await conn.sendFile(m.chat, sfiledl.result.url, 0, {fileName: `${sfiledl.result.title}`})
+await conn.sendMessage(m.chat, sfiledl.result.url, 0, {fileName: `${sfiledl.result.title}`})
 
 
 }
