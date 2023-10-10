5d8d3001fca5ceb5d93d98cdb77d45462c79d039
Dede Kurniawan <dede.klender@gmail.com>
2023-10-10 21:39:18 +0700

    update sfiledl.js

--- plugins/sfiledl.js
+++ plugins/sfiledl.js
@@ -4,11 +4,11 @@ import { MessageType, MessageOptions, Mimetype } from '@adiwajshing/baileys'
 
 let handler = async (m, { conn, args, usedPrefix, command }) => {
 await m.reply('Tunggu Sebentar...')
-let res = await fetch(`https://api.violetics.pw/api/downloader/sfile?apikey=beta&url=${args[0]}`)
+let res = await fetch(`https://api.yanzbotz.my.id/api/downloader/sfile?url=${args[0]}`)
 let json = await res.json()
 if (!args[0]) throw `Link not found..\n\nExample:\n${usedPrefix}sfile https://sfile.mobi/1Qot1AZcAsS7`
 // if (!args[0].match(/https:\/\/sfile.mobi\/)/gi)) throw `Link not found..\n\nExample:\n${usedPrefix}sfile https://sfile.mobi/1Qot1AZcAsS7`
-let src = `*Judul:* ${json.result.title}\n*Size:* ${json.result.size}\n*Link:* ${json.result.url}`.trim()
+let src = `*Judul:* ${json.result.title}\n*Link:* ${json.result.url}`.trim()
 m.reply(src)
 // conn.sendFile(m.chat, json.result.url, `${json.result.title}`,null, m)
 conn.sendMedia(m.chat, json.result.url, 0, {fileName: `${json.result.title}`})
