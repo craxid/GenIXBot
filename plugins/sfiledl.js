33525b9569dc1e7bed4d6521396b8ce49c472e43
Dede Kurniawan <dede.klender@gmail.com>
2023-10-10 21:40:56 +0700

    update sfiledl.js

--- plugins/sfiledl.js
+++ plugins/sfiledl.js
@@ -3,15 +3,23 @@ import fetch from 'node-fetch'
 import { MessageType, MessageOptions, Mimetype } from '@adiwajshing/baileys'
 
 let handler = async (m, { conn, args, usedPrefix, command }) => {
-await m.reply('Tunggu Sebentar...')
+await m.reply('Tunggu bentar...')
 let res = await fetch(`https://api.yanzbotz.my.id/api/downloader/sfile?url=${args[0]}`)
+
 let json = await res.json()
+
 if (!args[0]) throw `Link not found..\n\nExample:\n${usedPrefix}sfile https://sfile.mobi/1Qot1AZcAsS7`
+
 // if (!args[0].match(/https:\/\/sfile.mobi\/)/gi)) throw `Link not found..\n\nExample:\n${usedPrefix}sfile https://sfile.mobi/1Qot1AZcAsS7`
+
 let src = `*Judul:* ${json.result.title}\n*Link:* ${json.result.url}`.trim()
-m.reply(src)
+
+m.reply(wait)
+
 // conn.sendFile(m.chat, json.result.url, `${json.result.title}`,null, m)
+
 conn.sendMedia(m.chat, json.result.url, 0, {fileName: `${json.result.title}`})
+
 }
 
 handler.help = ['sfile'].map(v => v + ' <url>')
