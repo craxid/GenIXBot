7c42106b057ee90a81b0f3e773a17feb4b8e387b
Dede Kurniawan <dede.klender@gmail.com>
2023-10-10 21:20:54 +0700

    update sfiledl.js

--- plugins/sfiledl.js
+++ plugins/sfiledl.js
@@ -2,24 +2,24 @@
 
 import fb from 'api-dylux'
 import fetch from 'node-fetch'
-let limit = 15
+let limit = 
 
 let handler = async (m, { conn, text, usedPrefix, command }) => {
 
  
-if (!text) throw `✳️ Download Video Facebook\n\nContoh: !fbdl https://www.facebook.com/dede2015k/videos/228452386672915`
+if (!text) throw `✳️ Download Video Facebook\n\nContoh: !sfile https://sfile.mobi/2SoXEZQi3mF`
  
 try {
 let ling = await fetch(`https://api.arifzyn.xyz/download/facebook?url=${text}`)
-let fbdl = await ling.json()
+let sfiledl = await ling.json()
 
 let detil = `
-*Deskripsi:* ${fbdl.result.desc}
+*Deskripsi:* ${sfiledl.result.desc}
 
 _Sedang mengirim..._
 `
 let detil2 = `
-*Deskripsi:* ${fbdl.result.desc}
+*Deskripsi:* ${sfiledl.result.desc}
 `
 
 conn.sendMessage(m.chat, {text: detil,
@@ -29,14 +29,14 @@ isForwarded: true,
 externalAdReply: {
 title: ('Facebook Downloader'),
 body: global.namebot,
-thumbnailUrl: (`${fbdl.result.thumb}`),
+thumbnailUrl: (`${sfiledl.result.thumb}`),
 sourceUrl: global.fbku,
 mediaType: 1,
 renderLargerThumbnail: true
 }}},
 {quoted: m})
 
-await conn.sendFile(m.chat, fbdl.result.video_hd, 'ig.mp4', 'Facebook Downloader\n© Mika Bot', m)
+await conn.sendFile(m.chat, sfiledl.result.video_hd, 'ig.mp4', 'Facebook Downloader\n© Mika Bot', m)
 
  }
  catch {
