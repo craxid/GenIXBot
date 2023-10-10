ddd6c719b3d668867b07d38b5d53f6ae3622cdfb
Dede Kurniawan <dede.klender@gmail.com>
2023-10-10 21:19:26 +0700

    update sfiledl.js

---
+++ plugins/sfiledl.js
@@ -0,0 +1,53 @@
+
+
+import fb from 'api-dylux'
+import fetch from 'node-fetch'
+let limit = 15
+
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+
+ 
+if (!text) throw `✳️ Download Video Facebook\n\nContoh: !fbdl https://www.facebook.com/dede2015k/videos/228452386672915`
+ 
+try {
+let ling = await fetch(`https://api.arifzyn.xyz/download/facebook?url=${text}`)
+let fbdl = await ling.json()
+
+let detil = `
+*Deskripsi:* ${fbdl.result.desc}
+
+_Sedang mengirim..._
+`
+let detil2 = `
+*Deskripsi:* ${fbdl.result.desc}
+`
+
+conn.sendMessage(m.chat, {text: detil,
+contextInfo: {
+forwardingScore: 9999,
+isForwarded: true,
+externalAdReply: {
+title: ('Facebook Downloader'),
+body: global.namebot,
+thumbnailUrl: (`${fbdl.result.thumb}`),
+sourceUrl: global.fbku,
+mediaType: 1,
+renderLargerThumbnail: true
+}}},
+{quoted: m})
+
+await conn.sendFile(m.chat, fbdl.result.video_hd, 'ig.mp4', 'Facebook Downloader\n© Mika Bot', m)
+
+ }
+ catch {
+		m.reply(`❎ Error: Ada sebuah kesalahan`)
+	}
+}
+handler.help = ['facebook'].map(v => v + ' <url>')
+handler.tags = ['dl']
+handler.command = /^((sfile|fb)(downloder|dl)?)$/i
+handler.diamond = true
+
+export default handler
+
+//https://api.yanzbotz.my.id/api/downloader/sfile?url=https://sfile.mobi/2SoXEZQi3mF
