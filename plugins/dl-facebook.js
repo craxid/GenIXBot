ef08495d4f26345a54436111d7003974bbbd5d42
Dede Kurniawan <dede.klender@gmail.com>
2023-10-10 22:47:29 +0700

    update dl-facebook.js

--- plugins/dl-facebook.js
+++ plugins/dl-facebook.js
@@ -9,7 +9,7 @@ let handler = async (m, { conn, text, args, usedPrefix, command }) => {
 if (!args[0]) throw `✳️ Download Video Facebook\n\nContoh: !fbdl https://www.facebook.com/dede2015k/videos/228452386672915`
  
 try {
-let ling = await fetch(`https://api.arifzyn.xyz/download/facebook?url=${!args[0]}`)
+let ling = await fetch(`https://api.arifzyn.xyz/download/facebook?url=${args[0]}`)
 let fbdl = await ling.json()
 
 let detil = `
