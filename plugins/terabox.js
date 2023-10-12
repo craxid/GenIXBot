cbfed223f015e61c25de37b743f4f2432a1dac62
Dede Kurniawan <dede.klender@gmail.com>
2023-10-12 19:44:08 +0700

    update terabox.js

--- plugins/terabox.js
+++ plugins/terabox.js
@@ -9,31 +9,31 @@ if (!text) throw `✳️ Download aplikasi menggunakan ID\n\nContoh: !apkdl com.
 try {
 let ling = await fetch(`https://vihangayt.me/download/apk?id=${text}`)
 let apkdl = await ling.json()
-let apkname = `Nama Aplikasi : ${apkdl.data.name}.apk`
-let ukur = `${apkdl.data.size}`
+let apkname = `Nama Aplikasi : ${tb.result.name}.apk`
+let ukur = `${tb.result.size}`
 
 if (ukur.split('MB')[0] >= limit)
 return m.reply(`
 ${global.htki} *Mika Bot APK-DL* ${global.htka}
 
-${global.htjava} *Nama Aplikasi* : ${apkdl.data.name}
-${global.htjava} *Size* : ${apkdl.data.size}
+${global.htjava} *Nama Aplikasi* : ${tb.result.name}
+${global.htjava} *Size* : ${tb.result.size}
 
 ${global.htjava} _File melebihi batas unduhan_ *+${limit} MB*`)
 
 let detil = `
-*Nama Aplikasi:* ${apkdl.data.name}
-*Update:* ${apkdl.data.lastup}
-*Nama Paket:* ${apkdl.data.package}
-*Ukuran:* ${apkdl.data.size}
+*Nama Aplikasi:* ${tb.result.name}
+*Update:* ${tb.result.lastup}
+*Nama Paket:* ${tb.result.package}
+*Ukuran:* ${tb.result.size}
 
 _Sedang mengirim..._
 `
 let detil2 = `
-*Nama Aplikasi:* ${apkdl.data.name}.apk
-*Update:* ${apkdl.data.lastup}
-*Nama Paket:* ${apkdl.data.package}
-*Ukuran:* ${apkdl.data.size}
+*Nama Aplikasi:* ${tb.result.name}.apk
+*Update:* ${tb.result.lastup}
+*Nama Paket:* ${tb.result.package}
+*Ukuran:* ${tb.result.size}
 `
 
 conn.sendMessage(m.chat, {text: detil,
@@ -41,16 +41,16 @@ contextInfo: {
 forwardingScore: 9999,
 isForwarded: true,
 externalAdReply: {
-title: ('Tera Downloader'),
-body: (`${apkdl.data.name}`),
-thumbnailUrl: (`${apkdl.data.icon}`),
-sourceUrl: (`https://play.google.com/store/apps/details?id=${apkdl.data.package}`),
+title: ('Terabox Downloader'),
+body: (`${tb.result.name}`),
+thumbnailUrl: (`${tb.result.icon}`),
+sourceUrl: (`https://play.google.com/store/apps/details?id=${tb.result.package}`),
 mediaType: 1,
 renderLargerThumbnail: false
 }}},
 {quoted: m})
 
-await conn.sendFile(m.chat, apkdl.data.dllink, (`${apkdl.data.name}`), detil2, m)
+await conn.sendFile(m.chat, tb.result.dllink, (`${tb.result.name}`), detil2, m)
 
  }
  catch {
