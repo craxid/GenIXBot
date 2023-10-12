2b1070722afb178c4c92570f6a4dcaecd9258767
Dede Kurniawan <dede.klender@gmail.com>
2023-10-13 00:27:31 +0700

    update main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -231,7 +231,7 @@ isForwarded: true,
     "body": (`${username}`),
     "mediaType": 1,
     "thumbnailUrl": gmbr.getRandom(),
-    "renderLargerThumbnail": false
+    "renderLargerThumbnail": true
     }}}, 
     {quoted: m, fuk})
 
