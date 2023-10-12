b1c73da09f80547b39214aa9cbe129281712c653
Dede Kurniawan <dede.klender@gmail.com>
2023-10-13 01:17:35 +0700

    update jadianime.js

--- plugins/jadianime.js
+++ plugins/jadianime.js
@@ -13,9 +13,10 @@ m.reply(wait)
 let media = await q.download()
 let url = await uploadImage(media)
 let hasil = await toanime(url)
+let tegs = 'Convert Foto ke Anime\n©Mika Bot'
 //let hasil = await (await conn.getFile(`https://vihangayt.me/tools/toanime?url=${url}`))
 
-await conn.sendFile(m.chat, hasil.image_data, 'toanime.jpg', `Convert Foto ke Anime`, m)
+await conn.sendFile(m.chat, hasil.image_data, 'toanime.jpg', `Convert Foto ke Anim`, m)
 	
 }
 
