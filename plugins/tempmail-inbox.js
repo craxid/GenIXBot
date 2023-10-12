d43f6346edfc3da08dd743e049ac0e96bc61d403
Dede Kurniawan <dede.klender@gmail.com>
2023-10-12 15:02:23 +0700

    update tempmail-inbox.js

--- plugins/tempmail-inbox.js
+++ plugins/tempmail-inbox.js
@@ -37,7 +37,7 @@ renderLargerThumbnail: false
 }}},
 {quoted: m})*/
 
-await conn.sendMessage(m.chat, {text: imbox}, {quoted: m})
+await conn.sendMessage(m.chat, imbox, {quoted: m})
  }
  
 handler.help = ['mailbox'].map(v => v + ' <url>')
