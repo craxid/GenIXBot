1cc032b4b165694614ffdfc6e3142c67a118afd4
Dede Kurniawan <dede.klender@gmail.com>
2023-10-14 01:17:38 +0700

    update spotifysearch.js

--- plugins/spotifysearch.js
+++ plugins/spotifysearch.js
@@ -1,28 +1,53 @@
 
+
+import fb from 'api-dylux'
 import fetch from 'node-fetch'
-let handler = async (m, {conn, text }) => {
+let limit = 15
 
+let handler = async (m, { conn, text, args, usedPrefix, command }) => {
+ 
+if (!args[0]) throw `✳️ Spotify Downloader\n\nContoh: !spotify https://open.spotify.com/track/4Jj3yew48NodVHWePPUGR7`
+m.react(rwait)
 
-if (!text) throw '✳️ Masukan kueri Spotify'
+let ling = await fetch(`https://api.yanzbotz.my.id/api/downloader/spotify?url=${args[0]}`)
+let spotify = await ling.json()
 
-let ling = await fetch(`https://api.yanzbotz.my.id/api/cari/spotify?query=${text}`)
+let detil = `
+*Artis:* ${spotify.result.data.metadata.artist}
+*Judul:* ${spotify.result.data.metadata.name}
+*Album:* ${spotify.result.data.metadata.album}
 
-let hasil = await ling.json()
-let tes = hasil.all
-let teks = hasil.all.map(v => {
-    switch (v.type) {
-    case 'video': return `
-▢ ${v.result.data.title}
-▢ *Link* : ${v.result.data.url}
-▢ *Durasi* : ${v.result.data.duration}
-▢ *Popularitas:* ${v.resilt.data.popularity}
-   `.trim()
-    }
-  }).filter(v => v).join('\n\n________________________\n\n')
-  conn.sendFile(m.chat, tes[0].thumbnail, 'yts.jpeg', teks, m)
-}
-handler.help = ['spotifysearch'] 
+_Sedang mengirim..._
+`
+let detil2 = `
+*Artis:* ${spotify.result.data.metadata.artist}
+*Judul:* ${spotify.result.data.metadata.name}
+*Album:* ${spotify.result.data.metadata.album}
+`
+
+conn.sendMessage(m.chat, {text: detil,
+contextInfo: {
+forwardingScore: 9999,
+isForwarded: true,
+externalAdReply: {
+title: ('Spotify Downloader'),
+body: spotify.result.data.metadata.name,
+thumbnailUrl: (`${spotify.result.data.metadata.image}`),
+sourceUrl: (`${spotify.result.data.metadata.weburl}`),
+mediaType: 1,
+renderLargerThumbnail: false
+}}},
+{quoted: m})
+
+await conn.sendFile(m.chat, spotify.result.data.metadata.download, (`${spotify.result.data.metadata.name}(Mika Bot).m4a`), 'Spotify Downloader\n© Mika Bot', m)
+
+ }
+handler.help = ['facebook'].map(v => v + ' <url>')
 handler.tags = ['dl']
-handler.command = ['spotifysearch', 'sps'] 
+handler.command = /^((spotify|dl)?)$/i
+handler.diamond = true
 
 export default handler
+
+//https://api.yanzbotz.my.id/api/downloader/spotify?url=https://open.spotify.com/track/4Jj3yew48NodVHWePPUGR7
+//https://api.yanzbotz.my.id/api/cari/spotify?query=
