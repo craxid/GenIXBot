d291a6bc36868ac1ef4b598c63282ff3221473dc
Dede Kurniawan <dede.klender@gmail.com>
2023-10-14 00:47:12 +0700

    update dl-yts.js

--- plugins/dl-yts.js
+++ plugins/dl-yts.js
@@ -2,7 +2,7 @@
 import yts from 'yt-search'
 
 let handler = async (m, {conn, text }) => {
-  if (!text) throw '✳️ Masukan kueri video YouTube?'
+  if (!text) throw '✳️ Masukan kueri video YouTube'
   let results = await yts(text)
   let tes = results.all
   let teks = results.all.map(v => {
