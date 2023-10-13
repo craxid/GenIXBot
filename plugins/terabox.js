e98cfd9031c7c756f18f6815029e5e25317555a2
Dede Kurniawan <dede.klender@gmail.com>
2023-10-13 08:40:59 +0700

    update terabox.js

---
+++ plugins/terabox.js
@@ -0,0 +1,63 @@
+
+import fetch from 'node-fetch'
+let handler = async (m, { conn, text, args, isPrems, isOwner, usedPrefix, command }) => {
+
+let limit = 150
+
+if (!args[0]) throw `✳️ Download File Terabox\n\n Mana linknya?`
+ 
+try {
+let ling = await fetch(`https://api.yanzbotz.my.id/api/downloader/terabox?url=${args[0]}`)
+let tb = await ling.json()
+
+if (tb.result.size.split('MB')[0] >= limit)
+return m.reply(`
+${global.htki} *Mika Bot Terabox-DL* ${global.htka}
+
+${global.htjava} *Nama File* : ${tb.result.server_filename}
+${global.htjava} *Size* : ${tb.result.size}
+
+${global.htjava} _File melebihi batas unduhan_ *+${limit} MB*`)
+
+let detil = `
+*Nama File:* ${tb.result.server_filename}
+*Ukuran:* ${tb.result.size} MB
+*MD5:* ${tb.result.md5}
+
+_Sedang mengirim..._
+`
+let detil2 = `
+*Nama File:* ${tb.result.server_filename}
+*Ukuran:* ${tb.result.size} MB
+*MD5:* ${tb.result.md5}
+`
+
+conn.sendMessage(m.chat, {text: detil,
+contextInfo: {
+forwardingScore: 9999,
+isForwarded: true,
+externalAdReply: {
+title: ('Terabox Downloader'),
+body: tb.result.server_filename,
+thumbnailUrl: global.mikasampul,
+sourceUrl: (`${args[0]}`),
+mediaType: 1,
+renderLargerThumbnail: false
+}}},
+{quoted: m})
+
+await conn.sendFile(m.chat, { document: { url: tb.result.dllink }}, m)
+ }
+ catch {
+		m.reply(`❎ Error: Ada sebuah kesalahan`)
+	}
+}
+handler.help = ['terabox'].map(v => v + ' <url>')
+handler.tags = ['dl','prem']
+handler.command = /^((tera|box)(downloader|dl)?)$/i
+handler.premium = true
+
+export default handler
+
+
+//https://api.yanzbotz.my.id/api/downloader/terabox?url=https://www.terabox.app/wap/share/filelist?surl=JwlPncQOrzuYcvyrK683Cw
