dca1b6c022cfe3d6b55d1b3557c137482dc18232
Dede Kurniawan <dede.klender@gmail.com>
2023-10-14 00:46:15 +0700

    update spotifysearch.js

---
+++ plugins/spotifysearch.js
@@ -0,0 +1,30 @@
+
+import fetch from 'node-fetch'
+let handler = async (m, {conn, text }) => {
+  if (!text) throw '✳️ Masukan kueri video YouTube?'
+  let results = await yts(text)
+  let tes = results.all
+  let teks = results.all.map(v => {
+    switch (v.type) {
+      case 'video': return `
+▢ ${v.title}
+▢ *Link* : ${v.url}
+▢ *Durasi* : ${v.timestamp}
+▢ *Diunggah pada :* ${v.ago}
+▢ *Ditonton:* ${v.views}
+
+   `.trim()
+      case 'channel': return `
+▢ *${v.name}* (${v.url})
+▢${v.subCountLabel} (${v.subCount}) Subscriber
+▢ ${v.videoCount} video
+`.trim()
+    }
+  }).filter(v => v).join('\n\n________________________\n\n')
+  conn.sendFile(m.chat, tes[0].thumbnail, 'yts.jpeg', teks, m)
+}
+handler.help = ['ytsearch'] 
+handler.tags = ['dl']
+handler.command = ['ytsearch', 'yts'] 
+
+export default handler
