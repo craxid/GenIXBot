57ad151ab59aeebb71b00e15566aa0b757cefde7
Dede Kurniawan <dede.klender@gmail.com>
2023-10-14 20:42:41 +0700

    update spotifysearch.js

--- plugins/spotifysearch.js
+++ plugins/spotifysearch.js
@@ -6,19 +6,19 @@ let limit = 15
 
 let handler = async (m, { conn, text, args, usedPrefix, command }) => {
  
-if (!text) throw `✳️ Spotify Search\n\nContoh: !spotify Let it Go`
+if (!text) throw `✳️ spotfind Search\n\nContoh: !spotfind Let it Go`
 m.react(rwait)
 
-let ling = await fetch(`https://api.yanzbotz.my.id/api/cari/spotify?query=${text}`)
-let spotify = await ling.json()
+let ling = await fetch(`https://api.yanzbotz.my.id/api/cari/spotfind?query=${text}`)
+let spotfind = await ling.json()
 
 let detil = `
-*Judul:* ${spotify.result.data.artist}
-*Durasi:* ${spotify.result.data.title}
-*Popularitas:* ${spotify.result.data.popularity}
-*Pratinjau:* ${spotify.result.data.preview}
+*Judul:* ${spotfind.result.data.artist}
+*Durasi:* ${spotfind.result.data.title}
+*Popularitas:* ${spotfind.result.data.popularity}
+*Pratinjau:* ${spotfind.result.data.preview}
 
-*URL:* ${spotify.result.data.url}
+*URL:* ${spotfind.result.data.url}
 `
 
 conn.sendMessage(m.chat, {text: detil,
@@ -26,26 +26,26 @@ contextInfo: {
 forwardingScore: 9999,
 isForwarded: true,
 externalAdReply: {
-title: ('Spotify Search'),
-body: spotify.result.data.name,
+title: ('spotfind Search'),
+body: spotfind.result.data.name,
 thumbnailUrl: global.ppmika,
-sourceUrl: (`${spotify.result.data.url}`),
+sourceUrl: (`${spotfind.result.data.url}`),
 mediaType: 1,
 renderLargerThumbnail: false
 }}},
 {quoted: m})
 
-//await conn.sendFile(m.chat, spotify.result.data.download, (`${spotify.result.data.name} (Mika Bot).m4a`), 'Spotify Downloader\n© Mika Bot', m)
+//await conn.sendFile(m.chat, spotfind.result.data.download, (`${spotfind.result.data.name} (Mika Bot).m4a`), 'spotfind Downloader\n© Mika Bot', m)
 
  }
-handler.help = ['spotifysearch'].map(v => v + ' <url>')
+handler.help = ['spotfindsearch'].map(v => v + ' <url>')
 handler.tags = ['dl','prem']
-handler.command = /^((spotifysearch)?)$/i
+handler.command = /^((spotfindsearch)?)$/i
 handler.diamond = false
 
 export default handler
 
-//https://api.yanzbotz.my.id/api/downloader/spotify?url=https://open.spotify.com/track/4Jj3yew48NodVHWePPUGR7
+//https://api.yanzbotz.my.id/api/downloader/spotfind?url=https://open.spotfind.com/track/4Jj3yew48NodVHWePPUGR7
 
-//https://api.yanzbotz.my.id/api/downloader/spotify?url=https://open.spotify.com/track/4Jj3yew48NodVHWePPUGR7
-//https://api.yanzbotz.my.id/api/cari/spotify?query=
+//https://api.yanzbotz.my.id/api/downloader/spotfind?url=https://open.spotfind.com/track/4Jj3yew48NodVHWePPUGR7
+//https://api.yanzbotz.my.id/api/cari/spotfind?query=
