091392e0d88466f7deed686ed631bed20d579788
Dede Kurniawan <dede.klender@gmail.com>
2023-10-14 20:28:52 +0700

    update spotifydl.js

--- plugins/spotifydl.js
+++ plugins/spotifydl.js
@@ -39,7 +39,7 @@ renderLargerThumbnail: false
 }}},
 {quoted: m})
 
-await conn.sendFile(m.chat, spotify.result.data.metadata.download, (`${spotify.result.data.metadata.name}(Mika Bot).mp3`), 'Spotify Downloader\n© Mika Bot', m)
+await conn.sendFile(m.chat, spotify.result.data.metadata.download, (`${spotify.result.data.metadata.name} (Mika Bot).mp3`), 'Spotify Downloader\n© Mika Bot', m)
 
  }
 handler.help = ['facebook'].map(v => v + ' <url>')
