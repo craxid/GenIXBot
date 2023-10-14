473b346556ec72c185824c95ea96c575368a1850
Dede Kurniawan <dede.klender@gmail.com>
2023-10-14 20:59:19 +0700

    update spotifysearch.js

--- plugins/spotifysearch.js
+++ plugins/spotifysearch.js
@@ -11,16 +11,16 @@ m.react(rwait)
 
 let ling = await fetch(`https://api.yanzbotz.my.id/api/cari/spotify?query=${text}`)
 
-if (spotfind && spotfind.result.length > 0) {
-const hasil = spotfind.result[0];
+if (spotfind && hasil.result.length > 0) {
+const hasil = hasil.result[0];
 
 let detil = `
-*Judul:* ${spotfind.result.data.title}
-*Durasi:* ${spotfind.result.data.duration}
-*Popularitas:* ${spotfind.result.data.popularity}
-*Pratinjau:* ${spotfind.result.data.preview}
+*Judul:* ${hasil.result.data.title}
+*Durasi:* ${hasil.result.data.duration}
+*Popularitas:* ${hasil.result.data.popularity}
+*Pratinjau:* ${hasil.result.data.preview}
 
-*URL:* ${spotfind.result.data.url}
+*URL:* ${hasil.result.data.url}
 `
 
 conn.sendMessage(m.chat, {text: detil,
@@ -29,16 +29,16 @@ forwardingScore: 9999,
 isForwarded: true,
 externalAdReply: {
 title: ('Spotify Search'),
-body: spotfind.result.data.name,
+body: hasil.result.data.name,
 thumbnailUrl: global.ppmika,
-sourceUrl: (`${spotfind.result.data.url}`),
+sourceUrl: (`${hasil.result.data.url}`),
 mediaType: 1,
 renderLargerThumbnail: false
 }}},
 {quoted: m})
 }
 
-//await conn.sendFile(m.chat, spotfind.result.data.download, (`${spotfind.result.data.name} (Mika Bot).m4a`), 'spotfind Downloader\n© Mika Bot', m)
+//await conn.sendFile(m.chat, hasil.result.data.download, (`${hasil.result.data.name} (Mika Bot).m4a`), 'spotfind Downloader\n© Mika Bot', m)
 
  }
 handler.help = ['spotifysearch'].map(v => v + ' <url>')
@@ -48,7 +48,7 @@ handler.diamond = false
 
 export default handler
 
-//https://api.yanzbotz.my.id/api/downloader/spotfind?url=https://open.spotfind.com/track/4Jj3yew48NodVHWePPUGR7
+//https://api.yanzbotz.my.id/api/downloader/spotfind?url=https://open.hasil.com/track/4Jj3yew48NodVHWePPUGR7
 
-//https://api.yanzbotz.my.id/api/downloader/spotfind?url=https://open.spotfind.com/track/4Jj3yew48NodVHWePPUGR7
+//https://api.yanzbotz.my.id/api/downloader/spotfind?url=https://open.hasil.com/track/4Jj3yew48NodVHWePPUGR7
 //https://api.yanzbotz.my.id/api/cari/spotfind?query=
