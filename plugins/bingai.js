e90f5e2519b260d93d77a3574cd060c01e255982
Dede Kurniawan <dede.klender@gmail.com>
2023-10-14 13:29:33 +0700

    update bingai.js

--- plugins/bingai.js
+++ plugins/bingai.js
@@ -33,3 +33,29 @@ handler.diamond = true
 
 export default handler
 //conn.reply(set.owner[0] + '@s.whatsapp.net', 'Fitur: Bing Ai\n\n' + String(e), null)
+
+/*import { ChatBot, conversation_style } from 'bingai-js'
+export default async function handler(m, {
+    text: prompt
+}) {
+    if (!prompt) return m.reply("Usage:\n!bing < prompt >\n\nExample:\n!bing What is Javascript")
+    try {
+        m.reply(msg.wait)
+        const cookie = bingCookie
+        const a = new ChatBot(cookie);
+        await a.init();
+        /**
+         *   balanced : conversation_style.balanced
+         *   creative : conversation_style.creative
+         *   precise  : conversation_style.precise
+         *
+        m.reply(await a.ask(prompt, conversation_style.creative))
+    } catch (e) {
+        m.reply(msg.err)
+        conn.reply(set.owner[0] + '@s.whatsapp.net', 'Fitur: Bing Ai\n\n' + String(e), null)
+    }
+}
+handler["command"] = ["bing", "bingai"]
+handler["help"] = ["bing / bingai < prompt >"]
+handler["tags"] = ["ai"]
+handler["register"] = true*/
