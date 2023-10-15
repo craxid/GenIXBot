3536b627c6d8860691faef9606475d76191670dd
Dede Kurniawan <dede.klender@gmail.com>
2023-10-15 18:40:22 +0700

    update dl-tiktok.js

--- plugins/dl-tiktok.js
+++ plugins/dl-tiktok.js
@@ -1,4 +1,53 @@
 
+import fg from 'api-dylux' 
+import { tiktokdl } from '@bochilteam/scraper'
+    let limit =15
+let handler = async (m, { conn, text, args, usedPrefix, command}) => {
+
+    
+if (!args[0]) throw `✳️ Masukan link Tiktok\n\n 📌 Contoh : ${usedPrefix + command} https://vm.tiktok.com/L3N2t1Kt02/`
+if (!args[0].match(/tiktok/gi)) throw `❎  Mohon periksa apakah link berasal dari TikTok`
+m.react(rwait)
+
+try {
+    let p = await fg.tiktok(args[0]) 
+    
+if (size.split('MB')[0] >= limit) return m.reply(` ≡  *Mika Bot YTDL*\n\n▢ *⚖️ Size* : ${size}\n▢ *🎞️ Query* : ${q}\n\n▢ _The file exceeds the download limit_ *+${limit} MB*`)
+
+    let te = `
+┌─⊷ TIKTOK
+▢ *Nama:* ${p.nickname}
+▢ *Username:* ${p.unique_id}
+▢ *Durasi:* ${p.duration}
+▢ *Deskripsi:* ${p.description}
+└───────────`
+    conn.sendFile(m.chat, p.play, 'tiktok.mp4', te, m)
+    m.react(done)
+    } catch {  	
+    try { 
+	const { author: { nickname }, video, description } = await tiktokdl(args[0])
+    const url = video.no_watermark || video.no_watermark2 || video.no_watermark_raw
+    if (!url) throw '❎ Error while downloading the video'
+    conn.sendFile(m.chat, url, 'fb.mp4', `
+┌─⊷ *TIKTOK DL-2*
+▢ *Nickname:* ${nickname} ${description ? `\n▢ *Deskripsi:* ${description}` : ''}
+└───────────`, m)
+m.react(done)
+} catch {
+    m.reply(`❎ Error while downloading the video`)
+}
+}
+    
+}  
+handler.help = ['tiktok']
+handler.tags = ['dl']
+handler.command = /^(tiktok|ttdl|tiktokdl|tiktoknowm)$/i
+handler.diamond = true
+
+// batas
+export default handler
+
+
 import fetch from 'node-fetch'
 
 let handler = async (m, { conn, text, usedPrefix, command }) => {
