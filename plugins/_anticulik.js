fda497f2c247c04db51c8b45e64684228a69d6f8
Dede Kurniawan <dede.klender@gmail.com>
2023-10-17 18:22:28 +0700

    update _anticulik.js

--- plugins/_anticulik.js
+++ plugins/_anticulik.js
@@ -6,7 +6,7 @@ handler.all = async function (m, { isBlocked }) {
     if (isBlocked) return
     if ((m.mtype === 'groupInviteMessage' || m.text.startsWith('Undangan untuk bergabung') || m.text.startsWith('Invitation to join') || m.text.startsWith('Buka tautan ini')) && !m.isBaileys && !m.isGroup) {
     let teks = `Invite Group
-• 14 Day / Rp 10k
+• 14 Day / Rp 15k
 • 30 Day / Rp 25k
 
 Jika berminat hubungi: @${global.owner[0]} untuk order:)
