5bfa4f2a3f78ea420ccac3e914c3d56b81e46eb0
Dede Kurniawan <dede.klender@gmail.com>
2023-10-17 09:38:28 +0700

    update asahotak_bantu.js

--- plugins/asahotak_bantu.js
+++ plugins/asahotak_bantu.js
@@ -5,7 +5,7 @@ let handler = async (m, { conn }) => {
     let json = conn.asahotak[id][1]
     let ans = json.jawaban
     let clue = ans.replace(/[bcdfghjklmnpqrstvwxyz]/g, '_')
-    m.reply('```' + clue + '```')
+    m.reply('```' + clue + '```\nBalas soalnya, bukan pesan ini')
 }
 handler.command = /^ao$/i
 handler.diamond = true
