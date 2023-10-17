555cdade0cf4d3d9089cb484f8777d5745a5e9a1
Dede Kurniawan <dede.klender@gmail.com>
2023-10-17 18:28:38 +0700

    update handler.js

--- handler.js
+++ handler.js
@@ -536,7 +536,7 @@ text = (action === 'add' ? (chat.sWelcome || this.welcome || conn.welcome || 'We
 
 (chat.sBye || this.bye || conn.bye || 'Bye, @user!')).replace('@user', await this.getName(user))
 
-let wel = API(`https://api.lolhuman.xyz/api/base/welcome?apikey=DannKristii&img1=https://i.postimg.cc/CM34YRFb/photo-2021-02-05-10-13-39.jpg&img2=https://i.postimg.cc/CM34YRFb/photo-2021-02-05-10-13-39.jpg&background=https://i.ibb.co/8B6Q84n/LTqHsfYS.jpg&username=${name}&member=${member}&groupname=${gpname}`)
+let wel = API(`https://api.lolhuman.xyz/api/base/welcome?apikey=DannKristii&img1=${pp}&img2=${pp}&background=https://i.ibb.co/8B6Q84n/LTqHsfYS.jpg&username=${name}&member=${member}&groupname=${gpname}`)
 
 let lea = API(`https://api.lolhuman.xyz/api/base/leave?apikey=DannKristii&img1=https://i.postimg.cc/CM34YRFb/photo-2021-02-05-10-13-39.jpg&img2=https://i.postimg.cc/CM34YRFb/photo-2021-02-05-10-13-39.jpg&background=https://i.ibb.co/8B6Q84n/LTqHsfYS.jpg&username=${name}&member=${member}&groupname=${gpname}`)
 
