e052f0299916bde6fabc635f6888744d089ab633
Dede Kurniawan <dede.klender@gmail.com>
2023-10-17 09:13:03 +0700

    update handler.js

--- handler.js
+++ handler.js
@@ -527,13 +527,14 @@ let pp = 'https://i.ibb.co/sQTkHLD/ppkosong.png'
 let name = await this.getName(user)
 let gpname = await this.getName(id)
 let member = groupMetadata.participants.length
-                        pp: pp
-                        try {
-                            pp = await this.profilePictureUrl(user, 'image')
-                        } catch (e) {
-                        } finally {
-                            text = (action === 'add' ? (chat.sWelcome || this.welcome || conn.welcome || 'Welcome, @user!').replace('@subject', await this.getName(id)).replace('@desc', groupMetadata.desc ? String.fromCharCode(8206).repeat(4001) + groupMetadata.desc : '') :
-                                (chat.sBye || this.bye || conn.bye || 'Bye, @user!')).replace('@user', await this.getName(user))
+pp: pp
+try {
+pp = await this.profilePictureUrl(user, 'image')
+} catch (e) {
+} finally {
+text = (action === 'add' ? (chat.sWelcome || this.welcome || conn.welcome || 'Welcome, @user!').replace('@subject', await this.getName(id)).replace('@desc', groupMetadata.desc ? String.fromCharCode(8206).repeat(4001) + groupMetadata.desc : '') :
+
+(chat.sBye || this.bye || conn.bye || 'Bye, @user!')).replace('@user', await this.getName(user))
                             let wel = API(`https://api.lolhuman.xyz/api/base/welcome?apikey=gunturganteng&img1=https://i.postimg.cc/CM34YRFb/photo-2021-02-05-10-13-39.jpg&img2=https://i.postimg.cc/CM34YRFb/photo-2021-02-05-10-13-39.jpg&background=https://i.ibb.co/8B6Q84n/LTqHsfYS.jpg&username=${name}&member=${member}&groupname=${gpname}`)
                             let lea = API(`https://api.lolhuman.xyz/api/base/leave?apikey=gunturganteng&img1=https://i.postimg.cc/CM34YRFb/photo-2021-02-05-10-13-39.jpg&img2=https://i.postimg.cc/CM34YRFb/photo-2021-02-05-10-13-39.jpg&background=https://i.ibb.co/8B6Q84n/LTqHsfYS.jpg&username=${name}&member=${member}&groupname=${gpname}`)
                              this.sendFile(id, action === 'add' ? wel : lea, 'pp.jpg', text, null, false, { mentions: [user] })
