4099f223e1cc220f74947e010db6a96ea69d26b5
Dede Kurniawan <dede.klender@gmail.com>
2023-10-18 21:21:12 +0700

    update sticker-nobg.js

--- plugins/sticker-nobg.js
+++ plugins/sticker-nobg.js
@@ -13,9 +13,13 @@ let media = await q.download()
 let url = await uploadImage(media)
 let hasil = await (await conn.getFile(`https://api.zahwazein.xyz/convert/sticker-nobg?url=${url}&apikey=zenzkey_f59c1aacf2`))
 
-let stiker = await sticker(null, hasil, global.packname, global.author)
+try {
+let stiker = await sticker(null, apiUrl, global.packname, global.author)
 conn.sendFile(m.chat, stiker, null, { asSticker: true }, m)
-//await conn.sendFile(m.chat, hasil.data, 'img.webp', 'Sticker No bg\n© Mika Bot', m)
+} catch (e) {
+m.reply('Error saat mengkonversi stiker, file terlalu besar')
+await conn.sendFile(m.chat, apiUrl, 'smaker.png', null, m)
+}}
 	
 }
 handler.help = ['snobg']
