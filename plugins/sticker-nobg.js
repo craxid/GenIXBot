f52240d79c17a0c9c43a405f7d6daefbfb47feec
Dede Kurniawan <dede.klender@gmail.com>
2023-10-18 21:22:37 +0700

    update sticker-nobg.js

--- plugins/sticker-nobg.js
+++ plugins/sticker-nobg.js
@@ -2,6 +2,7 @@
 import fetch from 'node-fetch'
 import uploadImage from '../lib/uploadImage.js'
 import { sticker } from '../lib/sticker.js'
+
 let handler = async (m, { conn, usedPrefix, command, text }) => {
 let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 let name = await conn.getName(who)
