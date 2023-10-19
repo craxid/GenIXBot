b303a9e2f31d13fb54a6be7ba94ea4b0fcc2d633
Dede Kurniawan <dede.klender@gmail.com>
2023-10-19 08:04:21 +0700

    update _anticulik.js

--- plugins/_anticulik.js
+++ plugins/_anticulik.js
@@ -9,6 +9,7 @@ handler.all = async function (m, { isBlocked }) {
 • 14 Day / Rp 15k
 • 30 Day / Rp 25k
 
+Kamu dapat mengundang bot kedalam grup
 Jika berminat hubungi: @${global.deka} untuk order:)
 `
     this.reply(m.chat, teks, m)
