156dfedd03d0dcba61a1843accc4b86e6717f5ec
Dede Kurniawan <dede.klender@gmail.com>
2023-10-20 22:44:13 +0700

    update ktp.js

--- plugins/ktp.js
+++ plugins/ktp.js
@@ -9,7 +9,7 @@ let handler = async (m, { conn, args }) => {
   
   let res = fetch(`https://oni-chan.my.id/api/py-chan/ktpmaker?nik=${response[0]}&nama=${response[1]}&ttl=${response[2]}&jk=${response[3]}&gd=${response[4]}&almt=${response[5]}&rtw=${response[6]}&kel=${response[7]}&kc=${response[8]}&agm=${response[9]}&st=${response[10]}&krj=${response[11]}&ngr=${response[12]}&blk=${response[13]}&prv=${response[14]}&kab=${response[15]}&picturl=https://telegra.ph/file/13d20b5f39f5e4cf1aaf2.jpg&apikey=ui5F-4yJ3-Fo9s`)
   
-    conn.sendMessage(m.chat, res, 'ktp.jpg', `Nih kak\n© Mika Bot`, m)
+    conn.sendFile(m.chat, res, 'ktp.jpg', `Nih kak\n© Mika Bot`, m)
 }
 handler.help = ['ktp'].map(v => v + ' <text>')
 handler.tags = ['tools']
