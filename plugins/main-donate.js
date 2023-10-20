34381b2f1dc765f8d420eb3a7cd3c96fafc1e4d3
Dede Kurniawan <dede.klender@gmail.com>
2023-10-20 10:03:08 +0700

    update main-donate.js

--- plugins/main-donate.js
+++ plugins/main-donate.js
@@ -26,7 +26,7 @@ contextInfo: {
 externalAdReply: {
 title: (`Donasi`),
 body: ('Dukung aku agar bot ini tetap hidup'),
-thumbnailUrl: 'https://telegra.ph/file/77e1d9fa9c14042bcfcb4.png',
+thumbnailUrl: ('https://telegra.ph/file/77e1d9fa9c14042bcfcb4.png'),
 sourceUrl: ('https://teer.id/dede_klender'),
 mediaType: 1,
 thumbnailHeight: 640,
