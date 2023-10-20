5fde4ebf9d5c2a8fd6dc2e5c3640a0b6e19d6a75
Dede Kurniawan <dede.klender@gmail.com>
2023-10-20 22:34:36 +0700

    update ktp.js

--- plugins/ktp.js
+++ plugins/ktp.js
@@ -7,7 +7,7 @@ let handler = async (m, { conn, args }) => {
   
   m.reply(wait)
   
-  let res = `https://oni-chan.my.id/api/py-chan/ktpmaker?nik=${encodeURIComponent(0)}&nama=${response[1]}&ttl=${encodeURIComponent(1)}&jk=${response[3]}&gd=abc&almt=${response[4]}&rtw=${response[5]}&kel=${response[6]}&kc=${response[7]}&agm=${response[8]}&st=${response[9]}&krj=${response[10]}&ngr=${response[11]}&blk=${response[12]}&prv=${response[13]}&kab=${response[14]}&picturl=https://telegra.ph/file/13d20b5f39f5e4cf1aaf2.jpg&apikey=ui5F-4yJ3-Fo9s`
+  let res = `https://oni-chan.my.id/api/py-chan/ktpmaker?nik=${response[0]}&nama=${response[1]}&ttl=${response[2]}&jk=${response[3]}&gd=abc&almt=${response[4]}&rtw=${response[5]}&kel=${response[6]}&kc=${response[7]}&agm=${response[8]}&st=${response[9]}&krj=${response[10]}&ngr=${response[11]}&blk=${response[12]}&prv=${response[13]}&kab=${response[14]}&picturl=https://telegra.ph/file/13d20b5f39f5e4cf1aaf2.jpg&apikey=ui5F-4yJ3-Fo9s`
     conn.sendMessage(m.chat, res, 'ktp.jpg', `Nih kak\n${global.wm2}`, m, false)
 }
 handler.help = ['ktp'].map(v => v + ' <text>')
