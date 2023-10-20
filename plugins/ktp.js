25332c02250e98784c3d77eb2e28161fef0d5d91
Dede Kurniawan <dede.klender@gmail.com>
2023-10-20 22:53:16 +0700

    update ktp.js

--- plugins/ktp.js
+++ plugins/ktp.js
@@ -7,7 +7,7 @@ let handler = async (m, { conn, args }) => {
   
   m.reply(wait)
   
-  let [0,1,2,3,4,5] = text.split`+`
+  let [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15] = text.split`|`
   let res = await fetch(`https://oni-chan.my.id/api/py-chan/ktpmaker?nik=${encodeURIComponent(0)}&nama=${encodeURIComponent(1)}&ttl=${encodeURIComponent(2)}&jk=${encodeURIComponent(3)}&gd=${encodeURIComponent(4)}&almt=${encodeURIComponent(5)}&rtw=${encodeURIComponent(6)}&kel=${encodeURIComponent(7)}&kc=${encodeURIComponent(8)}&agm=${encodeURIComponent(9)}&st=${encodeURIComponent(10)}&krj=${encodeURIComponent(11)}&ngr=${encodeURIComponent(12)}&blk=${encodeURIComponent(13)}&prv=${encodeURIComponent(14)}&kab=${encodeURIComponent(15)}&picturl=https://telegra.ph/file/13d20b5f39f5e4cf1aaf2.jpg&apikey=ui5F-4yJ3-Fo9s`)
   
     conn.sendFile(m.chat, res, 'ktp.jpg', `Nih kak\n© Mika Bot`, m)
